ede1ad4805ac9b2228c3ea78100858d3c44cb165
gabriele <gabriele.marino@zextras.com>
2022-01-13 11:03:10 +0100

    fix: deleted double licence block in edit-view

--- src/views/app/detail-panel/edit/edit-view.jsx
+++ src/views/app/detail-panel/edit/edit-view.jsx
@@ -3,12 +3,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-/*
- * SPDX-FileCopyrightText: 2021 2021 Zextras <https://www.zextras.com>
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
-import React, { useCallback, useEffect, useMemo, useRef, useState, useContext } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import {
 	Button,
 	Catcher,
