5964e271b99b482e51df3abcc63a0c2ec8a981d4
Gabriele Marino <62339254+nubsthead@users.noreply.github.com>
2022-02-10 12:39:32 +0100

    feat: message displayer actions fill available view, the remaining ones will be inside dropdown
    * feat: message displayer actions fill available view, the remaining ones will be inside dropdown
    
    * fix: using calculated minWidth to avoid overflow conflicts
    
    * fix: improved paddings and corner cases

--- src/ui-actions/mail-message-preview-actions.tsx
+++ src/ui-actions/mail-message-preview-actions.tsx
@@ -3,191 +3,126 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-/* eslint-disable import/extensions */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
-/* eslint-disable import/no-unresolved */
-import React, { useContext, useMemo, FC, ReactElement } from 'react';
-import {
-	Row,
-	Dropdown,
-	IconButton,
-	SnackbarManagerContext,
-	Tooltip,
-	useModal
-} from '@zextras/carbonio-design-system';
-import { includes, map } from 'lodash';
-import { useTranslation } from 'react-i18next';
-import { useDispatch } from 'react-redux';
-import {
-	useIntegratedComponent,
-	useReplaceHistoryCallback,
-	useAppContext,
-	FOLDERS
-} from '@zextras/carbonio-shell-ui';
-
-import {
-	deleteMsg,
-	editAsNewMsg,
-	editDraft,
-	forwardMsg,
-	moveMsgToTrash,
-	redirectMsg,
-	replyAllMsg,
-	replyMsg,
-	sendDraft,
-	setMsgFlag,
-	setMsgAsSpam,
-	printMsg,
-	showOriginalMsg,
-	setMsgRead,
-	moveMessageToFolder,
-	deleteMessagePermanently
-	// @ts-ignore
-} from './message-actions';
-import { MailMessage } from '../types/mail-message';
-import { useSelection } from '../hooks/useSelection';
+import { FOLDERS } from '@zextras/carbonio-shell-ui';
+import React, {
+	useMemo,
+	FC,
+	ReactElement,
+	useRef,
+	useLayoutEffect,
+	useState,
+	useCallback,
+	useContext
+} from 'react';
+import { Row, IconButton, Tooltip, Dropdown, ThemeContext } from '@zextras/carbonio-design-system';
+import { difference, map, slice } from 'lodash';
+import { useParams } from 'react-router-dom';
+import { useVisibleActionsCount } from '../hooks/use-visible-actions-count';
 
 type MailMsgPreviewActionsType = {
-	folderId: string;
-	message: MailMessage;
-	timezone: string;
+	actions: Array<any>;
+	maxActions?: number;
+	maxWidth?: string;
+	mainAlignment?: string;
 };
+
+type ThemeContextProps = {
+	sizes: {
+		icon: {
+			large: string;
+		};
+	};
+};
+
 const MailMsgPreviewActions: FC<MailMsgPreviewActionsType> = ({
-	message,
-	folderId,
-	timezone
+	actions,
+	maxWidth = '120px',
+	mainAlignment = 'flex-end'
 }): ReactElement => {
-	const [t] = useTranslation();
-	const replaceHistory = useReplaceHistoryCallback();
-	const createSnackbar = useContext(SnackbarManagerContext);
-	const dispatch = useDispatch();
-	const createModal = useModal();
-	const ContactInput = useIntegratedComponent('contact-input');
-	const { setCount } = useAppContext();
-	const { deselectAll } = useSelection(folderId, setCount);
-	const systemFolders = useMemo(
-		() => [FOLDERS.INBOX, FOLDERS.SENT, FOLDERS.DRAFTS, FOLDERS.TRASH, FOLDERS.SPAM],
-		[]
+	const { folderId }: { folderId: string } = useParams();
+	const actionContainerRef = useRef<HTMLInputElement>(null);
+	const [open, setOpen] = useState(false);
+	const theme = useContext<ThemeContextProps>(ThemeContext);
+
+	const maxActions = useMemo(() => {
+		if (folderId === FOLDERS.TRASH) return 2;
+		if (folderId === FOLDERS.DRAFTS) return 4;
+		return 6;
+	}, [folderId]);
+
+	const [visibleActionsCount, calculateVisibleActionsCount] = useVisibleActionsCount(
+		actionContainerRef,
+		{ numberLimit: maxActions }
 	);
-	const primaryActions = useMemo(() => {
-		const arr = [];
 
-		if (message.parent === FOLDERS.DRAFTS) {
-			arr.push(sendDraft(message.id, message, t, dispatch));
-			arr.push(editDraft(message.id, folderId, t, replaceHistory));
-			arr.push(
-				moveMsgToTrash(
-					[message.id],
-					t,
-					dispatch,
-					createSnackbar,
-					deselectAll,
-					folderId,
-					replaceHistory,
-					message.conversation
-				)
-			);
-		}
-		if (
-			message.parent === FOLDERS.INBOX ||
-			message.parent === FOLDERS.SENT ||
-			!includes(systemFolders, message.parent)
-		) {
-			// INBOX, SENT OR CREATED_FOLDER
-			arr.push(replyMsg(message.id, folderId, t, replaceHistory));
-			arr.push(replyAllMsg(message.id, folderId, t, replaceHistory));
-			arr.push(forwardMsg(message.id, folderId, t, replaceHistory));
-			arr.push(
-				moveMsgToTrash(
-					[message.id],
-					t,
-					dispatch,
-					createSnackbar,
-					deselectAll,
-					folderId,
-					replaceHistory,
-					message.conversation
-				)
-			);
-			arr.push(
-				setMsgRead([message.id], message.read, t, dispatch, folderId, replaceHistory, deselectAll)
-			);
-		}
+	const firstActions = useMemo(
+		() =>
+			slice(
+				actions,
+				0,
+				visibleActionsCount > 0 && actions?.length > 2
+					? visibleActionsCount - 1
+					: visibleActionsCount
+			),
+		[actions, visibleActionsCount]
+	);
 
-		if (message.parent === FOLDERS.TRASH) {
-			arr.push(moveMessageToFolder([message.id], t, dispatch, true, createModal, deselectAll));
-			arr.push(deleteMessagePermanently([message.id], t, dispatch, createModal, deselectAll));
-		}
-		if (message.parent === FOLDERS.SPAM) {
-			arr.push(deleteMsg([message.id], t, dispatch, createSnackbar, createModal));
-			arr.push(setMsgAsSpam([message.id], true, t, dispatch, replaceHistory));
-			arr.push(printMsg(message.id, t, timezone));
-			arr.push(showOriginalMsg(message.id, t));
-		}
-		return arr;
-	}, [
-		message,
-		systemFolders,
-		t,
-		dispatch,
-		folderId,
-		replaceHistory,
-		createSnackbar,
-		deselectAll,
-		createModal,
-		timezone
-	]);
+	const secondActions = useMemo(() => difference(actions, firstActions), [actions, firstActions]);
+	const iconSize = useMemo(() => parseInt(theme.sizes.icon.large, 10), [theme?.sizes?.icon?.large]);
 
-	const secondaryActions = useMemo(() => {
-		const arr = [];
-		if (message.parent === FOLDERS.DRAFTS) {
-			arr.push(setMsgFlag([message.id], message.flagged, t, dispatch));
-		}
-		if (
-			message.parent === FOLDERS.INBOX ||
-			message.parent === FOLDERS.SENT ||
-			!includes(systemFolders, message.parent)
-		) {
-			// INBOX, SENT OR CREATED_FOLDER
-			arr.push(moveMessageToFolder([message.id], t, dispatch, false, createModal, deselectAll));
-			arr.push(printMsg(message.id, t, timezone));
-			arr.push(setMsgFlag([message.id], message.flagged, t, dispatch));
-			arr.push(redirectMsg(message.id, t, dispatch, createSnackbar, createModal, ContactInput));
-			arr.push(editAsNewMsg(message.id, folderId, t, replaceHistory));
-			arr.push(setMsgAsSpam([message.id], false, t, dispatch, replaceHistory));
-			arr.push(showOriginalMsg(message.id, t));
-		}
-		return arr;
-	}, [
-		message,
-		systemFolders,
-		t,
-		dispatch,
-		folderId,
-		replaceHistory,
-		timezone,
-		createSnackbar,
-		createModal,
-		ContactInput,
-		deselectAll
-	]);
+	const onIconClick = useCallback((ev: { stopPropagation: () => void }): void => {
+		ev.stopPropagation();
+		setOpen((o) => !o);
+	}, []);
+
+	const onDropdownClose = useCallback((): void => {
+		setOpen(false);
+	}, []);
+
+	const _maxWidth = useMemo(
+		() => (iconSize && maxActions ? `${iconSize * maxActions}px` : maxWidth),
+		[iconSize, maxActions, maxWidth]
+	);
+
+	const _minWidth = useMemo(
+		() => (folderId === FOLDERS.TRASH ? `${iconSize * maxActions}px` : theme.sizes.icon.large),
+		[folderId, iconSize, maxActions, theme?.sizes?.icon?.large]
+	);
+
+	useLayoutEffect(() => {
+		calculateVisibleActionsCount();
+	}, [calculateVisibleActionsCount]);
+
 	return (
-		<Row padding={{ left: 'small' }}>
-			{map(primaryActions, (action) => (
-				<Tooltip key={`${message.id}-${action.icon}`} label={action.label}>
-					<IconButton
-						size="small"
-						icon={action.icon}
-						onClick={(ev: React.MouseEvent<HTMLButtonElement>): void => {
-							if (ev) ev.preventDefault();
-							action.click();
-						}}
-					/>
-				</Tooltip>
-			))}
-			{secondaryActions.length > 0 && (
-				<Dropdown placement="right-end" items={secondaryActions}>
-					<IconButton size="small" icon="MoreVertical" />
+		<Row
+			ref={actionContainerRef}
+			mainAlignment={mainAlignment}
+			maxWidth={_maxWidth}
+			wrap="nowrap"
+			style={{
+				flexGrow: 1,
+				flexBasis: 'fit-content',
+				whiteSpace: 'nowrap',
+				overflow: 'hidden',
+				minWidth: _minWidth
+			}}
+		>
+			{firstActions?.length > 0 &&
+				map(firstActions, (action) => (
+					<Tooltip key={`${action.icon}`} label={action.label}>
+						<IconButton
+							size="small"
+							icon={action.icon}
+							onClick={(ev: React.MouseEvent<HTMLButtonElement>): void => {
+								if (ev) ev.preventDefault();
+								action.click();
+							}}
+						/>
+					</Tooltip>
+				))}
+			{secondActions?.length > 0 && (
+				<Dropdown items={secondActions} forceOpen={open} onClose={onDropdownClose}>
+					<IconButton size="small" icon="MoreVertical" onClick={onIconClick} />
 				</Dropdown>
 			)}
 		</Row>
