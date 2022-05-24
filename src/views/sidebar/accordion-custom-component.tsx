5073c91c2ebf7a80c2e3953672527f30ea04e8f0
Luca Stauble <luca.stauble@zextras.com>
2022-05-24 14:27:55 +0200

    chore: code cleanup

--- src/views/sidebar/accordion-custom-component.tsx
+++ src/views/sidebar/accordion-custom-component.tsx
@@ -537,9 +537,9 @@ export const AccordionCustomComponent: FC<{ item: AccordionFolder }> = ({ item }
 		return (
 			<Padding left="small">
 				<Tooltip placement="right" label={tooltipText}>
-					<div>
+					<Row>
 						<Icon icon="ArrowCircleRight" customColor="#ffb74d" size="large" />
-					</div>
+					</Row>
 				</Tooltip>
 			</Padding>
 		);
