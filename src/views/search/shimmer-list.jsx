83a71fe84b5dbe8712caffd74307ca674d5bbaad
Luca Stauble <luca.stauble@zextras.com>
2022-05-31 11:35:46 +0200

    feat: add optional delay to the ShimmerList

--- src/views/search/shimmer-list.jsx
+++ src/views/search/shimmer-list.jsx
@@ -3,17 +3,41 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { useMemo } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { Shimmer, Container } from '@zextras/carbonio-design-system';
 import { isNil, map, times } from 'lodash';
 import styled from 'styled-components';
 
-const ShimmerList = ({ count }) => (
-	<Container mainAlignment="start" height="fill" style={{ overflow: 'hidden' }}>
-		{times(33, (index) => (
-			<Shimmer.ListItem type={1} key={`${index}`} />
-		))}
-	</Container>
-);
+const ShimmerList = ({ count, delay = 0 }) => {
+	const [visible, setVisibility] = useState(delay > 0 ? 'hidden' : 'visible');
+	const timeoutRef = useRef();
+
+	// The returned callback is needed only if the timeout is really set
+	// eslint-disable-next-line consistent-return
+	useEffect(() => {
+		if (timeoutRef.current) {
+			clearTimeout(timeoutRef.current);
+		}
+
+		if (delay > 0) {
+			timeoutRef.current = setTimeout(() => setVisibility('visible'), delay);
+			return () => {
+				clearTimeout(timeoutRef.current);
+			};
+		}
+	}, [delay]);
+
+	return (
+		<Container
+			mainAlignment="start"
+			height="fill"
+			style={{ visibility: visible, overflow: 'hidden' }}
+		>
+			{times(33, (index) => (
+				<Shimmer.ListItem type={1} key={`${index}`} />
+			))}
+		</Container>
+	);
+};
 
 export default ShimmerList;
