8c7bf4ed0328079901086c4fa299a357285228bd
Luca Stauble <luca.stauble@zextras.com>
2022-06-01 14:14:41 +0200

    feat: added fade-in effect to the shimmer

--- src/views/search/shimmer-list.jsx
+++ src/views/search/shimmer-list.jsx
@@ -3,41 +3,37 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useLayoutEffect, useMemo, useRef, useState } from 'react';
 import { Shimmer, Container } from '@zextras/carbonio-design-system';
 import { isNil, map, times } from 'lodash';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const ShimmerList = ({ count, delay = 0 }) => {
-	const [visible, setVisibility] = useState(delay > 0 ? 'hidden' : 'visible');
-	const timeoutRef = useRef();
+const fadeIn = keyframes`
+	0% { opacity: 0 }
+  100% { opacity: 1 }
+`;
+
+const Wrapper = styled(Container)`
+	opacity: 0;
+	animation-name: ${fadeIn};
+	animation-timing-function: ease-in;
+	animation-fill-mode: forwards;
+	overflow: hidden;
+`;
+
+const ShimmerList = ({ count, delay = 0, transitionDuration = 400 }) => {
 	const itemCount = count > 33 ? 33 : count;
 
-	// The returned callback is needed only if the timeout is really set
-	// eslint-disable-next-line consistent-return
-	useEffect(() => {
-		if (timeoutRef.current) {
-			clearTimeout(timeoutRef.current);
-		}
-
-		if (delay > 0) {
-			timeoutRef.current = setTimeout(() => setVisibility('visible'), delay);
-			return () => {
-				clearTimeout(timeoutRef.current);
-			};
-		}
-	}, [delay]);
-
 	return (
-		<Container
+		<Wrapper
 			mainAlignment="start"
 			height="fill"
-			style={{ visibility: visible, overflow: 'hidden' }}
+			style={{ animationDelay: `${delay}ms`, animationDuration: `${transitionDuration}ms` }}
 		>
 			{times(itemCount, (index) => (
 				<Shimmer.ListItem type={1} key={`${index}`} />
 			))}
-		</Container>
+		</Wrapper>
 	);
 };
 
