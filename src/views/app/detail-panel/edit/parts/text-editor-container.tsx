fd857ed453a9cb9b219b176229b9e40d624390c5
Gammax92 <gottardifrancesco92@gmail.com>
2022-06-21 10:37:43 +0200

    fix: reply to msg is no longer cropped

--- src/views/app/detail-panel/edit/parts/text-editor-container.tsx
+++ src/views/app/detail-panel/edit/parts/text-editor-container.tsx
@@ -25,13 +25,14 @@ const TextEditorContainer: FC<PropType> = ({ onDragOverEvent, draftSavedAt }) =>
 		<>
 			{editor?.text && (
 				<Container
-					height="100%"
+					height="fit"
 					padding={{ all: 'small' }}
 					background="gray6"
 					crossAlignment="flex-end"
 				>
 					{editor?.richText && composerIsAvailable ? (
 						<Controller
+							height="fit"
 							name="text"
 							control={control}
 							defaultValue={editor?.text}
@@ -59,7 +60,7 @@ const TextEditorContainer: FC<PropType> = ({ onDragOverEvent, draftSavedAt }) =>
 							control={control}
 							defaultValue={editor?.text}
 							render={({ onChange, value }): ReactElement => (
-								<Container background="gray6" height="100%">
+								<Container background="gray6" height="fit">
 									<StyledComp.TextArea
 										value={value[0]}
 										onChange={(ev): void => {
