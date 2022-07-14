e038f1126065ca9bc21c870a3577d3989d264b58
Saiyan Abhishek <74703491+geeky-abhishek@users.noreply.github.com>
2022-07-14 16:39:30 +0530

    feat: added search filter to choose shared account

--- src/views/search/parts/folder-modal.tsx
+++ src/views/search/parts/folder-modal.tsx
@@ -1,49 +1,145 @@
 /*
- * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { FC, ReactElement, useCallback, useMemo, useState } from 'react';
-import { Container, CustomModal, Input, Text, Icon } from '@zextras/carbonio-design-system';
-import { TFunction } from 'i18next';
-import { filter, includes, isEmpty, reduce, remove, sortBy, startsWith } from 'lodash';
-import { useSelector } from 'react-redux';
-import { FOLDERS, useUserSettings } from '@zextras/carbonio-shell-ui';
 
+import React, { ChangeEvent, FC, ReactElement, useCallback, useMemo, useState } from 'react';
+import {
+	Container,
+	CustomModal,
+	Input,
+	Text,
+	Accordion,
+	AccordionItem,
+	Tooltip,
+	Avatar,
+	Icon,
+	Row,
+	Padding
+} from '@zextras/carbonio-design-system';
+import { TFunction } from 'i18next';
+import { filter, isEmpty, reduce, startsWith } from 'lodash';
+
+import {
+	AccordionFolder,
+	FOLDERS,
+	useFoldersAccordionByView,
+	useUserAccount
+} from '@zextras/carbonio-shell-ui';
+
+import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 import ModalFooter from '../../sidebar/commons/modal-footer';
 import { ModalHeader } from '../../sidebar/commons/modal-header';
-import FolderItem from '../../sidebar/commons/folder-item';
 import { selectFolders } from '../../../store/folders-slice';
 import { FolderType } from '../../../types';
-import {
-	getFolderIconColor,
-	getFolderIconName,
-	getFolderTranslatedName
-} from '../../sidebar/utils';
+import { getFolderIconColor, getFolderTranslatedName } from '../../sidebar/utils';
+import { FOLDER_VIEW } from '../../../constants';
+import AccordionCustomComponent from './folder-accordion-custom-comp';
 
 type ComponentProps = {
 	compProps: { open: boolean; onClose: () => void; setFolder: (arg: any) => void; t: TFunction };
 };
+const ContainerEl = styled(Container)`
+	overflow-y: auto;
+	display: block;
+`;
+const FittedRow = styled(Row)`
+	max-width: calc(100% - (2 * ${({ theme }): string => theme.sizes.padding.small}));
+	height: 48px;
+`;
+const getFolderOwner = (item: any): string => {
+	if (item.owner) return item.owner;
+	if (item.parent) {
+		return getFolderOwner(item.parent);
+	}
+	return item.name;
+};
 
+const CustomComponent: FC<{ item: AccordionFolder }> = ({ item }): ReactElement => (
+	<FittedRow>
+		<Padding horizontal="small">
+			<Avatar label={item.label} size="medium" />
+		</Padding>
+		<Tooltip label={item.label} placement="right" maxWidth="100%">
+			<AccordionItem item={item} />
+		</Tooltip>
+	</FittedRow>
+);
+
+type CustomComponent = {
+	CustomComponent: ReactElement;
+	divider: boolean;
+	background: string | undefined;
+	onClick: () => void;
+	items: Array<any>;
+};
 const FolderSelectModal: FC<ComponentProps> = ({ compProps }): ReactElement => {
 	const { open, onClose, setFolder, t } = compProps;
-	const folders = useSelector(selectFolders);
-	const settings = useUserSettings()?.prefs;
+
 	const [input, setInput] = useState('');
 	const [folderDestination, setFolderDestination] = useState<FolderType | any>({});
 
-	const [includeSpam, includeTrash, includeSharedFolders] = useMemo(
-		() => [
-			settings?.zimbraPrefIncludeSpamInSearch === 'TRUE',
-			settings?.zimbraPrefIncludeTrashInSearch === 'TRUE',
-			settings?.zimbraPrefIncludeSharedItemsInSearch === 'TRUE'
-		],
-		[settings]
+	const foldersFromStore = useSelector(selectFolders);
+
+	const getFolderAbsPath = useCallback(
+		(fid: string) => {
+			const path = foldersFromStore[fid]?.path;
+			return filter(path?.split('/'), (p) => p !== '').join('/');
+		},
+		[foldersFromStore]
+	);
+	const accountName = useUserAccount().name;
+	const [openIds, setOpenIds] = useState(['USER_ROOT']);
+	const additionalProps = (item: AccordionFolder): Record<string, any> => ({
+		onClick: (): void => {
+			setFolderDestination(item);
+		},
+		onOpen: () => setOpenIds((s: Array<string>) => (s.includes(item.id) ? s : [...s, item.id])),
+		onClose: () => setOpenIds((s: Array<string>) => s.filter((id: string) => id !== item.id)),
+		openIds
+	});
+
+	const accordions = useFoldersAccordionByView(
+		FOLDER_VIEW.message,
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
+		AccordionCustomComponent,
+		additionalProps
 	);
 
-	const filterFromInput = useMemo(
+	const requiredAcc = useMemo(() => {
+		const temp = reduce(
+			accordions,
+			(acc: Array<AccordionFolder & CustomComponent>, v) => {
+				acc.push({
+					id: v.id,
+					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+					// @ts-ignore
+					CustomComponent,
+					label:
+						v.id === FOLDERS.USER_ROOT
+							? accountName
+							: getFolderTranslatedName({ t, folderId: v.id, folderName: v.label }),
+					divider: true,
+					items: v.items,
+					background: folderDestination.id === v.id ? 'highlight' : undefined,
+					onClick: () => {
+						v.id !== FOLDERS.USER_ROOT && setFolderDestination(v);
+					}
+				});
+				return acc;
+			},
+			[]
+		);
+
+		return temp;
+	}, [accordions, accountName, folderDestination.id, t]);
+
+	const filteredFolders = useMemo(
 		() =>
-			filter(folders, (v) => {
+			filter(requiredAcc, (v) => {
 				if (isEmpty(v)) {
 					return false;
 				}
@@ -51,94 +147,37 @@ const FolderSelectModal: FC<ComponentProps> = ({ compProps }): ReactElement => {
 				const folderName = getFolderTranslatedName({
 					t,
 					folderId: v?.id,
-					folderName: v?.name
+					folderName: v?.label
 				})?.toLowerCase();
 
 				return startsWith(folderName, input.toLowerCase());
 			}),
-		[folders, input, t]
-	);
-	const foldersToFilterOut = useMemo(
-		() => [...(includeTrash ? [] : FOLDERS.TRASH), ...(includeSpam ? [] : FOLDERS.SPAM)],
-		[includeSpam, includeTrash]
-	);
-
-	const nestFilteredFolders: any = useCallback(
-		(items: Array<FolderType>, id: string, results: Array<FolderType>, getSharedFolder: boolean) =>
-			reduce(
-				filter(items, (item) =>
-					getSharedFolder
-						? item.parent === id && item.isSharedFolder
-						: item.parent === id && !includes(foldersToFilterOut, item.id) && !item.isSharedFolder
-				),
-				(acc: Array<FolderType>, item: any) => {
-					const match = filter(results, (result) => result.id === item.id);
-					if (match && match.length) {
-						return [
-							...acc,
-							{
-								...item,
-								label: getFolderTranslatedName({ t, folderId: item.id, folderName: item.name }),
-								icon: getFolderIconName(item),
-								iconCustomColor: getFolderIconColor(item),
-								items: nestFilteredFolders(items, item.id, results, getSharedFolder),
-								onClick: (): void => setFolderDestination(item),
-								open: !!input.length,
-								divider: true,
-								background: folderDestination.id === item.id ? 'highlight' : undefined
-							}
-						];
-					}
-					if (match && !match.length) {
-						return [...acc, ...nestFilteredFolders(items, item.id, results)];
-					}
-					return acc;
-				},
-				[]
-			),
-		[foldersToFilterOut, t, input.length, folderDestination.id]
-	);
-
-	const shareFolders = useMemo(
-		() => [
-			{
-				id: '1',
-				label: 'Shared Folders',
-				level: '0',
-				icon: 'Share',
-				divider: true,
-				items: nestFilteredFolders(folders, '1', filterFromInput, true),
-				background: folderDestination.id === '1' ? 'highlight' : undefined,
-				onClick: (): void => setFolderDestination({ id: '1' })
-			}
-		],
-		[filterFromInput, folderDestination.id, folders, nestFilteredFolders]
+		[input, requiredAcc, t]
 	);
 
 	const onConfirm = useCallback(() => {
+		const absoluteParent = getFolderOwner(folderDestination.folder);
+		const relativePath = getFolderAbsPath(folderDestination?.id);
+		const folderPath =
+			absoluteParent === 'USER_ROOT' ? relativePath : `${absoluteParent}/${relativePath}`;
+
 		setFolder([
 			{
-				label: `in:${folderDestination.name}`,
+				label: `in:${folderPath}`,
 				hasAvatar: true,
+				maxWidth: '200px',
 				isGeneric: false,
 				background: 'gray2',
 				avatarBackground: getFolderIconColor(folderDestination),
 				avatarIcon: 'FolderOutline',
 				isQueryFilter: true,
-				value: `in:"${folderDestination.name}"`
+				value: `in:"${folderPath}"`
 			}
 		]);
 		onClose();
-	}, [setFolder, folderDestination, onClose]);
+	}, [folderDestination, setFolder, getFolderAbsPath, onClose]);
 
 	const disabled = useMemo(() => isEmpty(folderDestination), [folderDestination]);
-
-	const accordionData = useMemo(() => {
-		const ownFolders = nestFilteredFolders(folders, '1', filterFromInput, false);
-		const trashFolder = remove(ownFolders, (c: any) => c.id === FOLDERS.TRASH);
-		const foldersWithTrash = sortBy(ownFolders, (item) => Number(item.id)).concat(trashFolder);
-		return includeSharedFolders ? foldersWithTrash.concat(shareFolders) : foldersWithTrash;
-	}, [filterFromInput, folders, includeSharedFolders, nestFilteredFolders, shareFolders]);
 
 	return (
 		<CustomModal open={open} onClose={onClose} maxHeight="90vh" size="medium">
@@ -176,10 +215,21 @@ const FolderSelectModal: FC<ComponentProps> = ({ compProps }): ReactElement => {
 							CustomIcon={({ hasFocus }: { hasFocus: boolean }): ReactElement => (
 								<Icon icon="FunnelOutline" size="large" color={hasFocus ? 'primary' : 'text'} />
 							)}
-							onChange={(e: any): void => setInput(e.target.value)}
+							onChange={(e: ChangeEvent<HTMLInputElement>): void => setInput(e.target.value)}
 						/>
 
-						<FolderItem folders={accordionData} />
+						<ContainerEl
+							orientation="vertical"
+							mainAlignment="flex-start"
+							minHeight="30vh"
+							maxHeight="60vh"
+						>
+							<Accordion
+								items={filteredFolders}
+								activeId={folderDestination?.id}
+								openIds={openIds}
+							/>
+						</ContainerEl>
 					</Container>
 					<ModalFooter
 						onConfirm={onConfirm}
