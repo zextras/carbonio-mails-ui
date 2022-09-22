371434247fea8327c5a54183c0fb2b353b7f0d79
gabriele <gabriele.marino@zextras.com>
2022-09-22 20:23:46 +0200

    feat: delete draft modal in board

--- src/views/app/detail-panel/edit/parts/nav-guard.tsx
+++ src/views/app/detail-panel/edit/parts/nav-guard.tsx
@@ -5,46 +5,115 @@
  */
 
 import React, { useEffect, useState, useMemo, FC, useCallback } from 'react';
+import { useDispatch } from 'react-redux';
 import { Prompt, useHistory } from 'react-router-dom';
-import { Modal, Padding, Text } from '@zextras/carbonio-design-system';
-import { t } from '@zextras/carbonio-shell-ui';
+import { Padding, Text, useModal } from '@zextras/carbonio-design-system';
+import { FOLDERS, t } from '@zextras/carbonio-shell-ui';
+import { StoreProvider } from '../../../../../store/redux';
+import { moveMsgToTrash } from '../../../../../ui-actions/message-actions';
 import ModalFooter from '../../../../sidebar/commons/modal-footer';
+import ModalHeader from '../../../../sidebar/commons/modal-header';
 
-export const RouteLeavingGuard: FC<{ when: boolean; onDeleteDraft: () => void }> = ({
-	when,
-	onDeleteDraft
-}) => {
+type DeleteDraftModalProps = {
+	ids: Array<string>;
+	onClose: () => void;
+	onConfirm: () => void;
+	onDelete: () => void;
+};
+
+export const DeleteDraftModal = ({
+	ids,
+	onClose,
+	onConfirm,
+	onDelete
+}: DeleteDraftModalProps): React.ReactElement => {
+	const dispatch = useDispatch();
+
+	const onCloseModal = useCallback(() => {
+		onClose?.();
+	}, [onClose]);
+
+	const onDraft = useCallback(() => {
+		onConfirm?.();
+		onClose?.();
+	}, [onClose, onConfirm]);
+
+	const onDeleteAction = useCallback(() => {
+		moveMsgToTrash({
+			ids,
+			dispatch,
+			folderId: FOLDERS.TRASH
+		})?.click();
+		onDelete?.();
+		onClose?.();
+	}, [dispatch, ids, onClose, onDelete]);
+
+	return (
+		<>
+			<ModalHeader title={t('label.before_you_leave', 'Before you leave')} onClose={onCloseModal} />
+			<Padding vertical="medium">
+				<Text>
+					{t('modal.delete_draft.message1', 'Do you want to keep this draft or delete it?')}
+				</Text>
+			</Padding>
+			<ModalFooter
+				secondaryBtnType={'outlined'}
+				onConfirm={onDraft}
+				label={t('label.keep_draft', 'Keep Draft')}
+				secondaryAction={onDeleteAction}
+				secondaryLabel={t('label.delete_draft', 'Delete Draft')}
+				secondaryColor="primary"
+				showDivider={false}
+				paddingTop="0"
+			/>
+		</>
+	);
+};
+
+export const RouteLeavingGuard: FC<{
+	when: boolean;
+	onDeleteDraft: () => void;
+	id: string | undefined;
+}> = ({ when, onDeleteDraft, id }) => {
 	const history = useHistory();
 	const lastLocationInitial = useMemo<string>(() => history.location.pathname, [history]);
-	const [modalVisible, setModalVisible] = useState(false);
 	const [lastLocation, setLastLocation] = useState(lastLocationInitial);
 	const [confirmedNavigation, setConfirmedNavigation] = useState(false);
+	const createModal = useModal();
 
 	const onDelete = useCallback(() => {
-		setModalVisible(false);
-		onDeleteDraft();
+		onDeleteDraft?.();
 		setConfirmedNavigation(true);
 	}, [onDeleteDraft]);
 
-	const onClose = useCallback(() => {
-		setModalVisible(false);
-	}, []);
-
 	const onDraft = useCallback(() => {
-		setModalVisible(false);
 		setConfirmedNavigation(true);
 	}, []);
 
 	const handleBlockedNavigation = useCallback(
 		(nextLocation) => {
 			if (!confirmedNavigation && nextLocation.pathname !== (lastLocation || lastLocationInitial)) {
-				setModalVisible(true);
+				const closeModal = createModal(
+					{
+						children: (
+							<StoreProvider>
+								<DeleteDraftModal
+									ids={[id ?? '']}
+									onDelete={onDelete}
+									onConfirm={onDraft}
+									onClose={(): void => closeModal()}
+								/>
+							</StoreProvider>
+						)
+					},
+					true
+				);
 				setLastLocation(nextLocation?.pathname);
 				return false;
 			}
 			return true;
 		},
-		[confirmedNavigation, lastLocation, lastLocationInitial]
+		[confirmedNavigation, createModal, id, lastLocation, lastLocationInitial, onDelete, onDraft]
 	);
 	useEffect(() => {
 		if (confirmedNavigation && lastLocation) {
@@ -53,34 +122,5 @@ export const RouteLeavingGuard: FC<{ when: boolean; onDeleteDraft: () => void }>
 		}
 	}, [confirmedNavigation, history, lastLocation]);
 
-	return (
-		<>
-			<Prompt when={when} message={handleBlockedNavigation} />
-			{/* Your own alert/dialog/modal component */}
-			<Modal
-				open={modalVisible}
-				title={t('label.before_you_leave', 'Before you leave')}
-				showCloseIcon
-				onClose={onClose}
-				customFooter={
-					<ModalFooter
-						secondaryBtnType={'outlined'}
-						onConfirm={onDraft}
-						label={t('label.keep_draft', 'Keep Draft')}
-						secondaryAction={onDelete}
-						secondaryLabel={t('label.delete_draft', 'Delete Draft')}
-						secondaryColor="primary"
-						showDivider={false}
-						paddingTop="0"
-					/>
-				}
-			>
-				<Padding vertical="medium">
-					<Text>
-						{t('modal.delete_draft.message1', 'Do you want to keep this draft or delete it?')}
-					</Text>
-				</Padding>
-			</Modal>
-		</>
-	);
+	return <Prompt when={when} message={handleBlockedNavigation} />;
 };
