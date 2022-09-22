688fb5d6069c4b137998ac9553b16e069160374a
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2022-09-22 21:01:23 +0200

    chore: added support for ppt and pptx

--- src/views/app/detail-panel/preview/attachments-block.tsx
+++ src/views/app/detail-panel/preview/attachments-block.tsx
@@ -82,14 +82,15 @@ function getAttachmentsLink(
 			[
 				'text/csv',
 				'text/plain',
-				'application/vnd.ms-excel',
 				'application/msword',
-				'application/microsoft-word',
+				'application/vnd.ms-excel',
+				'application/vnd.ms-powerpoint',
+				'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+				'application/vnd.openxmlformats-officedocument.presentationml.presentation',
 				'application/vnd.oasis.opendocument.spreadsheet',
 				'application/vnd.oasis.opendocument.presentation',
-				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-				'application/vnd.oasis.opendocument.text',
-				'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+				'application/vnd.oasis.opendocument.text'
 			],
 			attachmentType
 		)
--- src/views/app/detail-panel/preview/file-preview.tsx
+++ src/views/app/detail-panel/preview/file-preview.tsx
@@ -25,14 +25,15 @@ export const previewType = (contentType: string): 'image' | 'pdf' | undefined =>
 			[
 				'text/csv',
 				'text/plain',
-				'application/vnd.ms-excel',
 				'application/msword',
-				'application/microsoft-word',
+				'application/vnd.ms-excel',
+				'application/vnd.ms-powerpoint',
+				'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+				'application/vnd.openxmlformats-officedocument.presentationml.presentation',
 				'application/vnd.oasis.opendocument.spreadsheet',
 				'application/vnd.oasis.opendocument.presentation',
-				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-				'application/vnd.oasis.opendocument.text',
-				'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+				'application/vnd.oasis.opendocument.text'
 			],
 			contentType
 		)
