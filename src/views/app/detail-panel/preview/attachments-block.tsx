bbe30d2c44ca4a52e3e92e5c584a7a79b21e5416
Giuliano <75381134+giuliano176@users.noreply.github.com>
2022-09-27 09:13:45 +0200

    support docx files in previewer (#191)
    * fix: added previewer support for .docx, .ppt and .pptx files

--- src/views/app/detail-panel/preview/attachments-block.tsx
+++ src/views/app/detail-panel/preview/attachments-block.tsx
@@ -84,11 +84,14 @@ function getAttachmentsLink(
 			[
 				'text/csv',
 				'text/plain',
-				'application/vnd.ms-excel',
 				'application/msword',
+				'application/vnd.ms-excel',
+				'application/vnd.ms-powerpoint',
+				'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+				'application/vnd.openxmlformats-officedocument.presentationml.presentation',
 				'application/vnd.oasis.opendocument.spreadsheet',
 				'application/vnd.oasis.opendocument.presentation',
-				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
 				'application/vnd.oasis.opendocument.text'
 			],
 			attachmentType
