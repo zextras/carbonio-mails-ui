6c50377665f5f72c134d6823a634b42cd8301cf9
Luca Stauble <luca.stauble@zextras.com>
2022-12-12 16:58:28 +0100

    chore: merge from devel

--- jest-setup.ts
+++ jest-setup.ts
@@ -17,7 +17,8 @@ import { registerRestHandler } from './src/carbonio-ui-commons/test/mocks/networ
 import { handleGetMsgRequest } from './src/tests/mocks/network/msw/handle-get-msg';
 
 failOnConsole({
-	...getFailOnConsoleDefaultConfig()
+	...getFailOnConsoleDefaultConfig(),
+	shouldFailOnWarn: false
 });
 
 beforeAll(() => {
