b5ab7243205b5a6ca65bdd824850b5952558cfb8
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2022-12-20 11:51:53 +0530

    chore: added missing notice header

--- src/helpers/signatures.ts
+++ src/helpers/signatures.ts
@@ -1,3 +1,8 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
 import { Account } from '@zextras/carbonio-shell-ui';
 import { find, map } from 'lodash';
 import { SignatureDescriptor } from '../types/signatures';
