1afe3337b7901bd334a2a9b71b3dbac2e3ef6a39
Luca Stauble <luca.stauble@zextras.com>
2022-12-22 17:27:04 +0100

    chore: code refactor and comments

--- src/views/app/detail-panel/preview/mail-preview.tsx
+++ src/views/app/detail-panel/preview/mail-preview.tsx
@@ -108,8 +108,27 @@ const MailContent: FC<{ message: MailMessage; isMailPreviewOpen: boolean }> = ({
 		[loggedInUser, message]
 	);
 
-	const collapsedContent = useMemo(
-		() => (
+	const collapsedContent = useMemo(() => {
+		/*
+		 * Compose the invite ID
+		 * The invite ID is composed by the following fields:
+		 * - the appointment ID (if present)
+		 * - the message ID
+		 * If the 2 fields are both present they will be separated by a hyphen otherwise only the message ID will be used
+		 *
+		 * The appointment ID is present only if the appointment was automatically added to the calendar (following the
+		 * user's preferences)
+		 */
+		const inviteId = message.invite[0].comp[0].apptId
+			? `${message.invite[0].comp[0].apptId}-${message.id}`
+			: message.id;
+
+		// Compose the participation status
+		const participationStatus = message.invite[0].replies
+			? message.invite[0].replies[0].reply[0].ptst
+			: '';
+
+		return (
 			<Container
 				data-testid="MessageBody"
 				width="100%"
@@ -129,14 +148,8 @@ const MailContent: FC<{ message: MailMessage; isMailPreviewOpen: boolean }> = ({
 								// @ts-ignore
 								onLoadChange={(): null => null}
 								mailMsg={message}
-								inviteId={
-									message.invite[0].comp[0].apptId
-										? `${message.invite[0].comp[0].apptId}-${message.id}`
-										: message.id
-								}
-								participationStatus={
-									message.invite[0].replies ? message.invite[0].replies[0].reply[0].ptst : ''
-								}
+								inviteId={inviteId}
+								participationStatus={participationStatus}
 								to={filter(message.participants, { type: 'f' })}
 								invite={message.invite}
 								method={message.invite[0]?.comp[0].method}
@@ -166,19 +179,18 @@ const MailContent: FC<{ message: MailMessage; isMailPreviewOpen: boolean }> = ({
 					readReceiptSetting={readReceiptSetting}
 				/>
 			</Container>
-		),
-		[
-			message,
-			showAppointmentInvite,
-			readReceiptSetting,
-			InviteResponse,
-			moveToTrash,
-			isAttendee,
-			showShareInvite,
-			showReadReceiptModal,
-			onModalClose
-		]
-	);
+		);
+	}, [
+		message,
+		showAppointmentInvite,
+		readReceiptSetting,
+		InviteResponse,
+		moveToTrash,
+		isAttendee,
+		showShareInvite,
+		showReadReceiptModal,
+		onModalClose
+	]);
 	return (
 		<Collapse
 			open={isMailPreviewOpen}
