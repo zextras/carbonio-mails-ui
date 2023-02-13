bc934bb42f9da376dcaafd61513a033f87f43c46
Luca Stauble <luca.stauble@zextras.com>
2023-02-13 15:59:49 +0100

    chore: progress save

---
+++ src/helpers/tests/identities.test.ts
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+describe('Identities selection', () => {
+	describe('Primary account', () => {});
+});
