9b324ca292ddc830a483e19b6c94b14dc5cae370
Luca Stauble <luca.stauble@zextras.com>
2023-05-22 15:37:58 +0200

    fix: fix move modal title when a single message is about to being moved

--- src/ui-actions/move-conv-msg.jsx
+++ src/ui-actions/move-conv-msg.jsx
@@ -172,10 +172,12 @@ const MoveConvMessage = ({
 			if (isRestore) {
 				return t('label.restore', 'Restore');
 			}
-			return t('folder_panel.modal.move.title_modal', 'Move Conversation');
+			return isMessageView
+				? t('folder_panel.modal.move.title_modal_message', 'Move message')
+				: t('folder_panel.modal.move.title_modal_conversation', 'Move Conversation');
 		}
 		return t('folder_panel.modal.new.title', 'Create a new folder');
-	}, [isRestore, moveConvModal]);
+	}, [isMessageView, isRestore, moveConvModal]);
 
 	const footerConfirm = useMemo(() => {
 		if (moveConvModal) {
