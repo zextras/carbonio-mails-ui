efdf58d38b30e4116c60c49ddb602ecbd92841b1
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2023-08-22 11:50:25 +0200

    WIP: added warning missing subject or attachment

--- src/views/app/detail-panel/edit/edit-view-v2.tsx
+++ src/views/app/detail-panel/edit/edit-view-v2.tsx
@@ -26,7 +26,8 @@ import {
 	Icon,
 	Input,
 	Text,
-	Padding
+	Padding,
+	useModal
 } from '@zextras/carbonio-design-system';
 import { addBoard, t } from '@zextras/carbonio-shell-ui';
 import { filter, map, noop } from 'lodash';
@@ -44,6 +45,7 @@ import { RecipientsRows } from './parts/recipients-rows';
 import { TextEditorContainer, TextEditorContent } from './parts/text-editor-container-v2';
 import WarningBanner from './parts/warning-banner';
 import { GapContainer, GapRow } from '../../../../commons/gap-container';
+import { LineType } from '../../../../commons/utils';
 import { EditViewActions, MAILS_ROUTE, TIMEOUTS } from '../../../../constants';
 import {
 	getAvailableAddresses,
@@ -52,6 +54,7 @@ import {
 	IdentityDescriptor
 } from '../../../../helpers/identities';
 import { getMailBodyWithSignature } from '../../../../helpers/signatures';
+import { StoreProvider } from '../../../../store/redux';
 import {
 	useEditorAutoSendTime,
 	useEditorDraftSave,
@@ -66,7 +69,49 @@ import {
 	useEditorText,
 	useEditorAttachmentFiles
 } from '../../../../store/zustand/editor';
-import { BoardContext, EditorRecipients } from '../../../../types';
+import {
+	BoardContext,
+	EditorAttachmentFiles,
+	EditorRecipients,
+	MailsEditorV2
+} from '../../../../types';
+
+const attachmentWords: Array<string> = [
+	t('messages.modal.send_anyway.attach', 'attach'),
+	t('messages.modal.send_anyway.attachment', 'attachment'),
+	t('messages.modal.send_anyway.attachments', 'attachments'),
+	t('messages.modal.send_anyway.attached', 'attached'),
+	t('messages.modal.send_anyway.attaching', 'attaching'),
+	t('messages.modal.send_anyway.enclose', 'enclose'),
+	t('messages.modal.send_anyway.enclosed', 'enclosed'),
+	t('messages.modal.send_anyway.enclosing', 'enclosing')
+];
+
+function getSubjectOrAttachmentError({
+	attachmentIsExpected,
+	attachmentFiles,
+	subject
+}: {
+	attachmentIsExpected: boolean;
+	attachmentFiles: Array<EditorAttachmentFiles>;
+	subject: MailsEditorV2['subject'];
+}): string {
+	const attachmentIsPresent = attachmentFiles.length > 0;
+	const attachmentIsMissing = attachmentIsExpected && !attachmentIsPresent;
+	if (attachmentIsMissing && !subject) {
+		return t(
+			'messages.modal.send_anyway.no_subject_no_attachments',
+			'Email subject is empty and you didn’t attach any files.'
+		);
+	}
+	if (!subject) {
+		return t('messages.modal.send_anyway.subject', 'Subject is missing');
+	}
+	if (attachmentIsMissing) {
+		return t('messages.modal.send_anyway.no_attachments', 'You didn’t attach any files.');
+	}
+	return '';
+}
 
 export type EditViewProp = {
 	editorId: string;
@@ -102,7 +147,6 @@ export const EditView: FC<EditViewProp> = ({
 	const createSnackbar = useSnackbar();
 	const [dropZoneEnabled, setDropZoneEnabled] = useState<boolean>(false);
 	const attachmentFiles = useEditorAttachmentFiles({ id: editorId });
-
 	// Performs cleanups and invoke the external callback
 	const close = useCallback(() => {
 		closeController && closeController();
@@ -174,14 +218,54 @@ export const EditView: FC<EditViewProp> = ({
 		[close, saveDraft, setAutoSendTime]
 	);
 
+	const createModal = useModal();
 	const onSendClick = useCallback((): void => {
-		// TODO invoke pre-send (missing attachments and subject) checks
-		sendMessage({
-			onCountdownTick: onSendCountdownTick,
-			onComplete: onSendComplete,
-			onError: onSendError
+		const attachmentIsExpected = attachmentWords.some((el) => {
+			const [msgContent] = text.richText
+				? text.richText.split(LineType.HTML_SEP_ID)
+				: text.plainText.split(LineType.PLAINTEXT_SEP);
+			return msgContent.toLowerCase().includes(el);
 		});
-		close();
+		if ((attachmentIsExpected && !attachmentFiles.length) || !subject) {
+			const closeModal = createModal({
+				title: t('header.attention', 'Attention'),
+				confirmLabel: t('action.ok', 'Ok'),
+				dismissLabel: t('label.cancel', 'Cancel'),
+				showCloseIcon: true,
+				onConfirm: () => {
+					sendMessage({
+						onCountdownTick: onSendCountdownTick,
+						onComplete: onSendComplete,
+						onError: onSendError
+					});
+					close();
+					closeModal();
+				},
+				onClose: () => {
+					closeModal();
+				},
+				onSecondaryAction: () => {
+					closeModal();
+				},
+				children: (
+					<StoreProvider>
+						<Text overflow="break-word" style={{ paddingTop: '1rem' }}>
+							{getSubjectOrAttachmentError({ attachmentIsExpected, attachmentFiles, subject })}
+						</Text>
+						<Text overflow="break-word" style={{ paddingBottom: '1rem' }}>
+							{t('messages.modal.send_anyway.second', 'Do you still want to send the email?')}
+						</Text>
+					</StoreProvider>
+				)
+			});
+		} else {
+			sendMessage({
+				onCountdownTick: onSendCountdownTick,
+				onComplete: onSendComplete,
+				onError: onSendError
+			});
+			close();
+		}
 	}, [close, onSendComplete, onSendCountdownTick, onSendError, sendMessage]);
 
 	const onIdentityChanged = useCallback(
