bf3fc045aac45e1baaee044f64e40f7789cc7a82
Luca Stauble <luca.stauble@zextras.com>
2024-03-12 16:43:34 +0100

    build(deps): progress save

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,8 +1,8 @@
 /*
- * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-module.exports = {
+export default {
 	extends: ['@commitlint/config-conventional']
 };
