78e9554da9867569abd3a4291cf4ccc9aa34abaf
Luca Stauble <luca.stauble@zextras.com>
2024-03-19 14:51:02 +0100

    chore(config): update test environment configuration

--- jest.config.ts
+++ jest.config.ts
@@ -19,23 +19,7 @@ const config: Config = {
 		'\\.(css|less)$': '<rootDir>/__mocks__/fileMock.js'
 	},
 	collectCoverage: false,
-	testTimeout: 20000,
-
-	// The test environment that will be used for testing
-	/**
-	 * @note Override test environment to set again Request, Response, TextEncoder and other
-	 * fields
-	 * @see https://mswjs.io/docs/migrations/1.x-to-2.x#requestresponsetextencoder-is-not-defined-jest
-	 * @see https://github.com/mswjs/msw/issues/1916#issuecomment-1919965699
-	 */
-	testEnvironment: '<rootDir>/src/carbonio-ui-commons/test/jsdom-extended.ts',
-
-	/**
-	 * @see https://mswjs.io/docs/migrations/1.x-to-2.x#cannot-find-module-mswnode-jsdom
-	 */
-	testEnvironmentOptions: {
-		customExportConditions: ['']
-	}
+	testTimeout: 20000
 };
 
 export default config;
