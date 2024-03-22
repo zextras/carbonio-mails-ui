47fd2c6d67b2e0b178460b923c7581af950b1fcb
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-03-22 11:36:30 +0100

    test: added test for generateSmartLinkHtml function

--- src/ui-actions/tests/utils.test.ts
+++ src/ui-actions/tests/utils.test.ts
@@ -4,8 +4,11 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import { setupEditorStore } from '../../tests/generators/editor-store';
+import { generateEditorV2Case } from '../../tests/generators/editors';
+import { generateStore } from '../../tests/generators/store';
 import { MessageAction } from '../../types';
-import { findMessageActionById } from '../utils';
+import { findMessageActionById, generateSmartLinkHtml } from '../utils';
 
 describe('findMessageActionById', () => {
 	test('returns undefined if an empty actions array is passed', () => {
@@ -95,5 +98,45 @@ describe('findMessageActionById', () => {
 		const actions: Array<MessageAction> = [action1, action2, action3, action4, action5];
 		expect(findMessageActionById(actions, 'dummy-action-2')).toBe(action2);
 		expect(findMessageActionById(actions, 'dummy-action-4')).not.toBe(action2);
+	});
+});
+
+describe('generateSmartLinkHtml', () => {
+	it('generates correct HTML for smart link with attachment filename', async () => {
+		const editor = await generateEditorV2Case(1, generateStore().dispatch);
+		setupEditorStore({ editors: [editor] });
+
+		const smartLink = { publicUrl: 'https://example.com/file' };
+		const index = 0;
+		const result = generateSmartLinkHtml({
+			smartLink,
+			attachments: editor.savedAttachments,
+			index
+		});
+		const expectedFileName = editor.savedAttachments[index].filename;
+		const expectedUrl = `href='${smartLink.publicUrl}'`;
+		expect(result).toContain(expectedUrl);
+		expect(result).toContain(expectedFileName);
+	});
+
+	it('falls back to publicUrl when filename is undefined', async () => {
+		const editor = await generateEditorV2Case(1, generateStore().dispatch);
+		setupEditorStore({ editors: [editor] });
+
+		const smartLink = { publicUrl: 'https://example.com/file' };
+		const index = 0;
+		const attachmentsWithoutFileName = [{ ...editor.savedAttachments[index], filename: undefined }];
+		const result = generateSmartLinkHtml({
+			smartLink,
+			// disable typescript to check the fallback
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			attachments: attachmentsWithoutFileName,
+			index
+		});
+		const expectedUrl = `href='${smartLink.publicUrl}'`;
+		const expectedFileName = `download>${smartLink.publicUrl}</a>`;
+		expect(result).toContain(expectedUrl);
+		expect(result).toContain(expectedFileName);
 	});
 });
