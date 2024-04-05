156e7bdb08357451a67c631212210ce76fd4aa7c
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2024-04-05 15:02:26 +0530

    fix: search settings are ignored by search

--- src/views/search/search-view.tsx
+++ src/views/search/search-view.tsx
@@ -35,6 +35,7 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 	const [searchDisabled, setSearchDisabled] = useDisableSearch();
 	const settings = useUserSettings();
 	const isMessageView = settings.prefs.zimbraPrefGroupMailBy === 'message';
+	const includeSharedItemsInSearch = settings.prefs.zimbraPrefIncludeSharedItemsInSearch === 'TRUE';
 	const folders = useFoldersMap();
 	const [count, setCount] = useState(0);
 
@@ -71,7 +72,9 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 	const [loading, setLoading] = useState(false);
 	const [filterCount, setFilterCount] = useState(0);
 	const [showAdvanceFilters, setShowAdvanceFilters] = useState(false);
-	const [isSharedFolderIncluded, setIsSharedFolderIncluded] = useState(true);
+	const [isSharedFolderIncluded, setIsSharedFolderIncluded] = useState<boolean>(
+		includeSharedItemsInSearch
+	);
 	const [isInvalidQuery, setIsInvalidQuery] = useState<boolean>(false);
 	const searchResults = useAppSelector(selectSearches);
 
@@ -275,7 +278,7 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 				// @ts-ignore
 				query={query}
 				updateQuery={updateQuery}
-				isSharedFolderIncluded={isSharedFolderIncluded}
+				isSharedFolderIncluded={includeSharedItemsInSearch}
 				setIsSharedFolderIncluded={setIsSharedFolderIncluded}
 				open={showAdvanceFilters}
 				onClose={(): void => setShowAdvanceFilters(false)}
