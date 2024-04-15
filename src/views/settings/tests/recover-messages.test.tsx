f05d3b9824bb88aa9ef9f23abb00c0cb9fa15290
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-04-15 11:31:33 +0200

    test: fix recover messages test

--- src/views/settings/tests/recover-messages.test.tsx
+++ src/views/settings/tests/recover-messages.test.tsx
@@ -8,7 +8,7 @@ import React from 'react';
 
 import { screen } from '@testing-library/react';
 import moment from 'moment';
-import { HttpResponse, http } from 'msw';
+import { http } from 'msw';
 
 import { getSetupServer } from '../../../carbonio-ui-commons/test/jest-setup';
 import { setupTest } from '../../../carbonio-ui-commons/test/test-setup';
@@ -16,10 +16,10 @@ import { useProductFlavorStore } from '../../../store/zustand/product-flavor/sto
 import { RecoverMessages } from '../recover-messages';
 
 function getParams(url: string): Record<string, string> {
-	const params = new URLSearchParams(new URL(url).searchParams);
+	const params = [...new URLSearchParams(new URL(url).searchParams)];
 	const rec: Record<string, string> = {};
-	params.forEach(([k, v]) => {
-		rec[k] = v;
+	params.forEach(([key, value]) => {
+		rec[key] = value;
 	});
 	return rec;
 }
@@ -40,17 +40,24 @@ describe('Recover messages', () => {
 		const { user } = setupTest(<RecoverMessages />, {});
 		await user.click(screen.getByRole('button', { name: 'label.start_recovery' }));
 
-		getSetupServer().use(
-			http.post('/zx/backup/v1/undelete', ({ request }) => {
-				const params = getParams(request.url);
-				const { start } = params;
-				const { end } = params;
-				expect(start).toBeDefined();
-				expect(end).toBeDefined();
-				const oneWeek = moment(end).utc().diff(moment(start).utc());
-				expect(oneWeek).toBe(60 * 1000 * 60 * 24 * 7);
-				return HttpResponse.json({});
-			})
-		);
+		const APIrequestStartEnd = (): Promise<{ start: string; end: string }> =>
+			new Promise((resolve, reject) => {
+				getSetupServer().use(
+					http.post('/zx/backup/v1/undelete', async ({ request }) => {
+						if (request === undefined) {
+							reject(new Error('Empty request'));
+						}
+						const params = getParams(request.url);
+						const { start } = params;
+						const { end } = params;
+						resolve({ start, end });
+					})
+				);
+			});
+		const { start, end } = await APIrequestStartEnd();
+		expect(start).toBeDefined();
+		expect(end).toBeDefined();
+		const oneWeek = moment(end).utc().diff(moment(start).utc());
+		expect(oneWeek).toBe(60 * 1000 * 60 * 24 * 7);
 	});
 });
