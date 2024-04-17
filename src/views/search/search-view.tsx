8c3735d56baeef5bdb7b6c2b4547df440160445d
Manuel Garbin <manuel.garbin@studiostorti.com>
2024-04-17 10:49:18 +0200

    fix: temp fix for search view and message view
    Reset Conversation/Message slice in Mail App or Search store in Search view
    only if user move from mail to search or viceversa

--- src/views/search/search-view.tsx
+++ src/views/search/search-view.tsx
@@ -29,6 +29,7 @@ import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import { search } from '../../store/actions/search';
 import { resetSearchResults, selectSearches } from '../../store/searches-slice';
 import type { SearchProps } from '../../types';
+import { isSharedAccountFolder } from '../../helpers/folders';
 
 const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader }) => {
 	const [query, updateQuery] = useQuery();
@@ -38,6 +39,8 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 	const folders = useFoldersMap();
 	const [count, setCount] = useState(0);
 	const [containerWidth, setContainerWidth] = useState('70%');
+	const dispatch = useAppDispatch();
+	const [newSearch, setNewSearch] = useState(true);
 
 	useEffect(() => {
 		const element = document.getElementById("appContainerSearch");
@@ -61,15 +64,21 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 	);
 
 	useEffect(() => {
+		const lastApp = localStorage.getItem('lastApp');
+		if ( lastApp == "mail" || lastApp == "" ) {
+			localStorage.setItem('lastApp','search');
+			dispatch(resetSearchResults());
+			setNewSearch(true);
+		}
 		setAppContext({ isMessageView, count, setCount });
-	}, [count, isMessageView]);
+	}, [count, isMessageView, dispatch]);
 
 	const searchInFolders = useMemo(
 		() =>
 			reduce(
 				folders,
 				(acc: Array<string>, v: Folder, k: string) => {
-					if (('isShared' in v && v.isShared) || v.perm) {
+					if (isSharedAccountFolder(v.id) || v.perm) {
 						acc.push(k);
 					}
 					return acc;
@@ -84,7 +93,6 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 		[searchInFolders]
 	);
 
-	const dispatch = useAppDispatch();
 	const [loading, setLoading] = useState(false);
 	const [filterCount, setFilterCount] = useState(0);
 	const [showAdvanceFilters, setShowAdvanceFilters] = useState(false);
@@ -105,11 +113,11 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 		if (searchResults.status === 'fulfilled') {
 			return t('label.results_for', 'Results for: ');
 		}
-		if (searchResults.status === 'pending') {
+		if (searchResults.status === 'pending' && loading) {
 			return t('label.loading_results', 'Loading Results...');
 		}
 		return '';
-	}, [isInvalidQuery, searchResults.status, invalidQueryTooltip]);
+	}, [isInvalidQuery, searchResults.status, invalidQueryTooltip, loading]);
 
 	const queryToString = useMemo(
 		() =>
@@ -132,8 +140,6 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 					locale: prefLocale
 				})
 			);
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
 			if (search.rejected.match(resultAction)) {
 				if (
 					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
@@ -209,16 +215,29 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 	useEffect(() => {
 		if (searchResults.status === 'pending') {
 			setLoading(true);
+		} else {
+			setLoading(false);
 		}
-		setLoading(false);
 	}, [searchResults.status]);
 
 	useEffect(() => {
-		if (query && query.length > 0 && !isInvalidQuery) {
+		if (query && query.length > 0 && !newSearch && searchResults.query !== '' && queryToString !== searchResults.query) {
+			setNewSearch(true);
+			setFilterCount(0);
+			setIsInvalidQuery(false);
+			dispatch(resetSearchResults());
+			replaceHistory({
+				path: MAILS_ROUTE,
+				route: SEARCH_APP_ID
+			});
+		}
+		if (query && query.length > 0 && !isInvalidQuery && newSearch && queryToString !== searchResults.query) {
 			setFilterCount(query.length);
 			searchQuery(queryToString, true);
+			setNewSearch(false);
 		}
 		if (query && query.length === 0) {
+			setNewSearch(true);
 			setFilterCount(0);
 			setIsInvalidQuery(false);
 			dispatch(resetSearchResults());
@@ -233,6 +252,7 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 		isInvalidQuery,
 		searchQuery,
 		searchResults.query,
+		newSearch,
 		queryToString,
 		dispatch
 	]);
