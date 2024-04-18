1b0061b5a1a33cfd6874cab7f4dc92cab346c6d1
Alessio Coser <18616553+AlessioCoser@users.noreply.github.com>
2024-04-18 14:41:19 +0200

    test: complete the test of applyProductFlavourAPI

--- src/api/tests/apply-product-flavour.test.ts
+++ src/api/tests/apply-product-flavour.test.ts
@@ -6,6 +6,7 @@
 import { HttpResponse } from 'msw';
 import { createAPIInterceptor } from '../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
 import { applyProductFlavourAPI } from '../apply-product-flavour';
+import { useProductFlavorStore } from '../../store/zustand/product-flavor/store';
 
 describe('applyProductFlavourAPI', () => {
 	describe('when supported api is not present', () => {
@@ -16,6 +17,12 @@ describe('applyProductFlavourAPI', () => {
 		it('should return the community flavour by default', async () => {
 			expect(await applyProductFlavourAPI()).toBe('community');
 		});
+
+		it('the store should have the community flavour by default', async () => {
+			await applyProductFlavourAPI();
+
+			expect(useProductFlavorStore.getState().productFlavor).toBe('community');
+		});
 	});
 
 	describe('when advanced is not installed', () => {
@@ -23,15 +30,30 @@ describe('applyProductFlavourAPI', () => {
 			createAPIInterceptor('get', '/zx/auth/supported', HttpResponse.json(null, { status: 500 }));
 		});
 
-		it('should return the community flavour by default', async () => {
+		it('should return the community flavour', async () => {
 			expect(await applyProductFlavourAPI()).toBe('community');
 		});
 
-		it.skip('the store should have the community flavour by default', () => {});
+		it('the store should have the community flavour', async () => {
+			await applyProductFlavourAPI();
+
+			expect(useProductFlavorStore.getState().productFlavor).toBe('community');
+		});
 	});
 
 	describe('when advanced is installed', () => {
-		it.skip('should return the advance flavour', () => {});
-		it.skip('should set the advance flavour into the store', () => {});
+		beforeAll(() => {
+			createAPIInterceptor('get', '/zx/auth/supported', HttpResponse.json(null, { status: 200 }));
+		});
+
+		it('should return the advance flavour', async () => {
+			expect(await applyProductFlavourAPI()).toBe('advanced');
+		});
+
+		it('should set the advance flavour into the store', async () => {
+			await applyProductFlavourAPI();
+
+			expect(useProductFlavorStore.getState().productFlavor).toBe('advanced');
+		});
 	});
 });
