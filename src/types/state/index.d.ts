2373ad358ce1168e37c3359c5546871451f0209d
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-05-28 14:56:59 +0200

    chore: change searches state type definition

--- src/types/state/index.d.ts
+++ src/types/state/index.d.ts
@@ -109,7 +109,7 @@ export type SearchesStateType = {
 	searchResults: any;
 	searchResultsIds: Array<string>;
 	conversations?: Record<string, Conversation>;
-	messages?: Record<string, Partial<MailMessage> & Pick<MailMessage, 'id', 'parent'>>;
+	messages: Record<string, Partial<MailMessage> & Pick<MailMessage, 'id', 'parent'>>;
 	more: boolean;
 	offset: number;
 	sortBy?: SortBy;
