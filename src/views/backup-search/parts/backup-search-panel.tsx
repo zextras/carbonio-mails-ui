2a953c7dd39226d4982ab0997d438c730a577d7c
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-06-03 17:39:03 +0200

    fix: add backup message preview panel

--- src/views/backup-search/parts/backup-search-panel.tsx
+++ src/views/backup-search/parts/backup-search-panel.tsx
@@ -3,11 +3,15 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import React, { useMemo } from 'react';
+import React from 'react';
 
 import { Container, Icon, Padding, Text } from '@zextras/carbonio-design-system';
 import { t } from '@zextras/carbonio-shell-ui';
 import styled from 'styled-components';
+
+import { BackupSearchPanelTextLine } from './backup-search-panel-text-line';
+import { getFolder } from '../../../carbonio-ui-commons/store/zustand/folder';
+import { useBackupSearchStore } from '../../../store/zustand/backup-search/store';
 
 const SearchIcon = styled(Icon)`
 	width: 3.3rem;
@@ -19,7 +23,6 @@ export const BackupSearchPanel = ({
 }: {
 	itemId: string | undefined;
 }): React.JSX.Element => {
-	const displayerTitle = useMemo(() => itemId, [itemId]);
 	const restoreEmailsTitle = t(
 		'label.displayer_restore_emails_title',
 		'Select the e-mails you want to restore'
@@ -28,19 +31,31 @@ export const BackupSearchPanel = ({
 		'label.displayer_restore_emails_description',
 		'Once the selected email recovery is complete, \n you will receive an email with information regarding the results.'
 	);
+
+	const message = useBackupSearchStore((state) => state.messages[itemId ?? '']);
+	const subject = { title: t('label.subject', 'Subject'), text: message?.subject };
+	const sender = { title: t('label.from', 'From'), text: message?.sender };
+	const to = { title: t('label.to', 'To'), text: message?.to };
+	const dateCreated = {
+		title: t('label.date_created', 'Date created'),
+		text: new Date(message?.creationDate).toUTCString()
+	};
+	const dateDeleted = {
+		title: t('label.date_deleted', 'Date deleted'),
+		text: new Date(message?.deletionDate).toUTCString()
+	};
+
+	const folder = getFolder(message?.folderId);
+	const folderName = { title: t('label.folder', 'Folder'), text: folder?.name };
+
 	return itemId ? (
-		<Container background="gray5">
-			<Padding all="medium">
-				<Text
-					color="gray1"
-					overflow="break-word"
-					weight="bold"
-					size="large"
-					style={{ whiteSpace: 'pre-line', textAlign: 'center' }}
-				>
-					{displayerTitle}
-				</Text>
-			</Padding>
+		<Container padding={{ left: '8rem' }} background="gray5" width={'fill'}>
+			<BackupSearchPanelTextLine title={subject.title} text={subject.text} />
+			<BackupSearchPanelTextLine title={sender.title} text={sender.text} />
+			<BackupSearchPanelTextLine title={to.title} text={to.text} />
+			<BackupSearchPanelTextLine title={dateCreated.title} text={dateCreated.text} />
+			<BackupSearchPanelTextLine title={dateDeleted.title} text={dateDeleted.text} />
+			<BackupSearchPanelTextLine title={folderName.title} text={folderName.text} />
 		</Container>
 	) : (
 		<Container background="gray5">
