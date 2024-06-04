fb848e517091509cda0f7ab48670ad754dae6469
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-06-04 07:51:45 +0200

    test: fix restore-messages test

--- src/api/restore-messages.ts
+++ src/api/restore-messages.ts
@@ -4,9 +4,10 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export const restoreMessagesAPI = (messages: Array<string>): Promise<Response> =>
-	fetch(`/zx/backup/v1/restoreMessages`, {
+export function restoreMessagesAPI(messages: Array<string>): Promise<Response> {
+	return fetch(`/zx/backup/v1/restoreMessages`, {
 		method: 'POST',
 		credentials: 'same-origin',
 		body: JSON.stringify({ messages })
 	});
+}
--- src/api/tests/restore-messages.test.ts
+++ src/api/tests/restore-messages.test.ts
@@ -3,25 +3,24 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
+import { faker } from '@faker-js/faker';
 import { HttpResponse } from 'msw';
 
 import { createAPIInterceptor } from '../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
 import { restoreMessagesAPI } from '../restore-messages';
 
-describe('undeleteAPI', () => {
+describe('restorMessagesAPI', () => {
 	describe('when backend is available', () => {
 		beforeAll(() => {
 			createAPIInterceptor(
 				'post',
-				'/zx/backup/v1/undelete?start=2007-11-03T10:15:30.00Z&end=2007-12-03T10:15:30.00Z',
+				'/zx/backup/v1/restoreMessages',
 				HttpResponse.json({ operationId: 42 }, { status: 202 })
 			);
 		});
 
 		it('should return the reponse returned by fetch', async () => {
-			expect(
-				(await restoreMessagesAPI('2007-11-03T10:15:30.00Z', '2007-12-03T10:15:30.00Z')).status
-			).toBe(202);
+			expect((await restoreMessagesAPI([faker.number.toString()])).status).toBe(202);
 		});
 	});
 
@@ -29,15 +28,13 @@ describe('undeleteAPI', () => {
 		beforeAll(() => {
 			createAPIInterceptor(
 				'post',
-				'/zx/backup/v1/undelete',
+				'/zx/backup/v1/restoreMessages',
 				HttpResponse.json(null, { status: 500 })
 			);
 		});
 
 		it('should reply with a status that is not accepted', async () => {
-			expect(
-				(await restoreMessagesAPI('2007-11-03T10:15:30.00Z', '2007-12-03T10:15:30.00Z')).status
-			).not.toBe(202);
+			expect((await restoreMessagesAPI([faker.number.toString()])).status).not.toBe(202);
 		});
 	});
 });
