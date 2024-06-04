3f331e01cd896f1f3d164049fdf1108fd2487eb4
Manuel Garbin <manuel.garbin@studiostorti.com>
2024-06-04 17:09:19 +0200

    fix: fixed local storage null (detect if we are inside mail or search app)

--- src/views/search/search-view.tsx
+++ src/views/search/search-view.tsx
@@ -67,10 +67,10 @@ const SearchView: FC<SearchProps> = ({ useDisableSearch, useQuery, ResultsHeader
 	useEffect(() => {
 		const lastApp = localStorage.getItem('lastApp');
 		if ( lastApp == "mail" || lastApp == "" ) {
-			localStorage.setItem('lastApp','search');
 			dispatch(resetSearchResults());
 			setNewSearch(true);
 		}
+		localStorage.setItem('lastApp','search');
 		setAppContext({ isMessageView, count, setCount });
 	}, [count, isMessageView, dispatch]);
 
