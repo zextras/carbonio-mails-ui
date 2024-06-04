23d6abdc96c6327229e3bc4f0b589f9b651efb0d
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-06-04 10:07:26 +0200

    feat: add error management in restoreMessagesAPI

--- src/api/restore-messages.ts
+++ src/api/restore-messages.ts
@@ -4,10 +4,19 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-export function restoreMessagesAPI(messages: Array<string>): Promise<Response> {
+export async function restoreMessagesAPI(messages: Array<string>): Promise<Response> {
 	return fetch(`/zx/backup/v1/restoreMessages`, {
 		method: 'POST',
 		credentials: 'same-origin',
 		body: JSON.stringify({ messages })
-	});
+	})
+		.then((resp) => {
+			if (!resp.ok) {
+				throw new Error('Something went wrong with the search inside the backup');
+			}
+			return resp;
+		})
+		.catch(() => {
+			throw new Error('Something went wrong with the search inside the backup');
+		});
 }
