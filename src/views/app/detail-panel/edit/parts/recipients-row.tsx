265d0b7aa033dbfd9a03e54e995dfad9ef522115
Gammax92 <gottardifrancesco92@gmail.com>
2024-06-24 14:43:05 +0200

    wip: recipient row copy-paste

--- src/views/app/detail-panel/edit/parts/recipients-row.tsx
+++ src/views/app/detail-panel/edit/parts/recipients-row.tsx
@@ -17,7 +17,7 @@ const emailRegex =
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars, max-len, no-control-regex
 	/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
 
-const REGEX = /[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+.[a-zA-Z0-9_-]+/g;
+const emailsFromStringRegex = /[a-zA-Z0-9+._-]+@[a-zA-Z0-9._-]+.[a-zA-Z0-9_-]+/g;
 
 export interface ContactType extends ChipItem {
 	email: string;
@@ -97,17 +97,16 @@ export const RecipientsRow: FC<RecipientsRowProps> = ({
 
 	const handlePaste = (e: ClipboardEvent): void => {
 		const pastedData = e.clipboardData.getData('Text');
-
-		const emails = pastedData.match(REGEX);
+		const emails = pastedData.match(emailsFromStringRegex);
 
 		if (emails) {
 			const validEmails = emails.map((email) => ({
 				label: email,
 				error: !emailRegex.test(email)
 			}));
-
-			const updatedRecipients = map<ChipItem, Participant>(
-				validEmails,
+			const existingEmails = new Set(recipients.map((recipient) => recipient.address));
+			const uniqValidEmails = validEmails.filter((email) => !existingEmails.has(email.label));
+			const updatedRecipients = uniqValidEmails.map(
 				(contact) =>
 					({
 						address: contact.label,
@@ -152,8 +151,8 @@ export const RecipientsRow: FC<RecipientsRowProps> = ({
 					dragAndDropEnabled
 					orderedAccountIds={orderedAccountIds}
 					className="carbonio-bypass-context-menu"
-					createChipOnPaste={false}
 					onPaste={handlePaste}
+					value={chipInputValues}
 				/>
 			) : (
 				<CustomChipInput
