191df217eb112da7bd0106095658ebf8fe274088
Gammax92 <gottardifrancesco92@gmail.com>
2024-06-25 16:05:20 +0200

    fix: convert recipient to contacts for recipient rows

--- src/views/app/detail-panel/edit/parts/recipients-row.tsx
+++ src/views/app/detail-panel/edit/parts/recipients-row.tsx
@@ -9,7 +9,6 @@ import { ChipInput, ChipItem } from '@zextras/carbonio-design-system';
 import { useIntegratedComponent } from '@zextras/carbonio-shell-ui';
 import { map, reject, some } from 'lodash';
 
-import { emailParser } from './email-parser';
 import { ParticipantRoleType } from '../../../../../carbonio-ui-commons/constants/participants';
 import { Participant } from '../../../../../types';
 import styled from 'styled-components';
@@ -87,6 +86,13 @@ export const RecipientsRow: FC<RecipientsRowProps> = ({
 		[onRecipientsChange, type]
 	);
 
+	const recipientsAsContacts = map<Participant, ContactType>(recipients, (recipient) => ({
+		id: recipient.address,
+		label: recipient.address,
+		email: recipient.address,
+		error: false
+	}));
+
 	const chipInputValues = map<Participant, ChipItem>(recipients, (recipient) => ({
 		label: recipient.address
 	}));
@@ -95,21 +101,6 @@ export const RecipientsRow: FC<RecipientsRowProps> = ({
 		typeof value === 'string'
 			? { label: value, error: !emailRegex.test(value) }
 			: { label: 'unknown data', error: true };
-
-	const handlePaste = (e: ClipboardEvent): void => {
-		const pastedData = e.clipboardData?.getData('Text') || '';
-		const pastedRecipients = emailParser().parseMultipleEmails(pastedData);
-		if (pastedRecipients.length > 0) {
-			const existingRecipients = new Set(recipients.map((recipient) => recipient.address));
-			const updatedRecipients = pastedRecipients
-				.filter((email) => !existingRecipients.has(email))
-				.map(
-					(contact) => ({ address: contact, name: contact, fullName: contact, type }) as Participant
-				);
-
-			onRecipientsChange([...recipients, ...updatedRecipients]);
-		}
-	};
 
 	const onChipInputChange = (contacts: Array<ChipItem>): void => {
 		const data = map(
@@ -135,14 +126,12 @@ export const RecipientsRow: FC<RecipientsRowProps> = ({
 					placeholder={label}
 					maxChips={null}
 					onChange={onContactInputChange}
-					defaultValue={recipients}
+					defaultValue={recipientsAsContacts}
 					bottomBorderColor="transparent"
 					hasError={some(recipients || [], { error: true })}
 					dragAndDropEnabled
 					orderedAccountIds={orderedAccountIds}
 					className="carbonio-bypass-context-menu"
-					onPaste={handlePaste}
-					value={chipInputValues}
 				/>
 			) : (
 				<CustomChipInput
