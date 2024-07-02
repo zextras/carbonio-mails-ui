fda4ad806c1d64e9bac082ee13b96afb7b43ba49
gabriele <gabriele.marino@zextras.com>
2024-07-02 15:04:22 +0200

    fix: improve graphics of resizable border

--- src/views/resizable-container.tsx
+++ src/views/resizable-container.tsx
@@ -46,6 +46,22 @@ interface BorderWithResizeProps {
 	$translateTransform?: { x?: string; y?: string };
 }
 
+const HoverableContainer = styled(Container)<ContainerProps & { border: Border }>`
+	& > * {
+		border-right: ${({ theme, border }): string =>
+			border === BORDERS.EAST ? `1px solid ${theme.palette.gray2.regular}` : '0'};
+		border-bottom: ${({ theme, border }): string =>
+			border === BORDERS.SOUTH ? `1px solid ${theme.palette.gray2.regular}` : '0'};
+		transition: 0.2s ease-out;
+	}
+	&:hover > * {
+		border-right: ${({ theme, border }): string =>
+			border === BORDERS.EAST ? `1px solid ${theme.palette.primary.regular}` : '0'};
+		border-bottom: ${({ theme, border }): string =>
+			border === BORDERS.SOUTH ? `1px solid ${theme.palette.primary.regular}` : '0'};
+	}
+`;
+
 const BorderWithResize = styled.div<
 	BorderWithResizeProps & {
 		height?: never;
@@ -58,11 +74,6 @@ const BorderWithResize = styled.div<
 	width: ${({ $width }): string => $width};
 	height: ${({ $height }): string => $height};
 	${({ $position }): SimpleInterpolation => $position};
-	border-right: ${({ theme }): string => `1px solid ${theme.palette.gray2.regular}`};
-	transition: 0.2s ease-out;
-	&:hover {
-		border-right: ${({ theme }): string => `1px solid ${theme.palette.primary.regular}`};
-	}
 	${({ $translateTransform }): SimpleInterpolation =>
 		($translateTransform?.x || $translateTransform?.y) &&
 		css`
@@ -106,7 +117,14 @@ const ResizableBorder = ({
 			{...positions}
 			$cursor={border === BORDERS.EAST ? 'ew-resize' : 'ns-resize'}
 			onMouseDown={resizeHandler}
-		/>
+		>
+			<HoverableContainer width={'100%'} height={'100%'} border={border}>
+				<Container
+					width={border === BORDERS.EAST ? '1px' : '100%'}
+					height={border === BORDERS.EAST ? '100%' : '1px'}
+				/>
+			</HoverableContainer>
+		</BorderWithResize>
 	);
 };
 
