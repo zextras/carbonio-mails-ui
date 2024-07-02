cd1a2caacc1cb4d6e3e69da6e2fc62d65856f3f7
Davide Frison <davide.frison@zextras.com>
2024-07-02 13:00:17 +0200

    test: add tests for search advanced filter
    - test search is not called if invalid query
    - test setSearchDisabled invoked if search API fails with specific error

--- src/views/search/test/search-view.test.tsx
+++ src/views/search/test/search-view.test.tsx
@@ -6,11 +6,12 @@
 import React, { ReactElement } from 'react';
 
 import { screen } from '@testing-library/react';
-import { QueryChip, SearchViewProps } from '@zextras/carbonio-shell-ui';
+import { ErrorSoapBodyResponse, QueryChip, SearchViewProps } from '@zextras/carbonio-shell-ui';
 import { noop } from 'lodash';
 
 import { createSoapAPIInterceptor } from '../../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
 import { setupTest } from '../../../carbonio-ui-commons/test/test-setup';
+import * as searchByQuery from '../../../store/actions/searchByQuery';
 import { generateStore } from '../../../tests/generators/store';
 import { SearchRequest, SearchResponse } from '../../../types';
 import SearchView from '../search-view';
@@ -89,5 +90,52 @@ describe('SearchView', () => {
 		});
 		expect(advancedFiltersButton).toBeVisible();
 		expect(advancedFiltersButton).toBeDisabled();
+	});
+
+	it('should not call search API if query empty', async () => {
+		const store = generateStore();
+		const spySearchByQuery = jest.spyOn(searchByQuery, 'searchByQuery');
+		const resultsHeader = (props: { label: string }): ReactElement => <>{props.label}</>;
+		const searchViewProps: SearchViewProps = {
+			useQuery: () => [[], noop],
+			useDisableSearch: () => [false, noop],
+			ResultsHeader: resultsHeader
+		};
+
+		setupTest(<SearchView {...searchViewProps} />, {
+			store
+		});
+
+		const advancedFiltersButton = screen.getByRole('button', {
+			name: /label\.single_advanced_filter/i
+		});
+		expect(advancedFiltersButton).toBeVisible();
+		expect(advancedFiltersButton).toBeEnabled();
+		expect(spySearchByQuery).not.toBeCalled();
+	});
+
+	it('should call setSearchDisabled button if Search API fails with mail.QUERY_PARSE_ERROR', async () => {
+		const store = generateStore();
+		const interceptor = createSoapAPIInterceptor<SearchRequest, ErrorSoapBodyResponse>('Search', {
+			Fault: {
+				Reason: { Text: 'Failed to execute search' },
+				Detail: {
+					Error: { Code: 'mail.QUERY_PARSE_ERROR', Detail: 'Failed' }
+				}
+			}
+		});
+		const resultsHeader = (props: { label: string }): ReactElement => <>{props.label}</>;
+		const setSearchDisabled = jest.fn();
+		const searchViewProps: SearchViewProps = {
+			useQuery: () => [[], noop],
+			useDisableSearch: () => [false, setSearchDisabled],
+			ResultsHeader: resultsHeader
+		};
+
+		setupTest(<SearchView {...searchViewProps} />, {
+			store
+		});
+		await interceptor;
+		expect(setSearchDisabled).toBeCalled();
 	});
 });
