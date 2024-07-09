8d99cb083d196a3945d6b1ed56a5d7c45516af03
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-07-09 15:16:39 +0200

    test add test to conversationPreviewPanelContainer

--- src/views/app/detail-panel/conversation-preview-panel-container.test.tsx
+++ src/views/app/detail-panel/conversation-preview-panel-container.test.tsx
@@ -5,12 +5,13 @@
  */
 import React from 'react';
 
-import { screen } from '@testing-library/react';
+import { act, screen } from '@testing-library/react';
 import { useParams, useRouteMatch } from 'react-router-dom';
 
 import { ConversationPreviewPanelContainer } from './conversation-preview-panel-container';
 import { setupTest } from '../../../carbonio-ui-commons/test/test-setup';
 import { API_REQUEST_STATUS } from '../../../constants';
+import * as getConv from '../../../store/actions/get-conv';
 import { useSearchStore } from '../../../store/zustand/search/store';
 import { generateStore } from '../../../tests/generators/store';
 import { MailsStateType } from '../../../types';
@@ -58,6 +59,47 @@ describe('Conversation Preview Panel', () => {
 
 			setupTest(<ConversationPreviewPanelContainer />, { store: generateStore({}) });
 			expect(await screen.findByText(conversationSubject)).toBeVisible();
+		});
+
+		it('should not call searchConv API when status is fulfilled', async () => {
+			const conversationSubject = 'Test conversation';
+			const conversation = {
+				id: '1',
+				date: 0,
+				messages: [
+					{
+						id: '100',
+						parent: '1',
+						date: 0
+					}
+				],
+				participants: [],
+				subject: conversationSubject,
+				fragment: '',
+				read: false,
+				hasAttachment: false,
+				flagged: false,
+				urgent: false,
+				tags: [],
+				parent: '2',
+				messagesInConversation: 0,
+				sortIndex: 0
+			};
+
+			useSearchStore.setState({
+				conversations: { [conversation.id]: conversation },
+				status: API_REQUEST_STATUS.fulfilled
+			});
+
+			(useParams as jest.Mock).mockReturnValue({ folderId: '2', conversationId: '1' });
+			(useRouteMatch as jest.Mock).mockReturnValue({ path: 'search' });
+
+			const getConvAPI = jest.spyOn(getConv, 'getConv');
+			setupTest(<ConversationPreviewPanelContainer />, { store: generateStore({}) });
+			act(() => {
+				jest.advanceTimersByTime(10_000);
+			});
+			expect(getConvAPI).not.toHaveBeenCalled();
 		});
 	});
 
