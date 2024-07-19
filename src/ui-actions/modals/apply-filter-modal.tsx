4cae2d8892ebacfe90f1d72f10526b2233c8263b
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2024-07-19 17:31:04 +0530

    chore: fix sonarqube warning

--- src/ui-actions/modals/apply-filter-modal.tsx
+++ src/ui-actions/modals/apply-filter-modal.tsx
@@ -77,11 +77,12 @@ export const ApplyFilterModal: FC<ApplyFilterModalProps> = ({ criteria, closeMod
 					key: `applyFilter-${criteria.filterName}-error`,
 					type: 'error',
 					label:
-						err ??
-						t(
-							'messages.snackbar.apply_filter_error',
-							`filter is still running on the server but it’s taking too much time to report the affected items. The filter keeps working on the server without additional information`
-						),
+						err && err !== ''
+							? err
+							: t(
+									'messages.snackbar.apply_filter_error',
+									`filter is still running on the server but it’s taking too much time to report the affected items. The filter keeps working on the server without additional information`
+								),
 					replace: true,
 					autoHideTimeout: TIMEOUTS.SNACKBAR_DEFAULT_TIMEOUT,
 					hideButton: true
