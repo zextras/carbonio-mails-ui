9703640ba83d406d424e362094300d136baa5589
Davide Frison <davide.frison@zextras.com>
2024-07-25 20:18:42 +0200

    test(search-view): test api mail parse error results in invalid query

--- src/views/search/search-view-hooks.test.ts
+++ src/views/search/search-view-hooks.test.ts
@@ -5,12 +5,13 @@
  */
 import { renderHook } from '@testing-library/react-hooks';
 import * as hooks from '@zextras/carbonio-shell-ui';
-import { QueryChip } from '@zextras/carbonio-shell-ui';
+import { ErrorSoapBodyResponse, QueryChip } from '@zextras/carbonio-shell-ui';
 import { noop } from 'lodash';
 
 import { useRunSearch } from './search-view-hooks';
 import { createSoapAPIInterceptor } from '../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
 import { generateSettings } from '../../carbonio-ui-commons/test/mocks/settings/settings-generator';
+import { buildSoapErrorResponseBody } from '../../carbonio-ui-commons/test/mocks/utils/soap';
 import { updateConversations } from '../../store/zustand/message-store/store';
 import { generateConversation } from '../../tests/generators/generateConversation';
 import { SearchRequest, SearchResponse } from '../../types';
@@ -47,6 +48,46 @@ describe('search view hooks', () => {
 		await searchInterceptor;
 		await waitFor(() => {
 			expect(result.current.searchResults.conversationIds.size).toBe(0);
+		});
+	});
+
+	it('should set invalid query if API query error', async () => {
+		updateConversations([generateConversation({ id: '1' })], 0);
+		const settings = generateSettings({
+			prefs: {
+				zimbraPrefGroupMailBy: 'conversation'
+			}
+		});
+		jest.spyOn(hooks, 'useUserSettings').mockReturnValue(settings);
+		// eslint-disable-next-line @typescript-eslint/ban-types
+		const useDisableSearch = (): [boolean, Function] => [false, noop];
+		const interceptor = createSoapAPIInterceptor<SearchRequest, ErrorSoapBodyResponse>(
+			'Search',
+			buildSoapErrorResponseBody({
+				detailCode: 'mail.QUERY_PARSE_ERROR',
+				reason: 'Failed to execute search'
+			})
+		);
+
+		const { result, waitFor } = renderHook(() =>
+			useRunSearch({
+				query: [
+					{
+						hasAvatar: false,
+						id: '0',
+						label: 'ciao'
+					}
+				],
+				updateQuery: noop,
+				useDisableSearch,
+				invalidQueryTooltip: 'INVALID',
+				isSharedFolderIncluded: false
+			})
+		);
+
+		await interceptor;
+		await waitFor(() => {
+			expect(result.current.isInvalidQuery).toBe(true);
 		});
 	});
 });
