c6e3798e567d0131aefd91f5261acd003b39c439
Davide Frison <davide.frison@zextras.com>
2024-07-25 17:24:20 +0200

    test: add more tests to message store

--- src/store/zustand/message-store/store.test.ts
+++ src/store/zustand/message-store/store.test.ts
@@ -6,27 +6,30 @@
 import { renderHook } from '@testing-library/react-hooks';
 
 import {
+	updateConversationMessages,
 	updateConversations,
 	updateConversationStatus,
+	updateMessages,
+	updateMessagesParent,
 	useConversationById,
-	useConversationStatus
+	useConversationStatus,
+	useMessageById
 } from './store';
 import { API_REQUEST_STATUS } from '../../../constants';
 import { generateConversation } from '../../../tests/generators/generateConversation';
+import { generateMessage } from '../../../tests/generators/generateMessage';
 
 describe('message store', () => {
 	describe('conversation', () => {
 		it('should set and return a conversation', () => {
-			const conversation = generateConversation({ id: '1', subject: 'Test conversation' });
+			const conversation = generateConversation({ id: '1' });
 			updateConversations([conversation], 0);
 
 			const { result } = renderHook(() => useConversationById('1'));
 
 			expect(result.current).toEqual(conversation);
 		});
-	});
 
-	describe('conversationStatus', () => {
 		it('should get undefined if conversation loading status not present', () => {
 			const { result } = renderHook(() => useConversationStatus('123'));
 
@@ -39,6 +42,40 @@ describe('message store', () => {
 			const { result } = renderHook(() => useConversationStatus('123'));
 
 			expect(result.current).toBe(API_REQUEST_STATUS.fulfilled);
+		});
+
+		it('should update conversation messages', () => {
+			const conversation = generateConversation({ id: '1' });
+			updateConversations([conversation], 0);
+
+			const message = generateMessage({ id: '1' });
+			updateConversationMessages(conversation.id, [message]);
+
+			const { result } = renderHook(() => useConversationById('1'));
+
+			expect(result.current.messages).toHaveLength(1);
+			expect(result.current.messages[0]).toBe(message);
+		});
+	});
+
+	describe('messages', () => {
+		it('should set and return a message', () => {
+			const message = generateMessage({ id: '1' });
+			updateMessages([message], 0);
+
+			const { result } = renderHook(() => useMessageById('1'));
+
+			expect(result.current).toEqual(message);
+		});
+
+		it('should update and return messages parent', () => {
+			updateMessages([generateMessage({ id: '1' }), generateMessage({ id: '2' })], 0);
+
+			const newFolder = 'newFolder';
+			updateMessagesParent(newFolder, ['1', '2']);
+
+			expect(renderHook(() => useMessageById('1')).result.current.parent).toBe(newFolder);
+			expect(renderHook(() => useMessageById('2')).result.current.parent).toBe(newFolder);
 		});
 	});
 });
