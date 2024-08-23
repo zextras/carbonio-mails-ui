6baf5676601b08975c7d64b5d8d65992ed70e38c
Davide Frison <davide.frison@zextras.com>
2024-08-23 11:52:19 +0200

    test: add more characterization tests for search.test.ts

--- src/api/tests/search.test.ts
+++ src/api/tests/search.test.ts
@@ -33,4 +33,98 @@ describe('Search', () => {
 		const receivedRequest = await interceptor;
 		expect(receivedRequest.sortBy).toBe('dateAsc');
 	});
+
+	describe('sortBy flag', () => {
+		it('should add is:flagged to query if sortBy flagAsc and folderId present', async () => {
+			const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+			searchSoapApi({
+				folderId: '1',
+				limit: 10,
+				sortBy: 'flagAsc'
+			});
+
+			const receivedRequest = await interceptor;
+			expect(receivedRequest.query).toBe('inId:"1" is:flagged');
+		});
+
+		it('should add is:flagged to query if sortBy flagDesc and folderId present', async () => {
+			const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+			searchSoapApi({
+				folderId: '1',
+				limit: 10,
+				sortBy: 'flagDesc'
+			});
+
+			const receivedRequest = await interceptor;
+			expect(receivedRequest.query).toBe('inId:"1" is:flagged');
+		});
+	});
+
+	it('should not alter query if folderId missing', async () => {
+		const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+		searchSoapApi({
+			query: 'whatever',
+			limit: 10,
+			sortBy: 'flagAsc'
+		});
+
+		const receivedRequest = await interceptor;
+		expect(receivedRequest.query).toBe('whatever');
+	});
+
+	describe('sortBy priority', () => {
+		it('should add priority:high to query if sortBy priorityDesc and folderId present', async () => {
+			const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+			searchSoapApi({
+				folderId: '1',
+				limit: 10,
+				sortBy: 'priorityDesc'
+			});
+
+			const receivedRequest = await interceptor;
+			expect(receivedRequest.query).toBe('inId:"1" priority:high');
+		});
+		it('should add priority:high to query if sortBy priorityAsc and folderId present', async () => {
+			const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+			searchSoapApi({
+				folderId: '1',
+				limit: 10,
+				sortBy: 'priorityAsc'
+			});
+
+			const receivedRequest = await interceptor;
+			expect(receivedRequest.query).toBe('inId:"1" priority:high');
+		});
+	});
+	describe('sortBy attachment', () => {
+		it('should add has:attachment to query if sortBy attachDesc and folderId present', async () => {
+			const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+			searchSoapApi({
+				folderId: '1',
+				limit: 10,
+				sortBy: 'attachDesc'
+			});
+
+			const receivedRequest = await interceptor;
+			expect(receivedRequest.query).toBe('inId:"1" has:attachment');
+		});
+		it('should add has:attachment to query if sortBy attachAsc and folderId present', async () => {
+			const interceptor = createSoapAPIInterceptor<SearchRequest>('Search');
+
+			searchSoapApi({
+				folderId: '1',
+				limit: 10,
+				sortBy: 'attachAsc'
+			});
+
+			const receivedRequest = await interceptor;
+			expect(receivedRequest.query).toBe('inId:"1" has:attachment');
+		});
+	});
 });
