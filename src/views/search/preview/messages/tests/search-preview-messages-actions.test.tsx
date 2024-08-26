c5700f3a3d688397ed472a33839e0f616c90d9ba
Davide Frison <davide.frison@zextras.com>
2024-08-26 17:11:41 +0200

    test: add tests about rendering the panel

--- src/views/search/preview/messages/tests/search-preview-messages-actions.test.tsx
+++ src/views/search/preview/messages/tests/search-preview-messages-actions.test.tsx
@@ -3,15 +3,25 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-import { MessageActionsDescriptors } from '../../../../../constants';
-import { setSearchResultsByMessage } from '../../../../../store/zustand/search/store';
+import React, { FC } from 'react';
+
+import { screen } from '@testing-library/react';
+
+import { setupTest } from '../../../../../carbonio-ui-commons/test/test-setup';
+import { API_REQUEST_STATUS, MessageActionsDescriptors } from '../../../../../constants';
+import {
+	setSearchResultsByMessage,
+	updateMessageStatus
+} from '../../../../../store/zustand/search/store';
 import { generateMessage } from '../../../../../tests/generators/generateMessage';
+import { generateStore } from '../../../../../tests/generators/store';
 import { ExtraWindowCreationParams, ExtraWindowsCreationResult } from '../../../../../types';
 import {
 	previewMessageOnSeparatedWindow,
 	previewMessageOnSeparatedWindowAction
 } from '../search-preview-messages-actions';
 
+const TestComponent: FC = ({ children }) => <div>{children}</div>;
 describe('Preview message on new window', () => {
 	it('should call create window with correct parameters', () => {
 		setSearchResultsByMessage([generateMessage({ id: '1' })], false);
@@ -28,6 +38,29 @@ describe('Preview message on new window', () => {
 		expect(spyParams.closeOnUnmount).toBe(false);
 		expect(spyParams.returnComponent).toBe(false);
 		// NOTE: we can also test the UI by using spyParams.children
+	});
+
+	it('should render message on a detail panel', async () => {
+		setSearchResultsByMessage([generateMessage({ id: '1', isComplete: true })], false);
+		updateMessageStatus('1', API_REQUEST_STATUS.fulfilled);
+		let spyParams: Partial<ExtraWindowCreationParams> = {};
+		const spyCreateWindow = (props: ExtraWindowCreationParams): ExtraWindowsCreationResult => {
+			spyParams = props;
+			return { id: '1' };
+		};
+		const store = generateStore({});
+
+		// Should use a strict type, not any
+		const primaryActions: Array<any> = [];
+		const secondaryActions: Array<any> = [];
+		previewMessageOnSeparatedWindow('1', 'Test subject', spyCreateWindow, [
+			primaryActions,
+			secondaryActions
+		]);
+
+		// eslint-disable-next-line testing-library/no-node-access
+		setupTest(<TestComponent>{spyParams.children}</TestComponent>, { store });
+		expect(await screen.findByTestId('MailPreview-1')).toBeVisible();
 	});
 
 	it('should return an action with id "preview on separate window"', () => {
