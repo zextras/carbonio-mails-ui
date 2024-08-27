12090e2b4c55f7d4e4469ecdf576d73589c60c29
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-08-27 11:56:56 +0200

    chore: update package-lock and remove prettier errors

--- src/views/app/folder-panel/messages/message-list.tsx
+++ src/views/app/folder-panel/messages/message-list.tsx
@@ -140,7 +140,7 @@ export const MessageList: FC = () => {
 	);
 
 	const totalMessages = useMemo(
-		() => (sortOrder === 'readAsc' ? messages.length : folder?.n ?? messages.length ?? 0),
+		() => (sortOrder === 'readAsc' ? messages.length : (folder?.n ?? messages.length ?? 0)),
 		[folder?.n, messages.length, sortOrder]
 	);
 
