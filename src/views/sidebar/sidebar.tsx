a485148d7c8b4ee23d9e7295d3c4202a666dd47b
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-09-02 17:01:27 +0200

    fix: pass StoreProvider to useInitializeFolders as a prop

--- src/views/sidebar/sidebar.tsx
+++ src/views/sidebar/sidebar.tsx
@@ -24,6 +24,7 @@ import type { Folder } from '../../carbonio-ui-commons/types/folder';
 import { LOCAL_STORAGES } from '../../constants';
 import { useFolders } from '../../hooks/use-folders';
 import useGetTagsAccordion from '../../hooks/use-get-tags-accordions';
+import { StoreProvider } from '../../store/redux';
 import type { SidebarComponentProps } from '../../types/sidebar';
 
 const SidebarComponent: FC<SidebarComponentProps> = memo(function SidebarComponent({ accordions }) {
@@ -59,7 +60,7 @@ const SidebarComponent: FC<SidebarComponentProps> = memo(function SidebarCompone
 });
 
 const Sidebar: FC<SecondaryBarComponentProps> = ({ expanded }) => {
-	useInitializeFolders(FOLDER_VIEW.message);
+	useInitializeFolders({ view: FOLDER_VIEW.message, StoreProvider });
 	const { path } = useRouteMatch();
 	const accordions = useFolders();
 
