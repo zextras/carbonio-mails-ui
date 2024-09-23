06f6a988b2c159a7c794700b90232a67544507fc
Luca Stauble <luca.stauble@gmail.com>
2024-09-23 13:02:29 +0200

    test: add new test

---
+++ src/hooks/actions/test/use-msg-forward.test.ts
@@ -0,0 +1,103 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { FOLDERS } from '../../../carbonio-ui-commons/constants/folders';
+import { addBoard } from '../../../carbonio-ui-commons/test/mocks/carbonio-shell-ui';
+import { setupHook } from '../../../carbonio-ui-commons/test/test-setup';
+import { FOLDERS_DESCRIPTORS } from '../../../constants';
+import { generateMessage } from '../../../tests/generators/generateMessage';
+import { useForwardMsgDescriptor, useForwardMsgFn } from '../use-msg-forward';
+
+describe('useMsgForwardDescriptor', () => {
+	const msg = generateMessage();
+
+	it('Should return an object with specific id, icon, label and 2 functions', () => {
+		const {
+			result: { current: descriptor }
+		} = setupHook(useForwardMsgDescriptor, {
+			initialProps: [msg.id, FOLDERS.INBOX]
+		});
+
+		expect(descriptor).toEqual({
+			id: 'message-forward',
+			icon: 'Forward',
+			label: 'Forward',
+			execute: expect.any(Function),
+			canExecute: expect.any(Function)
+		});
+	});
+});
+
+describe('useMsgForwardFn', () => {
+	const msg = generateMessage();
+
+	it('Should return an object with execute and canExecute functions', () => {
+		const {
+			result: { current: functions }
+		} = setupHook(useForwardMsgFn, {
+			initialProps: [msg.id, FOLDERS.INBOX]
+		});
+
+		expect(functions).toEqual({
+			execute: expect.any(Function),
+			canExecute: expect.any(Function)
+		});
+	});
+
+	describe('canExecute', () => {
+		it.each`
+			folder                              | assertion
+			${FOLDERS_DESCRIPTORS.INBOX}        | ${true}
+			${FOLDERS_DESCRIPTORS.SENT}         | ${true}
+			${FOLDERS_DESCRIPTORS.DRAFTS}       | ${false}
+			${FOLDERS_DESCRIPTORS.TRASH}        | ${true}
+			${FOLDERS_DESCRIPTORS.SPAM}         | ${false}
+			${FOLDERS_DESCRIPTORS.USER_DEFINED} | ${true}
+		`(`should return $assertion if the folder is $folder.desc`, ({ folder, assertion }) => {
+			const {
+				result: { current: functions }
+			} = setupHook(useForwardMsgFn, {
+				initialProps: [msg.id, folder.id]
+			});
+
+			expect(functions.canExecute()).toEqual(assertion);
+		});
+	});
+
+	describe('execute', () => {
+		it('should create a board with specific parameters', async () => {
+			const {
+				result: { current: functions }
+			} = setupHook(useForwardMsgFn, {
+				initialProps: [msg.id, FOLDERS.INBOX]
+			});
+
+			functions.execute();
+
+			expect(addBoard).toHaveBeenCalledWith(
+				expect.objectContaining({
+					boardViewId: 'mails_editor_board_view',
+					context: expect.objectContaining({
+						originAction: 'forward',
+						originActionTargetId: msg.id
+					})
+				})
+			);
+		});
+
+		it('should not create a board if the action cannot be executed', async () => {
+			const {
+				result: { current: functions }
+			} = setupHook(useForwardMsgFn, {
+				initialProps: [msg.id, FOLDERS.DRAFTS]
+			});
+
+			functions.execute();
+
+			expect(addBoard).not.toHaveBeenCalled();
+		});
+	});
+});
