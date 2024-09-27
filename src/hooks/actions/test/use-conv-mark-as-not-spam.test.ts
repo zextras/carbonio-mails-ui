7b02b7a9475940628a7578300de5f7031ce10115
gabriele <gabriele.marino@zextras.com>
2024-09-27 15:28:36 +0200

    test: implement use-conv-mark-as-not-spam test

--- src/hooks/actions/test/use-conv-mark-as-not-spam.test.ts
+++ src/hooks/actions/test/use-conv-mark-as-not-spam.test.ts
@@ -18,112 +18,48 @@ import {
 	useConvMarkAsNotSpamFn
 } from '../use-conv-mark-as-not-spam';
 
-describe('useConvMarkAsNotSpamDescriptor', () => {
-	const ids = times(faker.number.int({ max: 42 }), () =>
-		faker.number.int({ max: 42000 }).toString()
-	);
-	const store = generateStore({
-		conversations: {
-			searchedInFolder: {},
-			conversations: {},
-			searchRequestStatus: API_REQUEST_STATUS.fulfilled,
-			currentFolder: FOLDERS.SPAM,
-			expandedStatus: {
-				[FOLDERS.SPAM]: 'fulfilled'
-			}
-		}
-	});
-
-	it('Should return an object with specific id, icon, label and 2 functions', () => {
-		const {
-			result: { current: descriptor }
-		} = setupHook(useConvMarkAsNotSpamDescriptor, {
-			store,
-			initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.SPAM }]
-		});
-
-		expect(descriptor).toEqual({
-			id: 'conversation-mark_as_not_spam',
-			icon: 'AlertCircleOutline',
-			label: 'Not spam',
-			execute: expect.any(Function),
-			canExecute: expect.any(Function)
-		});
-	});
-});
-
-describe('useConvMarkAsNotSpamFn', () => {
-	const ids = times(faker.number.int({ max: 42 }), () =>
-		faker.number.int({ max: 42000 }).toString()
-	);
-	const store = generateStore();
-
-	it('Should return an object with execute and canExecute functions', () => {
-		const {
-			result: { current: functions }
-		} = setupHook(useConvMarkAsNotSpamFn, {
-			store,
-			initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.SPAM }]
-		});
-
-		expect(functions).toEqual({
-			execute: expect.any(Function),
-			canExecute: expect.any(Function)
-		});
-	});
-
-	describe('canExecute', () => {
-		it.each`
-			folder                              | assertion
-			${FOLDERS_DESCRIPTORS.INBOX}        | ${false}
-			${FOLDERS_DESCRIPTORS.SENT}         | ${false}
-			${FOLDERS_DESCRIPTORS.DRAFTS}       | ${false}
-			${FOLDERS_DESCRIPTORS.TRASH}        | ${false}
-			${FOLDERS_DESCRIPTORS.SPAM}         | ${true}
-			${FOLDERS_DESCRIPTORS.USER_DEFINED} | ${false}
-		`(`should return $assertion if the folder is $folder.desc`, ({ folder, assertion }) => {
-			const {
-				result: { current: functions }
-			} = setupHook(useConvMarkAsNotSpamFn, {
-				store,
-				initialProps: [{ ids, shouldReplaceHistory: false, folderId: folder.id }]
-			});
-
-			expect(functions.canExecute()).toEqual(assertion);
-		});
-	});
-
-	describe('execute', () => {
-		it('should not call the API if the action cannot be executed', async () => {
-			const callFlag = jest.fn();
-			createSoapAPIInterceptor('ConvAction').then(callFlag);
-
-			const {
-				result: { current: functions }
-			} = setupHook(useConvMarkAsNotSpamFn, {
-				store,
-				initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.INBOX }]
-			});
-
-			await act(async () => {
-				functions.execute();
-			});
-
-			expect(callFlag).not.toHaveBeenCalled();
-		});
-
-		it('should call the API with the proper params if the action can be executed', async () => {
-			const ids = times(faker.number.int({ max: 20 }), () => faker.number.int().toString());
-			const apiInterceptor = createSoapAPIInterceptor<ConvActionRequest, ConvActionResponse>(
-				'ConvAction',
-				{
-					action: {
-						id: ids.join(','),
-						op: '!spam'
-					}
+describe('useConvMarkAsNotSpam', () => {
+	describe('Descriptor', () => {
+		const ids = times(faker.number.int({ max: 42 }), () =>
+			faker.number.int({ max: 42000 }).toString()
+		);
+		const store = generateStore({
+			conversations: {
+				searchedInFolder: {},
+				conversations: {},
+				searchRequestStatus: API_REQUEST_STATUS.fulfilled,
+				currentFolder: FOLDERS.SPAM,
+				expandedStatus: {
+					[FOLDERS.SPAM]: 'fulfilled'
 				}
-			);
+			}
+		});
 
+		it('Should return an object with specific id, icon, label and 2 functions', () => {
+			const {
+				result: { current: descriptor }
+			} = setupHook(useConvMarkAsNotSpamDescriptor, {
+				store,
+				initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.SPAM }]
+			});
+
+			expect(descriptor).toEqual({
+				id: 'conversation-mark_as_not_spam',
+				icon: 'AlertCircleOutline',
+				label: 'Not spam',
+				execute: expect.any(Function),
+				canExecute: expect.any(Function)
+			});
+		});
+	});
+
+	describe('Fn', () => {
+		const ids = times(faker.number.int({ max: 42 }), () =>
+			faker.number.int({ max: 42000 }).toString()
+		);
+		const store = generateStore();
+
+		it('Should return an object with execute and canExecute functions', () => {
 			const {
 				result: { current: functions }
 			} = setupHook(useConvMarkAsNotSpamFn, {
@@ -131,16 +67,81 @@ describe('useConvMarkAsNotSpamFn', () => {
 				initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.SPAM }]
 			});
 
-			act(() => {
-				functions.execute();
-				jest.advanceTimersByTime(TIMEOUTS.SET_AS_SPAM);
+			expect(functions).toEqual({
+				execute: expect.any(Function),
+				canExecute: expect.any(Function)
+			});
+		});
+
+		describe('canExecute', () => {
+			it.each`
+				folder                              | assertion
+				${FOLDERS_DESCRIPTORS.INBOX}        | ${false}
+				${FOLDERS_DESCRIPTORS.SENT}         | ${false}
+				${FOLDERS_DESCRIPTORS.DRAFTS}       | ${false}
+				${FOLDERS_DESCRIPTORS.TRASH}        | ${false}
+				${FOLDERS_DESCRIPTORS.SPAM}         | ${true}
+				${FOLDERS_DESCRIPTORS.USER_DEFINED} | ${false}
+			`(`should return $assertion if the folder is $folder.desc`, ({ folder, assertion }) => {
+				const {
+					result: { current: functions }
+				} = setupHook(useConvMarkAsNotSpamFn, {
+					store,
+					initialProps: [{ ids, shouldReplaceHistory: false, folderId: folder.id }]
+				});
+
+				expect(functions.canExecute()).toEqual(assertion);
+			});
+		});
+
+		describe('execute', () => {
+			it('should not call the API if the action cannot be executed', async () => {
+				const callFlag = jest.fn();
+				createSoapAPIInterceptor('ConvAction').then(callFlag);
+
+				const {
+					result: { current: functions }
+				} = setupHook(useConvMarkAsNotSpamFn, {
+					store,
+					initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.INBOX }]
+				});
+
+				await act(async () => {
+					functions.execute();
+				});
+
+				expect(callFlag).not.toHaveBeenCalled();
 			});
 
-			const requestParameter = await apiInterceptor;
-			expect(requestParameter.action.id).toBe(ids.join(','));
-			expect(requestParameter.action.op).toBe('!spam');
-			expect(requestParameter.action.l).toBeUndefined();
-			expect(requestParameter.action.tn).toBeUndefined();
+			it('should call the API with the proper params if the action can be executed', async () => {
+				const apiInterceptor = createSoapAPIInterceptor<ConvActionRequest, ConvActionResponse>(
+					'ConvAction',
+					{
+						action: {
+							id: ids.join(','),
+							op: '!spam'
+						}
+					}
+				);
+
+				const {
+					result: { current: functions }
+				} = setupHook(useConvMarkAsNotSpamFn, {
+					store,
+					initialProps: [{ ids, shouldReplaceHistory: false, folderId: FOLDERS.SPAM }]
+				});
+
+				act(() => {
+					functions.execute();
+					jest.advanceTimersByTime(TIMEOUTS.SET_AS_SPAM);
+				});
+
+				const requestParameter = await apiInterceptor;
+				expect(requestParameter.action.id).toBe(ids.join(','));
+				expect(requestParameter.action.op).toBe('!spam');
+				expect(requestParameter.action.l).toBeUndefined();
+				expect(requestParameter.action.tn).toBeUndefined();
+			});
 		});
 	});
 });
