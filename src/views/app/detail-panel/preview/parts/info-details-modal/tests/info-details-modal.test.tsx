f41d505172b3fe1160056cf5285896750b560454
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-10-24 15:53:14 +0200

    test(MailInfoDetailModal): add test for all of components properties

--- src/views/app/detail-panel/preview/parts/info-details-modal/tests/info-details-modal.test.tsx
+++ src/views/app/detail-panel/preview/parts/info-details-modal/tests/info-details-modal.test.tsx
@@ -14,16 +14,55 @@ import { selectMessage } from '../../../../../../../store/messages-slice';
 import { generateStore } from '../../../../../../../tests/generators/store';
 import { MailInfoDetailModal } from '../mail-info-detail-modal';
 
-describe('Signed info block', () => {
-	test(`Should show the correct title`, async () => {
+describe('Mail Info Detail Modal', () => {
+	test(`Should correctly render all parts`, async () => {
 		const onClose = jest.fn();
 		const store = generateStore();
 
 		await store.dispatch<any>(getMsgAsyncThunk({ msgId: '15' }));
 		const state = store.getState();
 		const msg = selectMessage(state, '15');
-		setupTest(<MailInfoDetailModal onClose={onClose} signature={msg.signature?.[0]} />, { store });
-		const title = await screen.findByText('Message details');
-		expect(title).toBeVisible();
+		setupTest(
+			<MailInfoDetailModal
+				onClose={onClose}
+				signature={msg.signature?.[0]}
+				messageIdFromMailHeaders={'messageId'}
+				creationDateFromMailHeaders={'creationDate'}
+				mailAuthenticationHeaders={{
+					dkim: { value: 'dkimvalue', pass: true },
+					spf: { value: 'spfvalue', pass: true },
+					dmarc: { value: 'dmarcvalue', pass: true }
+				}}
+				messageIsFromDistributionList
+				messageIsFromExternalDomain
+				sensitivityValue={'Private'}
+			/>,
+			{ store }
+		);
+		expect(screen.getByText('Message details')).toBeVisible();
+		expect(screen.getByText('General Information')).toBeVisible();
+		expect(screen.getByText('Message ID:')).toBeVisible();
+		expect(screen.getByText('messageId')).toBeVisible();
+		expect(screen.getByText('Created at:')).toBeVisible();
+		expect(screen.getByText('creationDate')).toBeVisible();
+		expect(screen.getByText('Sensitivity:')).toBeVisible();
+		expect(screen.getByText('Private')).toBeVisible();
+		expect(screen.getByText('This email is from a Distribution List')).toBeVisible();
+		expect(screen.getByText('This email is from an External Domain')).toBeVisible();
+		expect(screen.getByText('Authentication Headers')).toBeVisible();
+		expect(screen.getByText('DKIM:')).toBeVisible();
+		expect(screen.getByText('DMARC:')).toBeVisible();
+		expect(screen.getByText('SPF:')).toBeVisible();
+		expect(screen.getByText('dkimvalue')).toBeVisible();
+		expect(screen.getByText('spfvalue')).toBeVisible();
+		expect(screen.getByText('dmarcvalue')).toBeVisible();
+		expect(screen.getByText(`Issuer's Certificate Not Found`)).toBeVisible();
+		expect(
+			screen.getByText(
+				'This message includes a digital signature, but the certificate of the issuing authority could not be found.'
+			)
+		).toBeVisible();
+		expect(screen.getByText('Validity:')).toBeVisible();
+		expect(screen.getByText('Close')).toBeVisible();
 	});
 });
