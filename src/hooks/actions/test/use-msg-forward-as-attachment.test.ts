cfb309840a4365551ccf307dc91c4a98b6788d58
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2024-11-12 11:39:52 +0100

    test: added tests for forward as attachment action

---
+++ src/hooks/actions/test/use-msg-forward-as-attachment.test.ts
@@ -0,0 +1,118 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { addBoard } from '@zextras/carbonio-shell-ui';
+
+import { FOLDERS } from '../../../carbonio-ui-commons/constants/folders';
+import { setupHook } from '../../../carbonio-ui-commons/test/test-setup';
+import { FOLDERS_DESCRIPTORS } from '../../../constants';
+import { generateMessage } from '../../../tests/generators/generateMessage';
+import {
+	useMsgForwardAsAttachmentDescriptor,
+	useMsgForwardAsAttachmentFn
+} from '../use-msg-forward-as-attachment';
+
+describe('useMsgForwardAsAttachment', () => {
+	const msg = generateMessage();
+
+	describe('Descriptor', () => {
+		it('Should return an object with specific id, icon, label and 2 functions', () => {
+			const {
+				result: { current: descriptor }
+			} = setupHook(useMsgForwardAsAttachmentDescriptor, {
+				initialProps: [[msg.id], FOLDERS.INBOX]
+			});
+
+			expect(descriptor).toEqual({
+				id: 'message-forward_as_attachment',
+				icon: 'Attach',
+				label: 'Forward as attachment',
+				execute: expect.any(Function),
+				canExecute: expect.any(Function)
+			});
+		});
+	});
+
+	describe('useMsgForwardAsAttachmentFn', () => {
+		it('Should return an object with execute and canExecute functions', () => {
+			const {
+				result: { current: functions }
+			} = setupHook(useMsgForwardAsAttachmentFn, {
+				initialProps: [[msg.id], FOLDERS.INBOX]
+			});
+
+			expect(functions).toEqual({
+				execute: expect.any(Function),
+				canExecute: expect.any(Function)
+			});
+		});
+
+		describe('canExecute', () => {
+			it.each`
+				folder                              | assertion
+				${FOLDERS_DESCRIPTORS.INBOX}        | ${true}
+				${FOLDERS_DESCRIPTORS.SENT}         | ${true}
+				${FOLDERS_DESCRIPTORS.DRAFTS}       | ${false}
+				${FOLDERS_DESCRIPTORS.TRASH}        | ${true}
+				${FOLDERS_DESCRIPTORS.SPAM}         | ${false}
+				${FOLDERS_DESCRIPTORS.USER_DEFINED} | ${true}
+			`(`should return $assertion if the folder is $folder.desc`, ({ folder, assertion }) => {
+				const {
+					result: { current: functions }
+				} = setupHook(useMsgForwardAsAttachmentFn, {
+					initialProps: [[msg.id], folder.id]
+				});
+
+				expect(functions.canExecute()).toEqual(assertion);
+			});
+		});
+
+		describe('execute', () => {
+			it('should create a board with specific parameters', async () => {
+				const {
+					result: { current: functions }
+				} = setupHook(useMsgForwardAsAttachmentFn, {
+					initialProps: [[msg.id], FOLDERS.INBOX]
+				});
+
+				functions.execute();
+
+				expect(addBoard).toHaveBeenCalledWith(
+					expect.objectContaining({
+						boardViewId: 'mails_editor_board_view',
+						context: expect.objectContaining({
+							originAction: 'forwardAsAttachment',
+							compositionData: {
+								attachments: [
+									{
+										contentType: 'message/rfc822',
+										filename: `${msg.id}.eml`,
+										isInline: false,
+										mid: msg.id,
+										size: 0
+									}
+								]
+							},
+							originActionTargetId: msg.id
+						})
+					})
+				);
+			});
+
+			it('should not create a board if the action cannot be executed', async () => {
+				const {
+					result: { current: functions }
+				} = setupHook(useMsgForwardAsAttachmentFn, {
+					initialProps: [[msg.id], FOLDERS.DRAFTS]
+				});
+
+				functions.execute();
+
+				expect(addBoard).not.toHaveBeenCalled();
+			});
+		});
+	});
+});
