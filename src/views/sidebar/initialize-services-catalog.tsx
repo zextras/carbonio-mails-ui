b0c6f9e457a3c67c9bc3c24e2bdba945406874b5
gabriele <gabriele.marino@zextras.com>
2024-11-13 11:14:01 +0100

    fix: request catalog service on app loading

---
+++ src/views/sidebar/initialize-services-catalog.tsx
@@ -0,0 +1,30 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { useEffect } from 'react';
+
+import { setAppContext, useAppContext } from '@zextras/carbonio-shell-ui';
+import { isNil } from 'lodash';
+
+import { requestServiceCatalog } from '../../api/request-service-catalog';
+import { AppContext } from '../../types';
+
+export const InitializeServicesCatalog = (): null => {
+	const { servicesCatalog } = useAppContext<AppContext>();
+
+	useEffect(() => {
+		if (!servicesCatalog) {
+			requestServiceCatalog().then((res) => {
+				if (!isNil(res)) {
+					setAppContext((context: AppContext) => ({ ...context, servicesCatalog: res }));
+				} else {
+					setAppContext((context: AppContext) => ({ ...context, servicesCatalog: [] }));
+				}
+			});
+		}
+	}, [servicesCatalog]);
+
+	return null;
+};
