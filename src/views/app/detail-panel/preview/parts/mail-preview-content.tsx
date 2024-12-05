addc172405dcb79bb6014876661482bf0c74dc72
Keshav Bhatt <keshavnrj@gmail.com>
2024-12-05 18:10:46 +0530

    chore: remove commented code

--- src/views/app/detail-panel/preview/parts/mail-preview-content.tsx
+++ src/views/app/detail-panel/preview/parts/mail-preview-content.tsx
@@ -132,7 +132,6 @@ export const MailPreviewContent: FC<MailPreviewContentProps> = ({
 		>
 			{message.isComplete && (
 				<Container
-					// className="force-white-bg"
 					data-testid="MessageBody"
 					width="100%"
 					height="100%"
