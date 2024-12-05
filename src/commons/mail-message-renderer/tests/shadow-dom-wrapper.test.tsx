33f0d92a61d4e959496bcf95b39ba8a7392912ce
Keshav Bhatt <keshavnrj@gmail.com>
2024-12-05 21:26:57 +0530

    chore: move test into test folder

--- src/commons/mail-message-renderer/tests/shadow-dom-wrapper.test.tsx
+++ src/commons/mail-message-renderer/tests/shadow-dom-wrapper.test.tsx
@@ -7,7 +7,6 @@ import React from 'react';
 
 import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-
 import { useUserSettings } from '@zextras/carbonio-shell-ui';
 import * as darkReader from 'darkreader';
 
