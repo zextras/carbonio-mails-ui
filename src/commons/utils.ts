667adf951abb4a2f07cd46f2ff1bad247c43197f
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2024-12-05 12:00:28 +0100

    fix: stricter check in replaceLinkToAnchor for text that represent links

--- src/commons/utils.ts
+++ src/commons/utils.ts
@@ -90,24 +90,43 @@ export const plainTextToHTML = (str: string): string => {
 	return '';
 };
 
+function isValidUrl(url: string): boolean {
+	try {
+		const newUrl = new URL(url);
+		return ['http:', 'https:'].includes(newUrl.protocol);
+	} catch (err) {
+		return false;
+	}
+}
+
 export const replaceLinkToAnchor = (content: string): string => {
 	if (content === '') {
 		return '';
 	}
-	const linkRegexp = /(https?:\/\/[^\s]+|www\.[^\s]+)/g;
+	const linkRegexp = new RegExp(
+		'(https?:\\/\\/)' +
+			'(((?=[a-z\\d-]{1,100}\\.)(xn--)?[a-z\\d]+(-[a-z\\d]+)*\\.)+[a-z]{2,100}|' +
+			'((\\d{1,3}\\.){3}\\d{1,3}))' +
+			'(:\\d+)?(/[-a-z\\d%_.~+#\\/]+)*(\\?[;&:@a-z\\d%_.~\\+=#\\-]*)' +
+			'?([#:\\/\\.\\-a-z\\d_)]+)?',
+		'gi'
+	);
 	return content.replace(linkRegexp, (url) => {
-		const wrap = document.createElement('div');
-		const anchor = document.createElement('a');
+		if (isValidUrl(url)) {
+			const wrap = document.createElement('div');
+			const anchor = document.createElement('a');
 
-		const newInnerHtml = url.replace(/&#64;/g, '@').replace(/&#61;/g, '=');
-		let href = url;
-		if (!url.startsWith('http') && !url.startsWith('https')) {
-			href = `http://${url}`;
+			const newInnerHtml = url.replace(/&#64;/g, '@').replace(/&#61;/g, '=');
+			let href = url;
+			if (!url.startsWith('http') && !url.startsWith('https')) {
+				href = `http://${url}`;
+			}
+			anchor.href = href;
+			anchor.target = '_blank';
+			anchor.innerHTML = newInnerHtml;
+			wrap.appendChild(anchor);
+			return wrap.innerHTML;
 		}
-		anchor.href = href;
-		anchor.target = '_blank';
-		anchor.innerHTML = newInnerHtml;
-		wrap.appendChild(anchor);
-		return wrap.innerHTML;
+		return url;
 	});
 };
