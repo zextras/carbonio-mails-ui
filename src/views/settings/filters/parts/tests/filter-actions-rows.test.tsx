47425aa0039795569461bec463b402e997739d8a
Davide Frison <davide.frison@zextras.com>
2024-12-06 12:50:20 +0100

    test: rearrange filter-actions-rows.test.tsx

--- src/views/settings/filters/parts/tests/filter-actions-rows.test.tsx
+++ src/views/settings/filters/parts/tests/filter-actions-rows.test.tsx
@@ -31,56 +31,58 @@ describe('FilterActionsRows', () => {
 			{}
 		);
 	});
-	it('should not display Contact Input when tmpFilter value is different from actionRedirect', async () => {
-		setupTest(
-			<FilterActionRows
-				tmpFilter={{
-					actionStop: [{ flagName: 'flagged' }]
-				}}
-				index={0}
-				compProps={compProps}
-			/>,
-			{}
-		);
-		expect(screen.queryByTestId('filter-action-row-contact-input')).not.toBeInTheDocument();
-	});
-	it('should display Contact Input when selecting option "Redirect To Address"', async () => {
-		setupTest(
-			<FilterActionRows
-				tmpFilter={{
-					actionRedirect: [{ flagName: 'flagged' }]
-				}}
-				index={0}
-				compProps={compProps}
-			/>,
-			{}
-		);
-		await screen.findByTestId('filter-action-row-contact-input');
-	});
-	it('should display Contact Input when selecting option "Redirect To Address"', async () => {
-		const mockSetActions = jest.fn();
-		const mockCompProps = {
-			t: jest.fn(),
-			isIncoming: true,
-			setTempActions: mockSetActions,
-			tempActions: [],
-			zimbraFeatureMailForwardingInFiltersEnabled: 'TRUE' as const
-		};
-		const { user } = setupTest(
-			<FilterActionRows
-				tmpFilter={{
-					actionRedirect: [{ flagName: 'flagged' }]
-				}}
-				index={0}
-				compProps={mockCompProps}
-			/>,
-			{}
-		);
-		const redirectToAddressInput = await screen.findByTestId('filter-action-row-contact-input');
-		await user.type(redirectToAddressInput, 'valid@email.it');
-		await user.type(redirectToAddressInput, '[Enter]');
-		expect(mockSetActions).toHaveBeenCalledWith([
-			expect.objectContaining({ actionRedirect: [{ a: 'valid@email.it' }] })
-		]);
+	describe('Redirect To Address', () => {
+		it('should not display Contact Input when dropdown option is different from "Redirect To Address"', async () => {
+			setupTest(
+				<FilterActionRows
+					tmpFilter={{
+						actionStop: [{ flagName: 'flagged' }]
+					}}
+					index={0}
+					compProps={compProps}
+				/>,
+				{}
+			);
+			expect(screen.queryByTestId('filter-action-row-contact-input')).not.toBeInTheDocument();
+		});
+		it('should display Contact Input when selecting option "Redirect To Address"', async () => {
+			setupTest(
+				<FilterActionRows
+					tmpFilter={{
+						actionRedirect: [{ flagName: 'flagged' }]
+					}}
+					index={0}
+					compProps={compProps}
+				/>,
+				{}
+			);
+			await screen.findByTestId('filter-action-row-contact-input');
+		});
+		it('should update actions after inserting a value in "Redirect To Address" input', async () => {
+			const mockSetActions = jest.fn();
+			const mockCompProps = {
+				t: jest.fn(),
+				isIncoming: true,
+				setTempActions: mockSetActions,
+				tempActions: [],
+				zimbraFeatureMailForwardingInFiltersEnabled: 'TRUE' as const
+			};
+			const { user } = setupTest(
+				<FilterActionRows
+					tmpFilter={{
+						actionRedirect: [{ flagName: 'flagged' }]
+					}}
+					index={0}
+					compProps={mockCompProps}
+				/>,
+				{}
+			);
+			const redirectToAddressInput = await screen.findByTestId('filter-action-row-contact-input');
+			await user.type(redirectToAddressInput, 'valid@email.it');
+			await user.type(redirectToAddressInput, '[Enter]');
+			expect(mockSetActions).toHaveBeenCalledWith([
+				expect.objectContaining({ actionRedirect: [{ a: 'valid@email.it' }] })
+			]);
+		});
 	});
 });
