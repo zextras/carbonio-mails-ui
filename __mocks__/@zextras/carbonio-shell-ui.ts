cac80ee9f697820ad13707296825b2f2bfb62916
Luca Stauble <luca.stauble@gmail.com>
2024-12-11 17:22:20 +0100

    test: add default result for useAuthenticated mock

--- __mocks__/@zextras/carbonio-shell-ui.ts
+++ __mocks__/@zextras/carbonio-shell-ui.ts
@@ -9,7 +9,6 @@ import shell from '@zextras/carbonio-shell-ui';
 export * from '../../src/carbonio-ui-commons/test/mocks/carbonio-shell-ui';
 
 // TODO move it in the Commons submodule
-export const useAuthenticated = jest.fn<
-	ReturnType<typeof shell.useAuthenticated>,
-	Parameters<typeof shell.useAuthenticated>
->();
+export const useAuthenticated = jest
+	.fn<ReturnType<typeof shell.useAuthenticated>, Parameters<typeof shell.useAuthenticated>>()
+	.mockReturnValue(true);
