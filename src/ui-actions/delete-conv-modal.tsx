99781232d1390c0fe3d4994d81801020c42260c2
Keshav Bhatt <keshavnrj@gmail.com>
2024-12-12 17:15:32 +0530

    refactor: use msgActionSoapApi for delete action in delete-conv-modal

--- src/ui-actions/delete-conv-modal.tsx
+++ src/ui-actions/delete-conv-modal.tsx
@@ -8,11 +8,12 @@ import React, { FC, useCallback } from 'react';
 import { Container, Text } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
 
+import { msgActionSoapApi } from '../api/msg-action';
 import ModalFooter from '../carbonio-ui-commons/components/modals/modal-footer';
 import ModalHeader from '../carbonio-ui-commons/components/modals/modal-header';
 import { useAppDispatch } from '../hooks/redux';
 import { useUiUtilities } from '../hooks/use-ui-utilities';
-import { convAction, msgAction } from '../store/actions';
+import { convAction } from '../store/actions';
 
 type DeleteConvConfirmPropType = {
 	selectedIDs: Array<string>;
@@ -31,75 +32,73 @@ const DeleteConvConfirm: FC<DeleteConvConfirmPropType> = ({
 	const { createSnackbar } = useUiUtilities();
 
 	const onConfirmConvDelete = useCallback(() => {
-		dispatch(
-			isMessageView
-				? msgAction({
+		isMessageView
+			? msgActionSoapApi({
+					operation: 'delete',
+					ids: selectedIDs
+				})
+			: dispatch(
+					convAction({
 						operation: 'delete',
 						ids: selectedIDs
 					})
-				: convAction({
-						operation: 'delete',
-						ids: selectedIDs
-					})
-		).then((res) => {
-			if (res.type.includes('fulfilled')) {
-				deselectAll && deselectAll();
-				createSnackbar({
-					key: `trash-${selectedIDs}`,
-					replace: true,
-					severity: 'info',
-					label: t('label.email_perm_deleted', 'E-mail permanently deleted'),
-					autoHideTimeout: 3000,
-					hideButton: true
+				).then((res) => {
+					if (res.type.includes('fulfilled') || !('Fault' in res)) {
+						deselectAll && deselectAll();
+						createSnackbar({
+							key: `trash-${selectedIDs}`,
+							replace: true,
+							severity: 'info',
+							label: t('label.email_perm_deleted', 'E-mail permanently deleted'),
+							autoHideTimeout: 3000,
+							hideButton: true
+						});
+					} else {
+						createSnackbar({
+							key: `edit`,
+							replace: true,
+							severity: 'error',
+							label: t('label.error_try_again', 'Something went wrong, please try again'),
+							autoHideTimeout: 3000
+						});
+					}
+					onClose();
 				});
-			} else {
-				createSnackbar({
-					key: `edit`,
-					replace: true,
-					severity: 'error',
-					label: t('label.error_try_again', 'Something went wrong, please try again'),
-					autoHideTimeout: 3000
-				});
-			}
-			onClose();
-		});
 	}, [dispatch, isMessageView, selectedIDs, onClose, deselectAll, createSnackbar, t]);
 
 	return (
-		<>
+		<Container
+			padding={{ all: 'large' }}
+			mainAlignment="center"
+			crossAlignment="flex-start"
+			height="fit"
+		>
+			<ModalHeader
+				onClose={onClose}
+				title={t(
+					'messages.permanent_delete_title',
+					'Are you sure to permanently delete this element?'
+				)}
+			/>
 			<Container
-				padding={{ all: 'large' }}
+				padding={{ all: 'small' }}
 				mainAlignment="center"
 				crossAlignment="flex-start"
 				height="fit"
 			>
-				<ModalHeader
-					onClose={onClose}
-					title={t(
-						'messages.permanent_delete_title',
-						'Are you sure to permanently delete this element?'
+				<Text overflow="break-word">
+					{t(
+						'messages.permanent_delete_body',
+						'If you permanently delete this element you will not be able to recover it. Continue?'
 					)}
+				</Text>
+				<ModalFooter
+					onConfirm={onConfirmConvDelete}
+					label={t('label.delete_permanently', 'Delete permanently')}
+					background="error"
 				/>
-				<Container
-					padding={{ all: 'small' }}
-					mainAlignment="center"
-					crossAlignment="flex-start"
-					height="fit"
-				>
-					<Text overflow="break-word">
-						{t(
-							'messages.permanent_delete_body',
-							'If you permanently delete this element you will not be able to recover it. Continue?'
-						)}
-					</Text>
-					<ModalFooter
-						onConfirm={onConfirmConvDelete}
-						label={t('label.delete_permanently', 'Delete permanently')}
-						background="error"
-					/>
-				</Container>
 			</Container>
-		</>
+		</Container>
 	);
 };
 
