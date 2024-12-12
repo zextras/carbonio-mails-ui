c18940d711fdfcab14b6cadef0c1354239eb64b1
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-12-12 16:02:52 +0100

    test(message-list): re-enable and update tests
    Re-enabled and updated the tests for the message list component.
    Changed the SPDX copyright year to 2023 and removed commented-out
    code. Updated the test setup to use setMessagesInEmailStore and
    adjusted props accordingly. This ensures the tests are active
    and reflect the current implementation.

--- src/views/app/folder-panel/messages/tests/message-list-component.test.tsx
+++ src/views/app/folder-panel/messages/tests/message-list-component.test.tsx
@@ -1,95 +1,91 @@
 /*
- * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-// /*
-//  * SPDX-FileCopyrightText: 2023 Zextras <https://www.zextras.com>
-//  *
-//  * SPDX-License-Identifier: AGPL-3.0-only
-//  */
-// TODO: CO-1725 re-enable this
 
-// import React from 'react';
-//
-// import { screen } from '@testing-library/react';
-// import { noop, times } from 'lodash';
-//
-// import { FOLDERS } from '../../../../../carbonio-ui-commons/constants/folders';
-// import { setupTest } from '../../../../../carbonio-ui-commons/test/test-setup';
-// import { generateMessage } from '../../../../../tests/generators/generateMessage';
-// import { generateStore } from '../../../../../tests/generators/store';
-// import { MessageListComponent, MessageListComponentProps } from '../message-list-component';
-// import { MessageListItemComponent } from '../message-list-item-component';
-//
-// describe.each`
-// 	type                     | isSearchModule
-// 	${'message list'}        | ${false}
-// 	${'search message list'} | ${true}
-// `('$type component', ({ isSearchModule }) => {
-// 	test('populate a message list and check that the messages are visible', async () => {
-// 		// Populate a message list
-// 		const MESSAGES_COUNT = 100;
-// 		const folderId = FOLDERS.INBOX;
-// 		const messages = times(MESSAGES_COUNT, (index) =>
-// 			generateMessage({ id: `${index}`, folderId })
-// 		);
-//
-// 		const listItems = messages.map((message) => (
-// 			<MessageListItemComponent
-// 				message={message}
-// 				selected={{}}
-// 				isSelected={false}
-// 				active
-// 				toggle={noop}
-// 				isSelectModeOn={false}
-// 				key={message.id}
-// 				isSearchModule={isSearchModule}
-// 				deselectAll={noop}
-// 				visible
-// 			/>
-// 		));
-//
-// 		const props: MessageListComponentProps = {
-// 			deselectAll: noop,
-// 			displayerTitle: 'test',
-// 			folderId,
-// 			isAllSelected: false,
-// 			isSelectModeOn: false,
-// 			listItems,
-// 			messages,
-// 			messagesLoadingCompleted: true,
-// 			selectAll: noop,
-// 			selectAllModeOff: noop,
-// 			selected: {},
-// 			selectedIds: [],
-// 			setIsSelectModeOn: noop,
-// 			isSearchModule,
-// 			totalMessages: messages.length,
-// 			setDraggedIds: noop
-// 		};
-//
-// 		const store = generateStore({
-// 			messages: {
-// 				searchedInFolder: {},
-// 				messages: {
-// 					...messages.map((msg) => ({ [msg.id]: msg }))
-// 				},
-// 				searchRequestStatus: null
-// 			}
-// 		});
-//
-// 		setupTest(<MessageListComponent {...props} />, { store });
-//
-// 		await screen.findByTestId(`message-list-${folderId}`);
-// 		const items = await screen.findAllByTestId(/MessageListItem-/);
-//
-// 		// Test that there is a list item for each message
-// 		expect(items.length).toBe(messages.length);
-//
-// 		// Test that every list item is visible
-// 		items.forEach((item) => {
-// 			expect(item).toBeVisible();
-// 		});
-// 	});
-// });
+import React from 'react';
+
+import { screen } from '@testing-library/react';
+import { noop, times } from 'lodash';
+
+import { FOLDERS } from '../../../../../carbonio-ui-commons/constants/folders';
+import { setupTest } from '../../../../../carbonio-ui-commons/test/test-setup';
+import { setMessagesInEmailStore } from '../../../../../store/zustand/emails/store';
+import { generateMessage } from '../../../../../tests/generators/generateMessage';
+import { generateStore } from '../../../../../tests/generators/store';
+import { MessageListComponent, MessageListComponentProps } from '../message-list-component';
+import { MessageListItemComponent } from '../message-list-item-component';
+
+describe.each`
+	type                     | isSearchModule
+	${'message list'}        | ${false}
+	${'search message list'} | ${true}
+`('$type component', ({ isSearchModule }) => {
+	test('populate a message list and check that the messages are visible', async () => {
+		// Populate a message list
+		const MESSAGES_COUNT = 100;
+		const folderId = FOLDERS.INBOX;
+		const messages = times(MESSAGES_COUNT, (index) =>
+			generateMessage({ id: `${index}`, folderId })
+		);
+
+		setMessagesInEmailStore(messages, false);
+		const listItems = messages.map((message) => (
+			<MessageListItemComponent
+				messageId={message.id}
+				selected={{}}
+				isSelected={false}
+				active
+				toggle={noop}
+				isSelectModeOn={false}
+				key={message.id}
+				isSearchModule={isSearchModule}
+				deselectAll={noop}
+				visible
+			/>
+		));
+
+		const props: MessageListComponentProps = {
+			deselectAll: noop,
+			displayerTitle: 'test',
+			folderId,
+			messageIds: new Set(messages.map((msg) => msg.id)),
+			isAllSelected: false,
+			isSelectModeOn: false,
+			listItems,
+			messagesLoadingCompleted: true,
+			selectAll: noop,
+			selectAllModeOff: noop,
+			selected: {},
+			selectedIds: [],
+			setIsSelectModeOn: noop,
+			isSearchModule,
+			totalMessages: messages.length,
+			setDraggedIds: noop
+		};
+
+		const store = generateStore({
+			messages: {
+				searchedInFolder: {},
+				messages: {
+					...messages.map((msg) => ({ [msg.id]: msg }))
+				},
+				searchRequestStatus: null
+			}
+		});
+
+		setupTest(<MessageListComponent {...props} />, { store });
+
+		await screen.findByTestId(`message-list-${folderId}`);
+		const items = await screen.findAllByTestId(/MessageListItem-/);
+
+		// Test that there is a list item for each message
+		expect(items.length).toBe(messages.length);
+
+		// Test that every list item is visible
+		items.forEach((item) => {
+			expect(item).toBeVisible();
+		});
+	});
+});
