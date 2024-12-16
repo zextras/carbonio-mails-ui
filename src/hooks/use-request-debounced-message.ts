e0d094efe5210b5c0ae73e0cda89fe07bd75ddfe
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-12-16 11:20:00 +0100

    feat(hooks): refactor debounced message request
    Refactored `useRequestDebouncedMessage` to use `getMsg` directly instead of
    
    dispatching `getMsgAsyncThunk`. Removed unused `useAppDispatch` import.
    
    This simplifies the code and reduces dependencies.

--- src/hooks/use-request-debounced-message.ts
+++ src/hooks/use-request-debounced-message.ts
@@ -7,24 +7,22 @@ import { useEffect, useMemo } from 'react';
 
 import { debounce } from 'lodash';
 
-import { useAppDispatch } from './redux';
+import { getMsg } from '../api/helpers/get-msg-service';
 import { DEFAULT_API_DEBOUNCE_TIME } from '../constants';
-import { getMsgAsyncThunk } from '../store/actions';
 
 export const useRequestDebouncedMessage = (messageId: string, isComplete = false): void => {
-	const dispatch = useAppDispatch();
 	const requestDebouncedMessage = useMemo(
 		() =>
 			debounce(
 				() => {
 					if (!isComplete) {
-						dispatch(getMsgAsyncThunk({ msgId: messageId }));
+						getMsg({ msgId: messageId });
 					}
 				},
 				DEFAULT_API_DEBOUNCE_TIME,
 				{ leading: false, trailing: true }
 			),
-		[dispatch, isComplete, messageId]
+		[isComplete, messageId]
 	);
 
 	useEffect(() => {
