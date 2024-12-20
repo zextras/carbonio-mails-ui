1d623cd09ef060d6fac0b49e461ba399f50e60cb
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2024-12-20 11:30:14 +0100

    fix(tests): remove unused store dispatch
    Removed the unused `generateStore` import and store dispatch in the
    `searchConv` test. The test now directly calls `searchConv` without
    dispatching it through the store.

--- src/store/actions/tests/search-conv.test.ts
+++ src/store/actions/tests/search-conv.test.ts
@@ -6,20 +6,14 @@
 
 import { FOLDERS } from '../../../carbonio-ui-commons/constants/folders';
 import { createSoapAPIInterceptor } from '../../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
-import { generateStore } from '../../../tests/generators/store';
 import { type SearchConvRequest } from '../../../types';
 import { searchConv } from '../search-conv';
 
 describe('searchConv', () => {
 	test('the max property is set to 250_000', async () => {
-		const store = generateStore();
 		const interceptor = createSoapAPIInterceptor<SearchConvRequest>('SearchConv');
 
-		store.dispatch(
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			searchConv({ conversationId: '1', folderId: FOLDERS.INBOX, fetch: 'all' })
-		);
+		searchConv({ conversationId: '1', folderId: FOLDERS.INBOX, fetch: 'all' });
 
 		const req = await interceptor;
 		expect(req.max).toBe(250000);
