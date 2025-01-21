7d16590517966fa644c5b1c031defadf9cb00731
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-01-21 12:36:52 +0100

    refactor(emails): enhance conversation deletion logic
    Update handleConvActionResponse to include conversationIndexSlice and
    searchIndexSlice updates when deleting conversations.

--- src/store/emails/slices/populated-items/utils.ts
+++ src/store/emails/slices/populated-items/utils.ts
@@ -219,33 +219,42 @@ function handleConvActionResponse(
 	useEmailsStore: UseBoundStore<StoreApi<EmailsStoreState>>
 ): void {
 	useEmailsStore.setState(
-		produce(({ populatedItemsSlice }: EmailsStoreState) => {
-			if (!response || 'Fault' in response) {
-				forEach(convActionParams.ids, (id: string) => {
-					if (!populatedItemsSlice.conversations?.[id]) return;
-					if (convActionParams.operation === CONVACTIONS.FLAG) {
-						populatedItemsSlice.conversations[id].flagged = true;
-					}
-					if (convActionParams.operation === CONVACTIONS.UNFLAG) {
-						populatedItemsSlice.conversations[id].flagged = false;
-					}
-					if (convActionParams.operation === CONVACTIONS.MARK_READ) {
-						populatedItemsSlice.conversations[id].read = true;
-					}
-					if (convActionParams.operation === CONVACTIONS.MARK_UNREAD) {
-						populatedItemsSlice.conversations[id].read = false;
-					}
-				});
-				return;
+		produce(
+			({ populatedItemsSlice, conversationIndexSlice, searchIndexSlice }: EmailsStoreState) => {
+				if (!response || 'Fault' in response) {
+					forEach(convActionParams.ids, (id: string) => {
+						if (!populatedItemsSlice.conversations?.[id]) return;
+						if (convActionParams.operation === CONVACTIONS.FLAG) {
+							populatedItemsSlice.conversations[id].flagged = true;
+						}
+						if (convActionParams.operation === CONVACTIONS.UNFLAG) {
+							populatedItemsSlice.conversations[id].flagged = false;
+						}
+						if (convActionParams.operation === CONVACTIONS.MARK_READ) {
+							populatedItemsSlice.conversations[id].read = true;
+						}
+						if (convActionParams.operation === CONVACTIONS.MARK_UNREAD) {
+							populatedItemsSlice.conversations[id].read = false;
+						}
+					});
+					return;
+				}
+				if (!response.action) return;
+				const { id, op } = response.action;
+				if (op === CONVACTIONS.DELETE) {
+					id.split(',').forEach((convId) => {
+						delete populatedItemsSlice.conversations[convId];
+						conversationIndexSlice.conversationListIndex = filter(
+							conversationIndexSlice.conversationListIndex,
+							(conv) => conv !== convId
+						);
+						searchIndexSlice.conversationListIndex = searchIndexSlice.conversationListIndex.filter(
+							(conv) => conv !== convId
+						);
+					});
+				}
 			}
-			if (!response.action) return;
-			const { id, op } = response.action;
-			if (op === CONVACTIONS.DELETE) {
-				id.split(',').forEach((convId) => {
-					delete populatedItemsSlice.conversations[convId];
-				});
-			}
-		})
+		)
 	);
 }
 
