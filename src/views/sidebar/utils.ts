161a6a01c3f2df9e8991f461f523bdc6894c2c4c
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-02-04 14:18:24 +0100

    refactor(sidebar): remove unused functions
    Removed unused functions extractFolders and
    getFolderIconColorForAccordionFolder from utils.ts to clean up the code.

--- src/views/sidebar/utils.ts
+++ src/views/sidebar/utils.ts
@@ -5,16 +5,12 @@
  */
 import { type AccordionItemType } from '@zextras/carbonio-design-system';
 import { t } from '@zextras/carbonio-shell-ui';
-import { isNil, omitBy, reduce } from 'lodash';
+import { isNil, omitBy } from 'lodash';
 
 import { ROOT_NAME, ZIMBRA_STANDARD_COLORS } from '../../carbonio-ui-commons/constants';
 import { FOLDERS } from '../../carbonio-ui-commons/constants/folders';
 import { isSystemFolder } from '../../carbonio-ui-commons/helpers/folders';
-import {
-	type AccordionFolder,
-	type Folder,
-	type LinkFolderFields
-} from '../../carbonio-ui-commons/types/folder';
+import { type Folder, type LinkFolderFields } from '../../carbonio-ui-commons/types/folder';
 import { DragEnterAction, OnDropActionProps } from '../../carbonio-ui-commons/types/sidebar';
 import { getFolderIdParts, isDraft, isSpam } from '../../helpers/folders';
 
@@ -47,49 +43,11 @@ export const normalizeFolder = (
 		isNil
 	);
 
-export const extractFolders = (accordion: Array<any>, acc = {}): any =>
-	reduce(
-		accordion,
-		(acc2, folder) => {
-			if (folder.folder) {
-				return (folder.view === 'message' &&
-					folder.id !== FOLDERS.IM_LOGS &&
-					folder.id !== FOLDERS.USER_ROOT) ||
-					folder.id === FOLDERS.TRASH
-					? {
-							...acc2,
-							[folder.id]: normalizeFolder(folder),
-							...extractFolders(folder.folder, acc2)
-						}
-					: { ...acc2, ...extractFolders(folder.folder, acc2) };
-			}
-			return (folder.view === 'message' &&
-				folder.id !== FOLDERS.IM_LOGS &&
-				folder.id !== FOLDERS.USER_ROOT) ||
-				folder.id === FOLDERS.TRASH
-				? {
-						...acc2,
-						[folder.id]: normalizeFolder(folder)
-					}
-				: acc2;
-		},
-		acc
-	);
-
 export const capitalise = (word: string): string => {
 	const asciiRef = word?.charCodeAt(0);
 	const newAsciiRef = asciiRef - 32;
 	const newChar = String.fromCharCode(newAsciiRef);
 	return word ? newChar + word.substring(1) : '';
-};
-
-export const getFolderIconColorForAccordionFolder = (f: AccordionFolder): string => {
-	if (f?.folder?.color) {
-		return f.folder.color < 10
-			? ZIMBRA_STANDARD_COLORS[f.folder.color].hex
-			: (f?.folder.rgb ?? ZIMBRA_STANDARD_COLORS[0].hex);
-	}
-	return ZIMBRA_STANDARD_COLORS[0].hex;
 };
 
 export const getFolderIconColor = (f: Folder | AccordionItemType): string => {
