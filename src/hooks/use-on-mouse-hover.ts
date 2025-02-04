4f76393e2e8db9663c7a24587044177ca0b8c1eb
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-02-04 10:28:02 +0100

    refactor(hooks): remove unused isHovered state
    Removed the isHovered state and its associated event listeners from
    use-on-mouse-hover.ts as they were not being used.

--- src/hooks/use-on-mouse-hover.ts
+++ src/hooks/use-on-mouse-hover.ts
@@ -27,34 +27,28 @@ import { useState, useRef, useEffect, MutableRefObject } from 'react';
 
 type UseOnMouseHoverReturnType = {
 	ref: MutableRefObject<HTMLDivElement | null>;
-	isHovered: boolean;
 	hasBeenHovered: boolean;
 };
 export function useOnMouseHover(): UseOnMouseHoverReturnType {
-	const [isHovered, setIsHovered] = useState(false);
 	const [hasBeenHovered, setHasBeenHovered] = useState(false);
 	const ref = useRef<HTMLDivElement | null>(null);
 
 	useEffect(() => {
 		const handleMouseOver = (): void => {
-			setIsHovered(true);
 			setHasBeenHovered(true);
 		};
-		const handleMouseOut = (): void => setIsHovered(false);
 		const node = ref.current;
 
 		if (node) {
 			node.addEventListener('mouseover', handleMouseOver);
-			node.addEventListener('mouseout', handleMouseOut);
 		}
 
 		return () => {
 			if (node) {
 				node.removeEventListener('mouseover', handleMouseOver);
-				node.removeEventListener('mouseout', handleMouseOut);
 			}
 		};
 	}, []);
 
-	return { ref, isHovered, hasBeenHovered };
+	return { ref, hasBeenHovered };
 }
