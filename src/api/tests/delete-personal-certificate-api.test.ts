f7d4e9ad2cfcbba20df67af91eb0aefaec94b6a5
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2025-02-05 17:07:07 +0530

    test: test for delete certificates

---
+++ src/api/tests/delete-personal-certificate-api.test.ts
@@ -0,0 +1,82 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { deletePersonalCertificate } from '../delete-personal-certificate-api';
+
+describe('deletePersonalCertificate', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+	const apiURL = '/service/extension/encryption/smime/personal';
+	it('should return data when the API call is successful and response is ok', async () => {
+		const mockResponse = { ok: true };
+		global.fetch = jest.fn(() => Promise.resolve(mockResponse)) as jest.Mock;
+
+		const result = await deletePersonalCertificate('test-id', 'test-password');
+		expect(result).toEqual({ data: mockResponse });
+		expect(fetch).toHaveBeenCalledWith(apiURL, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: 'test-id', password: 'test-password' })
+		});
+	});
+
+	it('should return error when the API call is successful but response is not ok with valid error data', async () => {
+		const mockErrorData = { message: 'Error' };
+		global.fetch = jest.fn(() =>
+			Promise.resolve({
+				ok: false,
+				json: () => Promise.resolve(mockErrorData)
+			})
+		) as jest.Mock;
+
+		const result = await deletePersonalCertificate('test-id', 'test-password');
+		expect(result).toEqual({ error: mockErrorData });
+		expect(fetch).toHaveBeenCalledWith(apiURL, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: 'test-id', password: 'test-password' })
+		});
+	});
+
+	it('should return error when the API call is successful but response is not ok with invalid error data', async () => {
+		global.fetch = jest.fn(() =>
+			Promise.resolve({
+				ok: false,
+				json: () => Promise.reject(new Error('Invalid JSON'))
+			})
+		) as jest.Mock;
+
+		const result = await deletePersonalCertificate('test-id', 'test-password');
+		expect(result).toEqual({ error: 'Unknown error occurred' });
+		expect(fetch).toHaveBeenCalledWith(apiURL, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: 'test-id', password: 'test-password' })
+		});
+	});
+
+	it('should return error when the API call fails', async () => {
+		const errorMessage = 'Network error';
+		global.fetch = jest.fn(() => Promise.reject(new Error(errorMessage))) as jest.Mock;
+
+		const result = await deletePersonalCertificate('test-id', 'test-password');
+		expect(result).toEqual({ error: new Error(errorMessage) });
+		expect(fetch).toHaveBeenCalledWith(apiURL, {
+			method: 'DELETE',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: 'test-id', password: 'test-password' })
+		});
+	});
+});
---
+++ src/api/tests/delete-recipient-certificate-api.test.ts
@@ -0,0 +1,52 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { deleteRecipientCertificate } from '../delete-recipient-certificate-api';
+
+describe('deleteRecipientCertificate', () => {
+	const email = 'test@example.com';
+
+	beforeEach(() => {
+		jest.clearAllMocks();
+	});
+
+	it('should return data when the API call is successful', async () => {
+		const mockResponse = { ok: true, status: 200, statusText: 'OK' };
+		global.fetch = jest.fn(() => Promise.resolve(mockResponse)) as jest.Mock;
+
+		const result = await deleteRecipientCertificate(email);
+		expect(result).toEqual({ data: mockResponse });
+	});
+
+	it('should return error when the API call returns a non-OK response', async () => {
+		const mockResponse = { ok: false, status: 404, statusText: 'Not Found' };
+		global.fetch = jest.fn(() => Promise.resolve(mockResponse)) as jest.Mock;
+
+		const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation((message) => {
+			/* mock implementation */
+		});
+
+		const result = await deleteRecipientCertificate(email);
+		expect(result).toEqual({ error: 'Not Found' });
+
+		expect(consoleErrorSpy).toHaveBeenCalledWith('Response not OK:', 404, 'Not Found');
+		consoleErrorSpy.mockRestore();
+	});
+
+	it('should return error when the API call fails', async () => {
+		const errorMessage = 'Network error';
+		global.fetch = jest.fn(() => Promise.reject(new Error(errorMessage))) as jest.Mock;
+
+		const consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation((message) => {
+			/* mock implementation */
+		});
+
+		const result = await deleteRecipientCertificate(email);
+		expect(result).toEqual({ error: new Error(errorMessage) });
+
+		expect(consoleErrorSpy).toHaveBeenCalledWith('Error during fetch:', new Error(errorMessage));
+		consoleErrorSpy.mockRestore();
+	});
+});
