6034d638b941d2b3fa45e1d44e021a2698827b5f
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2025-02-05 15:08:41 +0530

    test: added test

---
+++ src/api/tests/check-exist-password-api.test.ts
@@ -0,0 +1,32 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { HttpResponse } from 'msw';
+
+import { createAPIInterceptor } from '../../carbonio-ui-commons/test/mocks/network/msw/create-api-interceptor';
+import { checkExistEncryptionPassword } from '../check-exist-password-api';
+
+const api = '/service/extension/encryption/password/exist';
+describe('checkExistEncryptionPassword', () => {
+	describe('when the api call succeeds', () => {
+		beforeEach(() => {
+			createAPIInterceptor('get', api, HttpResponse.json({}, { status: 200 }));
+		});
+
+		it('should return data', async () => {
+			expect(await checkExistEncryptionPassword()).toEqual({ data: {} });
+		});
+	});
+
+	describe('when the api call returns non-200 status', () => {
+		beforeEach(() => {
+			createAPIInterceptor('get', api, HttpResponse.json({}, { status: 404 }));
+		});
+
+		it('should return error', async () => {
+			expect(await checkExistEncryptionPassword()).toEqual({ error: '' });
+		});
+	});
+});
