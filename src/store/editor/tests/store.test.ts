88c0562141d8b9b434eb89630094ee0f104575a2
Dhaval Dodiya <dhaval.dodiya@zextras.com>
2025-02-06 12:49:58 +0530

    test: tests of upload certificate and editor store

--- src/store/editor/tests/store.test.ts
+++ src/store/editor/tests/store.test.ts
@@ -71,4 +71,40 @@ describe('store', () => {
 		const newEditor = useEditorsStore.getState().editors[editor.id];
 		expect(newEditor.size).toEqual(123);
 	});
+
+	test('setIsSmimeSign should set the isSmimeSign value for the provided editor id', async () => {
+		const editor = await readyToBeSentEditorTestCase();
+		setupEditorStore({ editors: [editor] });
+
+		useEditorsStore.getState().setIsSmimeSign(editor.id, true);
+
+		const newEditor = useEditorsStore.getState().editors[editor.id];
+		expect(newEditor.isSmimeSign).toEqual(true);
+	});
+
+	test('isSmimeSign should be aligned in the new editor', async () => {
+		const editor = await readyToBeSentEditorTestCase({ isSmimeSign: true });
+		setupEditorStore({ editors: [editor] });
+
+		const newEditor = useEditorsStore.getState().editors[editor.id];
+		expect(newEditor.isSmimeSign).toEqual(true);
+	});
+
+	test('setIsSmimeEncrypt should set the isSmimeEncrypt value for the provided editor id', async () => {
+		const editor = await readyToBeSentEditorTestCase();
+		setupEditorStore({ editors: [editor] });
+
+		useEditorsStore.getState().setIsSmimeEncrypt(editor.id, true);
+
+		const newEditor = useEditorsStore.getState().editors[editor.id];
+		expect(newEditor.isSmimeEncrypt).toEqual(true);
+	});
+
+	test('isSmimeEncrypt should be aligned in the new editor', async () => {
+		const editor = await readyToBeSentEditorTestCase({ isSmimeEncrypt: true });
+		setupEditorStore({ editors: [editor] });
+
+		const newEditor = useEditorsStore.getState().editors[editor.id];
+		expect(newEditor.isSmimeEncrypt).toEqual(true);
+	});
 });
