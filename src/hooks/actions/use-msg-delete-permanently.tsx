5021e96491d2de42dfaa8f3c9a7888e28407c696
Davide Frison <davide.frison@zextras.com>
2025-02-14 12:57:19 +0100

    refactor: define permanently-delete-modal.tsx
    - reuse UI and pass onDeleteConfirm

--- src/hooks/actions/use-msg-delete-permanently.tsx
+++ src/hooks/actions/use-msg-delete-permanently.tsx
@@ -5,12 +5,14 @@
  */
 import React, { useCallback, useMemo } from 'react';
 
+import { useSnackbar } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
 
 import { MessageActionsDescriptors } from '../../constants';
 import { isSpam, isTrash } from '../../helpers/folders';
+import { msgActionEmailStoreAction } from '../../store/emails/actions/msg-action-action';
 import { ActionFn, UIActionDescriptor } from '../../types';
-import { DeleteConvConfirm } from '../../ui-actions/delete-conv-modal';
+import { PermanentlyDeleteModal } from '../../ui-actions/permanently-delete-modal';
 import { useUiUtilities } from '../use-ui-utilities';
 
 type MsgDeletePermanentlyFunctionsParameter = {
@@ -25,26 +27,58 @@ export const useMsgDeletePermanentlyFn = ({
 	folderId
 }: MsgDeletePermanentlyFunctionsParameter): ActionFn => {
 	const { createModal, closeModal } = useUiUtilities();
+	const createSnackbar = useSnackbar();
+	const [t] = useTranslation();
+
+	const deleteMessage = useCallback(
+		async (onClose: () => void) => {
+			const response = await msgActionEmailStoreAction({
+				operation: 'delete',
+				ids
+			});
+			if (!('Fault' in response)) {
+				deselectAll?.();
+				createSnackbar({
+					key: `trash-${ids}`,
+					replace: true,
+					severity: 'info',
+					label: t('label.email_perm_deleted', 'E-mail permanently deleted'),
+					autoHideTimeout: 3000,
+					hideButton: true
+				});
+			} else {
+				createSnackbar({
+					key: `edit`,
+					replace: true,
+					severity: 'error',
+					label: t('label.error_try_again', 'Something went wrong, please try again'),
+					autoHideTimeout: 3000
+				});
+			}
+			onClose();
+		},
+		[ids, deselectAll, createSnackbar, t]
+	);
 
 	const canExecute = useCallback((): boolean => isTrash(folderId) || isSpam(folderId), [folderId]);
 	const execute = useCallback((): void => {
 		if (canExecute()) {
 			const modalId = Date.now().toString();
+			const closeModalFn = (): void => closeModal(modalId);
 			createModal(
 				{
 					id: modalId,
 					children: (
-						<DeleteConvConfirm
-							selectedIDs={ids}
-							onClose={(): void => closeModal(modalId)}
-							deselectAll={deselectAll || ((): null => null)}
+						<PermanentlyDeleteModal
+							onClose={closeModalFn}
+							onDeleteConfirm={() => deleteMessage(closeModalFn)}
 						/>
 					)
 				},
 				true
 			);
 		}
-	}, [canExecute, closeModal, createModal, deselectAll, ids]);
+	}, [canExecute, closeModal, createModal, deleteMessage]);
 
 	return useMemo(() => ({ canExecute, execute }), [canExecute, execute]);
 };
