ab225cf582f2f96b3a490cc4e4cd847ee21f8f99
Davide Frison <davide.frison@zextras.com>
2025-02-14 12:57:19 +0100

    refactor: define permanently-delete-modal.tsx
    - reuse UI and pass onDeleteConfirm

--- src/hooks/actions/use-conv-delete-permanently.tsx
+++ src/hooks/actions/use-conv-delete-permanently.tsx
@@ -5,13 +5,14 @@
  */
 import React, { useCallback, useMemo } from 'react';
 
-import { useModal } from '@zextras/carbonio-design-system';
+import { useModal, useSnackbar } from '@zextras/carbonio-design-system';
 import { useTranslation } from 'react-i18next';
 
 import { ConversationActionsDescriptors } from '../../constants';
 import { isSpam, isTrash } from '../../helpers/folders';
+import { convActionEmailStoreAction } from '../../store/emails/actions/conv-action-action';
 import { ActionFn, UIActionDescriptor } from '../../types';
-import { DeleteConvConfirm } from '../../ui-actions/delete-conv-modal';
+import { PermanentlyDeleteModal } from '../../ui-actions/permanently-delete-modal';
 
 type ConvDeletePermanentlyFunctionsParameter = {
 	ids: Array<string>;
@@ -28,24 +29,57 @@ export const useConvDeletePermanentlyFn = ({
 
 	const canExecute = useCallback((): boolean => isTrash(folderId) || isSpam(folderId), [folderId]);
 
+	const createSnackbar = useSnackbar();
+	const [t] = useTranslation();
+
+	const deleteConversation = useCallback(
+		async (onClose: () => void) => {
+			const response = await convActionEmailStoreAction({
+				operation: 'delete',
+				ids
+			});
+			if (!('Fault' in response)) {
+				deselectAll?.();
+				createSnackbar({
+					key: `trash-${ids}`,
+					replace: true,
+					severity: 'info',
+					label: t('label.email_perm_deleted', 'E-mail permanently deleted'),
+					autoHideTimeout: 3000,
+					hideButton: true
+				});
+			} else {
+				createSnackbar({
+					key: `edit`,
+					replace: true,
+					severity: 'error',
+					label: t('label.error_try_again', 'Something went wrong, please try again'),
+					autoHideTimeout: 3000
+				});
+			}
+			onClose();
+		},
+		[ids, deselectAll, createSnackbar, t]
+	);
+
 	const execute = useCallback((): void => {
 		if (canExecute()) {
-			const id = Date.now().toString();
+			const id = 'permanently-delete-conversation-modal';
+			const closeModalFn = (): void => closeModal(id);
 			createModal(
 				{
 					id,
 					children: (
-						<DeleteConvConfirm
-							selectedIDs={ids}
-							onClose={(): void => closeModal(id)}
-							deselectAll={deselectAll}
+						<PermanentlyDeleteModal
+							onClose={closeModalFn}
+							onDeleteConfirm={() => deleteConversation(closeModalFn)}
 						/>
 					)
 				},
 				true
 			);
 		}
-	}, [canExecute, closeModal, createModal, deselectAll, ids]);
+	}, [canExecute, closeModal, createModal, deleteConversation]);
 
 	return useMemo(() => ({ canExecute, execute }), [canExecute, execute]);
 };
@@ -60,6 +94,7 @@ export const useConvDeletePermanentlyDescriptor = ({
 		folderId,
 		deselectAll
 	});
+
 	const [t] = useTranslation();
 	return {
 		id: ConversationActionsDescriptors.DELETE_PERMANENTLY.id,
