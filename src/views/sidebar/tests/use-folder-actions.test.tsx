5cfb1797779b4f8e9dadc493a9e12a4a7a2e99ad
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-02-17 18:02:18 +0100

    test(sidebar): add tests for folder actions
    - Add tests for disabling actions in drafts folder
    - Add tests for disabling actions in inbox and sent folders
    - Add tests for disabling actions in spam folder
    - Add tests for disabling actions in trash folder
    - Add tests for disabling actions in folders inside trash folder

---
+++ src/views/sidebar/tests/use-folder-actions.test.tsx
@@ -0,0 +1,374 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import React, { act } from 'react';
+
+import { renderHook, waitFor } from '@testing-library/react';
+import { useModal } from '@zextras/carbonio-design-system';
+import { t, useAppContext } from '@zextras/carbonio-shell-ui';
+import { startsWith } from 'lodash';
+
+import { folderActionSoapApi } from '../../../api/folder-action-soap-api';
+import { FolderActionsType, FOLDERS } from '../../../carbonio-ui-commons/constants/folders';
+import { generateFolder } from '../../../carbonio-ui-commons/test/mocks/folders/folders-generator';
+import { allowedActionOnSharedAccount } from '../../../carbonio-ui-commons/utils/utils';
+import { getFolderIdParts } from '../../../helpers/folders';
+import { setMessagesInEmailStore } from '../../../store/emails/store';
+import { populateMessagesInEmailStore } from '../../../tests/generators/generateMessage';
+import { Folder } from '../../../types';
+import { FolderActionsProps } from '../../../types/sidebar';
+import { SelectFolderModal } from '../../../ui-actions/modals/select-folder-modal';
+import { EmptyModal } from '../empty-modal';
+import { NewModal } from '../new-modal';
+import { useFolderActions } from '../use-folder-actions';
+
+jest.mock('@zextras/carbonio-design-system', () => ({
+	...jest.requireActual('@zextras/carbonio-design-system'),
+	useModal: jest.fn()
+}));
+
+jest.mock('../delete-modal');
+jest.mock('../edit-modal');
+jest.mock('../empty-modal');
+jest.mock('../new-modal');
+jest.mock('../shares-info-modal');
+jest.mock('../../../api/folder-action-soap-api');
+// jest.mock('../../../carbonio-ui-commons/utils/utils');
+// jest.mock('../../../helpers/folders');
+// jest.mock('../../../hooks/use-selection');
+// jest.mock('../../../hooks/use-ui-utilities');
+// jest.mock('../../../store/emails/store');
+// jest.mock('../../../ui-actions/modals/select-folder-modal');
+// jest.mock('../../../ui-actions/move-msg');
+
+const useAppContextMock = useAppContext as jest.Mock;
+const startsWithMock = startsWith as jest.Mock;
+const folderActionSoapApiMock = folderActionSoapApi as jest.Mock;
+const allowedActionOnSharedAccountMock = allowedActionOnSharedAccount as jest.Mock;
+const getFolderIdPartsMock = getFolderIdParts as jest.Mock;
+// const useSelectionMock = useSelection as jest.Mock;
+// const useUiUtilitiesMock = useUiUtilities as jest.Mock;
+
+const closeModalMock = jest.fn();
+// useModalMock.mockReturnValue({
+// 	createModal: createModalMock,
+// 	closeModal: closeModalMock
+// });
+//
+const setCountMock = jest.fn();
+useAppContextMock.mockReturnValue({
+	setCount: setCountMock
+});
+
+// const deselectAllMock = jest.fn();
+// useSelectionMock.mockReturnValue({
+// 	deselectAll: deselectAllMock
+// });
+
+// const createSnackbarMock = jest.fn();
+// useUiUtilitiesMock.mockReturnValue({
+// 	createSnackbar: createSnackbarMock
+// });
+
+const defaultFolder = generateFolder({ id: FOLDERS.INBOX });
+
+describe('useFolderActions', () => {
+	beforeEach(() => {
+		jest.clearAllMocks();
+		// allowedActionOnSharedAccountMock.mockReturnValue(true);
+		// getFolderIdPartsMock.mockReturnValue({ id: '1' });
+	});
+
+	it('should return the correct actions for the inbox folder', async () => {
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const { result } = renderHook(() => useFolderActions(defaultFolder));
+
+		expect(result.current).toEqual([
+			{
+				id: FolderActionsType.NEW,
+				'data-testid': `folder-action-${FolderActionsType.NEW}`,
+				icon: 'FolderAddOutline',
+				label: t('label.new_folder', 'New Folder'),
+				onClick: expect.any(Function),
+				disabled: false,
+				tooltipLabel: ''
+			},
+			{
+				id: FolderActionsType.MOVE,
+				'data-testid': `folder-action-${FolderActionsType.MOVE}`,
+				disabled: true,
+				icon: 'MoveOutline',
+				label: t('label.move', 'Move'),
+				onClick: expect.any(Function)
+			},
+			{
+				id: FolderActionsType.EMPTY,
+				'data-testid': `folder-action-${FolderActionsType.EMPTY}`,
+				icon: 'EmptyFolderOutline',
+				label: t('folder_panel.action.wipe.folder_panel', 'Wipe Folder'),
+				disabled: true,
+				onClick: expect.any(Function)
+			},
+			{
+				id: FolderActionsType.EDIT,
+				'data-testid': `folder-action-${FolderActionsType.EDIT}`,
+				icon: 'Edit2Outline',
+				label: t('label.edit', 'Edit'),
+				onClick: expect.any(Function)
+			},
+			{
+				id: FolderActionsType.DELETE,
+				'data-testid': `folder-action-${FolderActionsType.DELETE}`,
+				disabled: true,
+				icon: 'Trash2Outline',
+				label: t('label.delete', 'Delete'),
+				onClick: expect.any(Function)
+			},
+			{
+				'data-testid': 'folder-action-read',
+				icon: 'EmailReadOutline',
+				id: 'read',
+				label: 'label.mark_all_as_read',
+				onClick: expect.any(Function)
+			}
+		]);
+	});
+
+	it('should return the correct actions for a shared folder', async () => {
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const folder = {
+			...defaultFolder,
+			id: 'shared:1',
+			isLink: true
+		} as Folder;
+
+		const { result } = renderHook(() => useFolderActions(folder));
+		expect(result.current).toEqual([
+			{
+				id: FolderActionsType.EDIT,
+				'data-testid': `folder-action-${FolderActionsType.EDIT}`,
+				icon: 'Edit2Outline',
+				label: t('folder_panel.action.edit_properties', 'Edit Properties'),
+				onClick: expect.any(Function)
+			},
+			{
+				id: FolderActionsType.REMOVE_FROM_LIST,
+				'data-testid': `folder-action-${FolderActionsType.REMOVE_FROM_LIST}`,
+				icon: 'CloseOutline',
+				label: t('label.remove_from_this_list', 'Remove from this list'),
+				onClick: expect.any(Function)
+			},
+			{
+				id: FolderActionsType.SHARES_INFO,
+				'data-testid': `folder-action-${FolderActionsType.SHARES_INFO}`,
+				icon: 'InfoOutline',
+				label: t('label.shares_info', `Shared folder's info`),
+				onClick: expect.any(Function)
+			}
+		]);
+	});
+
+	it('should disable the new action if the user does not have permission', async () => {
+		const folderWithReadPermissionsOnly = { ...defaultFolder, perm: 'r' };
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const { result } = renderHook(() => useFolderActions(folderWithReadPermissionsOnly));
+		expect(result.current[0].disabled).toBe(true);
+	});
+
+	it('should call the createModal function with the correct parameters when the NEW action is clicked', async () => {
+		const createModalSpy = jest.fn();
+		(useModal as jest.Mock).mockImplementation(() => ({
+			createModal: createModalSpy
+		}));
+		const event = {
+			stopPropagation: jest.fn
+		} as unknown as React.SyntheticEvent<HTMLElement>;
+
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const { result: actions } = renderHook(() => useFolderActions(defaultFolder));
+		const newAction = actions.current.find(
+			(action) => action.id === FolderActionsType.NEW
+		) as FolderActionsProps;
+
+		act(() => {
+			newAction.onClick(event);
+		});
+
+		expect(createModalSpy).toHaveBeenCalledTimes(1);
+		const modal = <NewModal folder={defaultFolder} onClose={expect.any(Function)} />;
+		expect(createModalSpy).toHaveBeenCalledWith(
+			expect.objectContaining({
+				maxHeight: '90vh',
+				size: 'medium',
+				children: modal
+			}),
+			true
+		);
+	});
+
+	it('should call the createModal function with the correct parameters when the MOVE action is clicked', async () => {
+		const createModalSpy = jest.fn();
+		(useModal as jest.Mock).mockImplementation(() => ({ createModal: createModalSpy }));
+		const event = {
+			stopPropagation: jest.fn
+		} as unknown as React.SyntheticEvent<HTMLElement>;
+
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const { result: actions } = renderHook(() => useFolderActions(defaultFolder));
+		const moveAction = actions.current.find(
+			(action) => action.id === FolderActionsType.MOVE
+		) as FolderActionsProps;
+
+		act(() => {
+			moveAction.onClick(event);
+		});
+
+		expect(createModalSpy).toHaveBeenCalledTimes(1);
+		const modal = (
+			<SelectFolderModal
+				folder={defaultFolder}
+				onClose={expect.any(Function)}
+				headerTitle={expect.any(String)}
+				actionLabel={expect.any(String)}
+				inputLabel={expect.any(String)}
+				confirmAction={expect.any(Function)}
+				allowFolderCreation={expect.any(Boolean)}
+				allowRootSelection={expect.any(Boolean)}
+				showSharedAccounts={expect.any(Boolean)}
+				showTrashFolder={expect.any(Boolean)}
+				showSpamFolder={expect.any(Boolean)}
+			/>
+		);
+
+		expect(createModalSpy).toHaveBeenCalledWith(
+			expect.objectContaining({
+				maxHeight: '90vh',
+				size: 'medium',
+				children: modal
+			}),
+			true
+		);
+	});
+	it('should call the EMPTY function with the correct parameters when the MOVE action is clicked', async () => {
+		const createModalSpy = jest.fn();
+		(useModal as jest.Mock).mockImplementation(() => ({ createModal: createModalSpy }));
+		const event = {
+			stopPropagation: jest.fn
+		} as unknown as React.SyntheticEvent<HTMLElement>;
+
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const { result: actions } = renderHook(() => useFolderActions(defaultFolder));
+		const moveAction = actions.current.find(
+			(action) => action.id === FolderActionsType.EMPTY
+		) as FolderActionsProps;
+
+		act(() => {
+			moveAction.onClick(event);
+		});
+
+		expect(createModalSpy).toHaveBeenCalledTimes(1);
+		const modal = <EmptyModal folder={defaultFolder} onClose={expect.any(Function)} />;
+
+		expect(createModalSpy).toHaveBeenCalledWith(
+			expect.objectContaining({
+				children: modal
+			}),
+			true
+		);
+	});
+
+	it('should call the empty function with the correct parameters when the EMPTY action is clicked', async () => {
+		const createModalSpy = jest.fn();
+		(useModal as jest.Mock).mockImplementation(() => ({
+			createModal: createModalSpy
+		}));
+		const event = {
+			stopPropagation: jest.fn
+		} as unknown as React.SyntheticEvent<HTMLElement>;
+
+		const messages = await waitFor(() =>
+			populateMessagesInEmailStore({
+				messageGeneratorParams: [{ id: '1', folderId: FOLDERS.INBOX }]
+			})
+		);
+		await waitFor(() => setMessagesInEmailStore(messages, false));
+
+		const { result: actions } = renderHook(() => useFolderActions(defaultFolder));
+		const emptyAction = actions.current.find(
+			(action) => action.id === FolderActionsType.EMPTY
+		) as FolderActionsProps;
+
+		act(() => {
+			emptyAction.onClick(event);
+		});
+
+		expect(createModalSpy).toHaveBeenCalledTimes(1);
+		const modal = <EmptyModal folder={defaultFolder} onClose={expect.any(Function)} />;
+
+		expect(createModalSpy).toHaveBeenCalledWith(
+			{
+				id: expect.any(String),
+				children: modal
+			},
+			true
+		);
+	});
+
+	it('should call the createModal function with the correct parameters when the edit action is clicked', () => {});
+
+	it('should call the createModal function with the correct parameters when the delete action is clicked', () => {});
+
+	it('should call the folderActionSoapApi function when the remove from list action is clicked', () => {});
+
+	it('should call the createModal function with the correct parameters when the shares info action is clicked', () => {});
+
+	it('should call the folderActionSoapApi function when the mark all read action is clicked', () => {});
+
+	it('should disable the move and delete actions for the inbox and sent folders', () => {});
+
+	it('should disable the move, delete and mark all read actions for the drafts folder', () => {});
+
+	it('should disable the new, move and delete actions for the spam folder', () => {});
+
+	it('should disable the move, delete and edit actions for the trash folder', () => {});
+
+	it('should disable the new and edit actions for folders inside the trash folder', () => {});
+});
