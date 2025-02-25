0b1bcda87ee29fb1a9dda12ea94ac5a2c5504ee3
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2025-02-25 10:31:58 +0100

    feat: added more tests for StandalonePreviewPanel

--- src/views/app/detail-panel/tests/standalone-preview-panel.test.tsx
+++ src/views/app/detail-panel/tests/standalone-preview-panel.test.tsx
@@ -8,9 +8,13 @@ import React from 'react';
 import { screen } from '@testing-library/react';
 import { useParams } from 'react-router-dom';
 
+import { FOLDERS } from '../../../../carbonio-ui-commons/constants/folders';
 import { setupTest } from '../../../../carbonio-ui-commons/test/test-setup';
 import { API_REQUEST_STATUS } from '../../../../constants';
-import { useCompleteMessageOrFetch } from '../../../../store/emails/hooks/hooks';
+import {
+	useCompleteConversationOrFetch,
+	useCompleteMessageOrFetch
+} from '../../../../store/emails/hooks/hooks';
 import { updateMessageStatus } from '../../../../store/emails/store';
 import { useExtraWindow } from '../../extra-windows/use-extra-window';
 import { StandalonePreviewPanel } from '../standalone-preview-panel';
@@ -24,23 +28,41 @@ jest.mock('react-router-dom', () => ({
 
 describe('StandalonePreviewPanel', () => {
 	const mockUseCompleteMessageOrFetch = useCompleteMessageOrFetch as jest.Mock;
+	const mockUseCompleteConversationOrFetch = useCompleteConversationOrFetch as jest.Mock;
 	const mockUseExtraWindow = useExtraWindow as jest.Mock;
 	afterEach(() => {
 		jest.clearAllMocks();
 	});
 
-	it('renders message preview when type is preview', () => {
+	it('renders message preview when type is message', () => {
 		(useParams as jest.Mock).mockReturnValue({
 			folderId: '1',
 			type: 'message',
 			itemId: '1'
 		});
-		const message = { isComplete: true, read: true, subject: 'Test Subject' };
+		const message = { isComplete: true, read: true, subject: 'Test Subject', id: '1' };
 		updateMessageStatus('1', API_REQUEST_STATUS.fulfilled);
 		mockUseCompleteMessageOrFetch.mockReturnValue({ message });
 		mockUseExtraWindow.mockReturnValue({ isInsideExtraWindow: false });
 		setupTest(<StandalonePreviewPanel />);
+		expect(screen.getByTestId('MailPreview-1')).toBeInTheDocument();
+	});
 
-		expect(screen.getByText('Test Subject')).toBeInTheDocument();
+	it('renders conversation preview when type is conversation', () => {
+		(useParams as jest.Mock).mockReturnValue({
+			folderId: FOLDERS.TRASH,
+			type: 'conversation',
+			itemId: '1'
+		});
+		const conversation = {
+			conversation: {
+				messageIds: ['1']
+			},
+			conversationStatus: API_REQUEST_STATUS.fulfilled
+		};
+		mockUseCompleteConversationOrFetch.mockReturnValue(conversation);
+		mockUseExtraWindow.mockReturnValue({ isInsideExtraWindow: false });
+		setupTest(<StandalonePreviewPanel />);
+		expect(screen.getByTestId('ConversationMessagePreview-1')).toBeInTheDocument();
 	});
 });
