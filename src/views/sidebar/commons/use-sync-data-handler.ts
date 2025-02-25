b91ccd5618b0495a05d4d3616736bc4992faeea3
Davide Frison <davide.frison@zextras.com>
2025-02-25 13:08:07 +0100

    fix: reintroduce check on deletedIds length

--- src/views/sidebar/commons/use-sync-data-handler.ts
+++ src/views/sidebar/commons/use-sync-data-handler.ts
@@ -47,7 +47,7 @@ function handleFoldersNotify({
 		!isEmpty(notifyList) &&
 		(notify?.created?.folder ||
 			notify?.modified?.folder ||
-			notify?.deleted ||
+			notify?.deleted?.length > 0 ||
 			notify?.created?.link ||
 			notify?.modified?.link);
 
