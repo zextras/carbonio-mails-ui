0479b64ae73ca64b9b464213377ac9f3ebe968b1
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-04-02 15:46:14 +0200

    test(editor-paste-handler): add tests

---
+++ src/views/app/detail-panel/edit/parts/test/editor-paste-handler.test.ts
@@ -0,0 +1,48 @@
+/*
+ * SPDX-FileCopyrightText: 2021 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import { type Editor } from 'tinymce';
+
+import { handleEditorPaste } from '../editor-paste-handler';
+
+const createMockEditor = (): Editor =>
+	({
+		insertContent: jest.fn(),
+		setProgressState: jest.fn()
+	}) as unknown as Editor;
+
+jest.mock('axios');
+
+describe('handleEditorPaste', () => {
+	const defaultClipboardEvent = {
+		preventDefault: jest.fn(),
+		clipboardData: {
+			items: [
+				{
+					type: 'image/png',
+					getAsFile: jest.fn(() => new File(['dummy content'], 'test.png', { type: 'image/png' }))
+				}
+			]
+		}
+	} as unknown as ClipboardEvent;
+
+	it('should return early if clipboardData is missing', () => {
+		const editor = createMockEditor();
+		const event = { ...defaultClipboardEvent, clipboardData: null };
+		handleEditorPaste(editor, 'editor-1', event);
+		expect(event.preventDefault).not.toHaveBeenCalled();
+	});
+
+	it('should return early if there are no valid images', () => {
+		const editor = createMockEditor();
+		const event = {
+			...defaultClipboardEvent,
+			clipboardData: { items: [{ type: 'text/plain', getAsFile: jest.fn(() => null) }] }
+		} as unknown as ClipboardEvent;
+		handleEditorPaste(editor, 'editor-1', event);
+		expect(event.preventDefault).not.toHaveBeenCalled();
+	});
+});
