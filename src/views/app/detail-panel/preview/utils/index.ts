684ea15fc22283e3253c94bb83d41aad92d9b3cb
Davide Frison <36330277+frisonisland@users.noreply.github.com>
2025-04-03 16:32:49 +0200

    feat: [CO-1954] add locale as lang_tag query parameter when previewing a document (#849)
     Requires: zextras/carbonio-mailbox#687
    
    * feat: add lang_tag to document preview query
    
    * test: update document preview test
    
    * fix: ensure lang_tag in document preview link uses hyphen instead of underscore
    
    ---------
    
    Co-authored-by: Giuliano <75381134+giuliano176@users.noreply.github.com>
    Co-authored-by: Keshav Bhatt <keshavnrj@gmail.com>
    Co-authored-by: Keshav Bhatt <keshavbhatt@users.noreply.github.com>

--- src/views/app/detail-panel/preview/utils/index.ts
+++ src/views/app/detail-panel/preview/utils/index.ts
@@ -7,6 +7,7 @@ import { includes, uniqBy } from 'lodash';
 import { DefaultTheme } from 'styled-components';
 
 import { calcColor, getFileExtension } from '../../../../../commons/utilities';
+import { getUserLocale } from '../../../../../commons/utils';
 import {
 	AttachmentPart,
 	EditorAttachmentFiles,
@@ -64,7 +65,9 @@ export const getAttachmentsLink = ({
 			attachmentType
 		)
 	) {
-		return `${getLocationOrigin()}/service/preview/document/${messageId}/${attachments.join(',')}`;
+		let localeForPreview = getUserLocale();
+		localeForPreview = localeForPreview.replace(/_/g, '-');
+		return `${getLocationOrigin()}/service/preview/document/${messageId}/${attachments.join(',')}?lang_tag=${localeForPreview}`;
 	}
 	return `${getLocationOrigin()}/service/home/~/?auth=co&id=${messageId}&part=${attachments.join(
 		','
