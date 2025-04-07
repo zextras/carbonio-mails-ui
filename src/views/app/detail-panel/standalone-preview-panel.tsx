d4a647467427e7d9920639c68374a8f7eeeb1fd8
drazen04 <hangtime23@hotmail.it>
2025-04-07 10:17:43 +0200

    remove old router switch logic

--- src/views/app/detail-panel/standalone-preview-panel.tsx
+++ src/views/app/detail-panel/standalone-preview-panel.tsx
@@ -6,30 +6,10 @@
 
 import React from 'react';
 
-import { Route, Routes, useParams } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import { ConversationPreviewPanelContainer } from './conversation-preview-panel-container';
-import { MessagePreviewPanel } from './message-preview-panel';
 import { MessagePreviewPanelContainer } from './message-preview-panel-container';
-
-// TODO: StandalonePreviewPanelWrapper hold the logic so StandalonePreviewPanel can be removed when tests are fixed
-export function StandalonePreviewPanel(): React.JSX.Element {
-	const { folderId, type, itemId } = useParams<{
-		folderId: string;
-		type: string;
-		itemId: string;
-	}>();
-
-	// FIXME: Maybe there is a better way to handle this
-	// 	- Maybe Route fallback
-	if (!folderId || !type || !itemId) {
-		throw new Error('Missing route parameters');
-	}
-
-	if (type === 'message') return <MessagePreviewPanel folderId={folderId} messageId={itemId} />;
-	if (type === 'conversation') return <ConversationPreviewPanelContainer />;
-	return <span>{`Unknown type ${type}`}</span>;
-}
 
 export default function StandalonePreviewPanelWrapper(): React.JSX.Element {
 	return (
