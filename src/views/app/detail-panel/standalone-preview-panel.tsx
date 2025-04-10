fe1fa3e11620856cb64e3928c472b1e1fedafc40
nubsthead <nubsthead3@gmail.com>
2025-04-10 11:01:54 +0200

    fix: add modal manager to the new entrypoint

--- src/views/app/detail-panel/standalone-preview-panel.tsx
+++ src/views/app/detail-panel/standalone-preview-panel.tsx
@@ -6,6 +6,7 @@
 
 import React from 'react';
 
+import { ModalManager } from '@zextras/carbonio-design-system';
 import { Route, Routes } from 'react-router-dom';
 
 import { ConversationPreviewPanelContainer } from './conversation-preview-panel-container';
@@ -13,15 +14,17 @@ import { MessagePreviewPanelContainer } from './message-preview-panel-container'
 
 export default function StandalonePreviewPanel(): React.JSX.Element {
 	return (
-		<Routes>
-			<Route
-				path={`folder/:folderId/conversation/:conversationId`}
-				element={<ConversationPreviewPanelContainer />}
-			/>
-			<Route
-				path={`folder/:folderId/message/:messageId`}
-				element={<MessagePreviewPanelContainer />}
-			/>
-		</Routes>
+		<ModalManager>
+			<Routes>
+				<Route
+					path={`folder/:folderId/conversation/:conversationId`}
+					element={<ConversationPreviewPanelContainer />}
+				/>
+				<Route
+					path={`folder/:folderId/message/:messageId/:part?`}
+					element={<MessagePreviewPanelContainer />}
+				/>
+			</Routes>
+		</ModalManager>
 	);
 }
