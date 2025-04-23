1eb1a73e47dd34057bc755bb8210cafb79f7aedb
nubsthead <nubsthead3@gmail.com>
2025-04-23 10:34:39 +0200

    fix: apply editor height to fit content

--- src/views/app/detail-panel/edit/edit-view.tsx
+++ src/views/app/detail-panel/edit/edit-view.tsx
@@ -546,6 +546,7 @@ export const EditView = React.forwardRef<EditViewHandle, EditViewProp>(function 
 		<Container
 			data-testid={'edit-view-editor'}
 			mainAlignment={flexStart}
+			height={'fit'}
 			crossAlignment={flexStart}
 			padding={{ all: 'large' }}
 			background={'gray5'}
