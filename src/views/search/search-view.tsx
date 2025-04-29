eeeb1a6e4633fed0eea3f2d6b88c4ea6b4b6b89d
Yuliya <yuliya.aheeva@mail.ru>
2025-04-29 14:47:36 +0200

    chore: search results depends on query

--- src/views/search/search-view.tsx
+++ src/views/search/search-view.tsx
@@ -107,7 +107,7 @@ const SearchView = ({
 								) : (
 									<SearchConversationList
 										searchDisabled={searchDisabled}
-										searchResults={searchResults.conversationListIndex}
+										searchResults={query.length > 0 ? searchResults.conversationListIndex : []}
 										query={queryToString}
 										loading={loading}
 										setShowAdvanceFilters={setShowAdvanceFilters}
