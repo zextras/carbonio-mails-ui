ecdf18e2541f34ae09ab8234d33d5fd2d7a1292f
FrancescoGottardi <gottardifrancesco92@gmail.com>
2025-05-06 14:49:15 +0200

    fix: conflicts

--- src/api/restore-messages-api.ts
+++ src/api/restore-messages-api.ts
@@ -6,20 +6,12 @@
 
 export async function restoreMessagesApi(
 	messages: Array<string>
-<<<<<<< Updated upstream
 ): Promise<object | { error: unknown }> {
-=======
-): Promise<Response | { error: unknown }> {
->>>>>>> Stashed changes
 	return fetch(`/zx/backup/v1/restoreMessages`, {
 		method: 'POST',
 		credentials: 'same-origin',
 		body: JSON.stringify({ messages })
-<<<<<<< Updated upstream
 	})
 		.then(() => ({}))
 		.catch((error) => ({ error }));
-=======
-	}).catch((error) => ({ error }));
->>>>>>> Stashed changes
 }
