109d572d0563a9b905b978a31e05556eea7cdb3f
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-05-19 12:57:31 +0200

    refactor(sync-data-handler-store.test.ts): remove unused notification mocks
    Removes redundant triggerNotification mocks from tests in
    sync-data-handler-store.test.ts to simplify test setup and focus on
    message handling logic.

--- src/store/emails/sync-data-handler/tests/sync-data-handler-store.test.ts
+++ src/store/emails/sync-data-handler/tests/sync-data-handler-store.test.ts
@@ -30,11 +30,6 @@ jest.mock('../../../../carbonio-ui-commons/store/zustand/tags', () => ({
 describe('handleNotifyMessagesCreated', () => {
 	describe('addMessagesToMessageSlice', () => {
 		it('should add messages to populatedItemsSlice.messages', async () => {
-			const triggerNotificationSpy = jest.fn();
-			jest
-				.spyOn(triggerNotification, 'triggerNotification')
-				.mockImplementation(triggerNotificationSpy);
-
 			const message = generateMessage({ id: '1' });
 			setMessagesInEmailStore([message], false);
 			const newMessage = generateMessage({ id: '2' });
@@ -46,11 +41,6 @@ describe('handleNotifyMessagesCreated', () => {
 		});
 
 		it('should update messageListIndex with new message ids', async () => {
-			const triggerNotificationSpy = jest.fn();
-			jest
-				.spyOn(triggerNotification, 'triggerNotification')
-				.mockImplementation(triggerNotificationSpy);
-
 			const message = generateMessage({ id: '1' });
 			setMessagesInEmailStore([message], false);
 			const newMessage = generateMessage({ id: '2' });
