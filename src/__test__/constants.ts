ea9a7c3f6f35ea49be9c30db5a8b545bf2e4c2bc
Keshav Bhatt <keshavnrj@gmail.com>
2025-05-27 20:00:58 +0530

    fix: update import in constants to use carbonio-ui-commons

--- src/__test__/constants.ts
+++ src/__test__/constants.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { FOLDERS } from '../constants/folders';
+import { FOLDERS } from '@zextras/carbonio-ui-commons';
 
 export const FOLDERS_DESCRIPTORS = {
 	userRoot: {
