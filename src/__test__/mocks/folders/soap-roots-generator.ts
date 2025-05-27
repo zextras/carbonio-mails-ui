fbd14fbcc3bf39075f432a8192d52373d76f0f98
Keshav Bhatt <keshavnrj@gmail.com>
2025-05-27 19:58:06 +0530

    fix: update imports in soap-roots-generator to use carbonio-ui-commons

--- src/__test__/mocks/folders/soap-roots-generator.ts
+++ src/__test__/mocks/folders/soap-roots-generator.ts
@@ -4,10 +4,15 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 import { faker } from '@faker-js/faker';
+import {
+	BaseFolder,
+	FOLDERS,
+	FolderView,
+	SoapFolder,
+	SoapLink
+} from '@zextras/carbonio-ui-commons';
 import { map, orderBy } from 'lodash';
 
-import { FOLDERS } from '../../../constants/folders';
-import { BaseFolder, FolderView, SoapFolder, SoapLink } from '../../../types/folder';
 import { getMocksContext } from '../utils/mocks-context';
 
 let luuid: string | undefined = '';
