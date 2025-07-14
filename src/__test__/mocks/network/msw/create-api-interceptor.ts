a5bb0556aec783a138a76eca9cdbe1af60ba14ed
Giuliano Caregnato <giuliano.caregnato@zextras.com>
2025-07-14 11:00:08 +0200

    fix(create-api-interceptor.ts): add generic to HttpResponse
    Specify DefaultBodyType as the generic argument for HttpResponse in
    createAPIInterceptor to resolve type error.

--- src/__test__/mocks/network/msw/create-api-interceptor.ts
+++ src/__test__/mocks/network/msw/create-api-interceptor.ts
@@ -54,7 +54,7 @@ export type APIInterceptor = {
 export const createAPIInterceptor = (
 	method: 'get' | 'post',
 	url: string,
-	response: HttpResponse
+	response: HttpResponse<DefaultBodyType>
 ): APIInterceptor => {
 	let calledTimes = 0;
 	const requests: Array<StrictRequest<DefaultBodyType>> = [];
