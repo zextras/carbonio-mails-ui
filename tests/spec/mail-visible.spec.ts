7fdaf3dcfa2e4f411d87a71ced90598890a00144
Davide Frison <davide.frison@zextras.com>
2025-08-22 17:04:54 +0200

    chore: add playwright tests checking email is visible

---
+++ tests/spec/mail-visible.spec.ts
@@ -0,0 +1,13 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { test, expect } from '@playwright/test';
+
+test('has title', async ({ page }) => {
+	await page.goto('http://localhost:1234/carbonio');
+
+	// eslint-disable-next-line testing-library/prefer-screen-queries
+	await expect(page.getByText('Test to myself'), 'Email should be visible').toBeVisible();
+});
