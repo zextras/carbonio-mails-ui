ed12b22d38599e7f62686bc0c95fb56051a3ecd5
Keshav Bhatt <keshavnrj@gmail.com>
2025-08-22 13:49:47 +0530

    chore: move test, update import path in text-linkify test file

--- src/commons/mail-message-renderer/text-message-renderer/tests/text-linkify.test.ts
+++ src/commons/mail-message-renderer/text-message-renderer/tests/text-linkify.test.ts
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { linkifyText } from './text-linkify';
+import { linkifyText } from '../text-linkify';
 
 describe('linkifyToHtml', () => {
 	const email = 'foo@bar.com';
