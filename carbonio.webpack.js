82c6a002a4e763ea477f67ef7df6f9305ef4fa8e
Keshav Bhatt <keshavnrj@gmail.com>
2025-09-29 20:42:55 +0530

    feat: add support for dynamic BASE_PATH with commit hash and define supported locales

--- carbonio.webpack.js
+++ carbonio.webpack.js
@@ -5,10 +5,26 @@
  */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+
+const { execSync } = require('child_process');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
+
+const { SUPPORTED_LOCALES } = require('./src/constants/locale-consts');
 
 const customizeConfig = (config) => {
 	const newConfig = { ...config };
+
+	// Generate commit hash for static path (similar to shell project)
+	const commitHash = execSync('git rev-parse HEAD').toString().trim();
+	const baseStaticPath = `/static/iris/carbonio-mails-ui/${commitHash}/`;
+
+	const supportedLocalesList = Object.values(SUPPORTED_LOCALES);
+
+	const tinymceLocales = supportedLocalesList.map(
+		(locale) => ('tinymceLocale' in locale && locale.tinymceLocale) || locale.value
+	);
 
 	newConfig.resolve = {
 		...config.resolve,
@@ -18,6 +34,35 @@ const customizeConfig = (config) => {
 		},
 		modules: [path.resolve(__dirname, 'src'), 'node_modules']
 	};
+
+	// Add DefinePlugin to define BASE_PATH_2 as a global variable with commit hash
+	newConfig.plugins = newConfig.plugins || [];
+	newConfig.plugins.push(
+		new webpack.DefinePlugin({
+			BASE_PATH_2: JSON.stringify(baseStaticPath)
+		})
+	);
+
+	newConfig.plugins.push(
+		new CopyPlugin({
+			patterns: [
+				{
+					from: 'assets/',
+					to: ''
+				},
+				{
+					from: `plugins/help/js/i18n/**/(${tinymceLocales.join('|')}).js`,
+					to: '',
+					context: 'node_modules/tinymce/'
+				},
+				{
+					from: 'plugins/emoticons',
+					to: 'plugins/emoticons',
+					context: 'node_modules/tinymce/'
+				}
+			]
+		})
+	);
 
 	return newConfig;
 };
