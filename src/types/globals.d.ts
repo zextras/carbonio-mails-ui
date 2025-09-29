62d7473d279f3f3807f24bbaad6fd7ee556435f3
Keshav Bhatt <keshavnrj@gmail.com>
2025-09-29 20:33:18 +0530

    chore: add global type definitions for BASE_PATH variable

---
+++ src/types/globals.d.ts
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+// Global variables injected by webpack DefinePlugin
+declare global {
+	const BASE_PATH: string;
+}
+
+export {};
