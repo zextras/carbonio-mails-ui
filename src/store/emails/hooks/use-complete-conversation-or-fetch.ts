241b0e255835553ef1900c87c003b72bbf4d410c
Keshav Bhatt <keshavnrj@gmail.com>
2025-11-07 17:21:30 +0530

    fix: simplify conversation fetching logic by removing unused user settings and navigation checks

--- src/store/emails/hooks/use-complete-conversation-or-fetch.ts
+++ src/store/emails/hooks/use-complete-conversation-or-fetch.ts
@@ -4,13 +4,11 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo } from 'react';
 
-import { useUserSettings } from '@zextras/carbonio-shell-ui';
 import { debounce } from 'lodash';
 
-import { API_REQUEST_STATUS, DEFAULT_API_DEBOUNCE_TIME } from 'constants/index';
-import { convActionEmailStoreAction } from 'store/emails/actions/conv-action-action';
+import { DEFAULT_API_DEBOUNCE_TIME } from 'constants/index';
 import { searchConvEmailStoreAction } from 'store/emails/actions/search-conv-action';
 import { useConversationById, useConversationStatus } from 'store/emails/store';
 import { NormalizedConversation, SearchRequestStatus } from 'types/index.d';
@@ -30,36 +28,19 @@ export function useCompleteConversationOrFetch(
 ): ConversationWithStatus {
 	const conversation = useConversationById(conversationId);
 	const conversationStatus = useConversationStatus(conversationId);
-	const settings = useUserSettings();
-	const prefMarkMsgRead = settings?.prefs?.zimbraPrefMarkMsgRead !== '-1';
-	const prevConversationIdRef = useRef<string | null>(null);
-	const hasNavigated = prevConversationIdRef.current !== conversationId;
 
 	const requestDebouncedConversation = useMemo(
 		() =>
 			debounce(
 				() => {
 					if (conversation && !conversationStatus) {
-						const shouldMarkAsRead = !conversation.read && prefMarkMsgRead;
-						searchConvEmailStoreAction(conversationId, folderId, shouldMarkAsRead);
-					} else if (
-						hasNavigated &&
-						conversation &&
-						!conversation.read &&
-						conversationStatus === API_REQUEST_STATUS.fulfilled &&
-						prefMarkMsgRead
-					) {
-						convActionEmailStoreAction({
-							operation: 'read',
-							ids: [conversationId]
-						});
+						searchConvEmailStoreAction(conversationId, folderId);
 					}
-					prevConversationIdRef.current = conversationId;
 				},
 				DEFAULT_API_DEBOUNCE_TIME,
 				{ leading: false, trailing: true }
 			),
-		[conversation, conversationId, conversationStatus, folderId, prefMarkMsgRead, hasNavigated]
+		[conversation, conversationId, conversationStatus, folderId]
 	);
 
 	useEffect(() => {
