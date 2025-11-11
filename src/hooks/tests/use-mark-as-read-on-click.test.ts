5dbb172365b9cac94613eb8b76052f54efd5ea62
Keshav Bhatt <keshavnrj@gmail.com>
2025-11-11 18:59:37 +0530

    test: add unit tests for useMarkAsReadOnClick hook functionality

---
+++ src/hooks/tests/use-mark-as-read-on-click.test.ts
@@ -0,0 +1,66 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+import { renderHook, act } from '@testing-library/react';
+
+import { useMarkAsReadOnClick } from 'hooks/use-mark-as-read-on-click';
+
+jest.mock('@zextras/carbonio-shell-ui', () => ({
+	...jest.requireActual('@zextras/carbonio-shell-ui'),
+	// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+	useUserSettings: () => ({
+		prefs: { zimbraPrefMarkMsgRead: '0' }
+	})
+}));
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+const createActionMock = () => ({
+	canExecute: jest.fn(() => true),
+	execute: jest.fn()
+});
+
+describe('useMarkAsReadOnClick', () => {
+	it('should execute action when unread, preference enabled and conditions pass', () => {
+		const action = createActionMock();
+		const { result } = renderHook(() =>
+			useMarkAsReadOnClick({ isRead: false, action, conditions: [true, true] })
+		);
+
+		act(() => {
+			result.current();
+		});
+
+		expect(action.canExecute).toHaveBeenCalled();
+		expect(action.execute).toHaveBeenCalled();
+	});
+
+	it('should NOT execute when already read', () => {
+		const action = createActionMock();
+		const { result } = renderHook(() =>
+			useMarkAsReadOnClick({ isRead: true, action, conditions: [true] })
+		);
+
+		act(() => {
+			result.current();
+		});
+
+		expect(action.canExecute).not.toHaveBeenCalled();
+		expect(action.execute).not.toHaveBeenCalled();
+	});
+
+	it('should NOT execute when a condition fails', () => {
+		const action = createActionMock();
+		const { result } = renderHook(() =>
+			useMarkAsReadOnClick({ isRead: false, action, conditions: [true, false] })
+		);
+
+		act(() => {
+			result.current();
+		});
+
+		expect(action.canExecute).not.toHaveBeenCalled();
+		expect(action.execute).not.toHaveBeenCalled();
+	});
+});
