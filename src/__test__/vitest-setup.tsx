e8256f32540ebba59aa677bb4987f199552bff56
Davide Frison <davide.frison@zextras.com>
2025-11-25 14:47:10 +0100

    chore: add vitest config and dependencies

---
+++ src/__test__/vitest-setup.tsx
@@ -0,0 +1,125 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import '@testing-library/jest-dom';
+import { configure } from '@testing-library/react';
+import { noop } from 'lodash';
+import { SetupServer, setupServer } from 'msw/node';
+import { vi, beforeAll, beforeEach, afterEach, afterAll } from 'vitest';
+
+import * as shell from '../../__mocks__/@zextras/carbonio-shell-ui';
+import { useDistributionListsStore } from '../store/distribution-lists';
+import { getRestHandlers } from '@test-utils/network/msw/handlers';
+
+vi.mock('@zextras/carbonio-shell-ui', () => shell);
+
+// Setup MSW mock server
+let server = setupServer();
+
+configure({
+	asyncUtilTimeout: 2000
+});
+
+/**
+ * Default logic to execute before all the tests
+ */
+type DefaultBeforeAllTestsProps = {
+	onUnhandledRequest: 'warn' | 'error';
+};
+
+const defaultBeforeAllTests = (
+	{ onUnhandledRequest }: DefaultBeforeAllTestsProps = { onUnhandledRequest: 'warn' }
+): void => {
+	// Do not useFakeTimers with `whatwg-fetch` if using mocked server
+	// https://github.com/mswjs/msw/issues/448
+
+	server?.close();
+	server = setupServer(...getRestHandlers());
+	server.listen({ onUnhandledRequest });
+};
+
+beforeAll(() => {
+	defaultBeforeAllTests();
+});
+
+beforeEach(() => {
+	vi.useFakeTimers({ shouldAdvanceTime: true });
+	useDistributionListsStore.getState().reset();
+});
+
+afterEach(() => {
+	vi.clearAllTimers();
+	vi.useRealTimers();
+});
+
+afterAll(() => {
+	server.resetHandlers();
+	server.close();
+});
+
+// mock a simplified crypto
+Object.defineProperty(window.crypto, 'randomUUID', {
+	writable: true,
+	value: vi.fn(() => Math.random().toString())
+});
+
+Object.defineProperty(window.URL, 'createObjectURL', {
+	writable: true,
+	value: vi.fn()
+});
+
+// Mock Worker
+class Worker {
+	url: string;
+
+	onmessage: (msg: string) => void;
+
+	constructor(stringUrl: string) {
+		this.url = stringUrl;
+		this.onmessage = noop;
+	}
+
+	postMessage(msg: string): void {
+		this.onmessage(msg);
+	}
+}
+
+Object.defineProperty(window, 'Worker', {
+	writable: true,
+	value: Worker
+});
+
+// Mock ResizeObserver
+Object.defineProperty(window, 'ResizeObserver', {
+	writable: true,
+	value: function ResizeObserverMock(): ResizeObserver {
+		return {
+			observe: (): undefined => undefined,
+			unobserve: (): undefined => undefined,
+			disconnect: (): undefined => undefined
+		};
+	}
+});
+
+// mock a simplified Intersection Observer
+Object.defineProperty(window, 'IntersectionObserver', {
+	writable: true,
+	value: vi.fn(function intersectionObserverMock(
+		callback: IntersectionObserverCallback,
+		options: IntersectionObserverInit
+	) {
+		return {
+			thresholds: options.threshold,
+			root: options.root,
+			rootMargin: options.rootMargin,
+			observe: vi.fn(),
+			unobserve: vi.fn(),
+			disconnect: vi.fn()
+		};
+	})
+});
+
+export const getSetupServer = (): SetupServer => server;
---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Zextras <https://www.zextras.com>
+ *
+ * SPDX-License-Identifier: AGPL-3.0-only
+ */
+
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+	plugins: [
+		react({
+			jsxImportSource: '@emotion/react',
+			babel: {
+				plugins: ['@emotion/babel-plugin']
+			}
+		}),
+		tsconfigPaths()
+	],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: ['./src/__test__/vitest-setup.tsx', './src/__test__/setup-browser-env.ts'],
+		clearMocks: true,
+		mockReset: true,
+		testTimeout: 20000,
+		reporters: ['default', 'junit'],
+		coverage: {
+			enabled: true,
+			provider: 'v8',
+			reporter: ['text', 'cobertura', 'lcov'],
+			reportsDirectory: 'coverage',
+			include: ['src/**/*.{ts,tsx}'],
+			exclude: ['**/__test__/**', '**/tests/**', '**/mocks/**', '**/*.test.{js,jsx,ts,tsx}']
+		}
+	}
+});
