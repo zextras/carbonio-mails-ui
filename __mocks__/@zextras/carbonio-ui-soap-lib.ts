212626d4844823c5eb2ddf412a68c91815e49bdb
Davide Frison <davide.frison@zextras.com>
2025-11-28 18:14:55 +0100

    test: add back legacy xml soap lib

--- __mocks__/@zextras/carbonio-ui-soap-lib.ts
+++ __mocks__/@zextras/carbonio-ui-soap-lib.ts
@@ -4,6 +4,8 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import { getSoapFetch } from '@test-utils/network/fetch';
+
 export const useSync = vi.fn();
 export const useInfoRefresh = vi.fn();
 
@@ -19,6 +21,6 @@ export const ApiManager = {
 	getApiManager: vi.fn(() => apiManagerInstance)
 };
 
-// export const legacyXmlSoapFetch = vi.fn();
+export const legacyXmlSoapFetch = vi.fn();
 
-// export const legacySoapFetch = getSoapFetch('test-environment');
+export const legacySoapFetch = getSoapFetch('test-environment');
