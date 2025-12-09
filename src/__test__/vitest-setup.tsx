8007f112d6b74b534359ef5e83cef70a64f5316f
Keshav Bhatt <keshavnrj@gmail.com>
2025-12-09 15:19:35 +0530

    chore: enhance fake timers setup to clear native timers in vitest

--- src/__test__/vitest-setup.tsx
+++ src/__test__/vitest-setup.tsx
@@ -73,7 +73,7 @@ afterEach(() => {
 });
 
 beforeEach(() => {
-	vi.useFakeTimers({ shouldAdvanceTime: true });
+	vi.useFakeTimers({ shouldAdvanceTime: true, shouldClearNativeTimers: true });
 	useEditorsStore.setState({});
 	getUseEmailStoreAndHooksForTesting().resetMessagesAndPopulatedItems();
 	getUseEmailStoreAndHooksForTesting().resetConversationAndPopulatedItems();
