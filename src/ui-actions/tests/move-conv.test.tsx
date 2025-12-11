d41caf0c7ed0984bd073e2e35b2bef3c878c996d
Luca Stauble <luca.stauble@gmail.com>
2025-12-11 10:15:06 +0100

    test: add test for onMoveComplete callback

--- src/ui-actions/tests/move-conv.test.tsx
+++ src/ui-actions/tests/move-conv.test.tsx
@@ -170,6 +170,7 @@ describe('MoveConversation', () => {
 		expect(request.action.l).toBe(destinationFolder);
 		expect(request.action.tn).toBeUndefined();
 	});
+
 	it('should show an error message if API call returns a Fault case', async () => {
 		populateFoldersStore();
 
@@ -233,5 +234,36 @@ describe('MoveConversation', () => {
 
 		await user.click(await screen.findByRole('button', { name: /GO TO FOLDER/ }));
 		expect(navigate).toHaveBeenCalledWith('/mails/folder/2', { replace: true });
+	});
+
+	it('should call onMoveComplete on success', async () => {
+		const onMoveComplete = jest.fn();
+		populateFoldersStore();
+
+		createSoapAPIInterceptor<ConvActionRequest, ConvActionResponse>('ConvAction', {
+			action: {
+				id: convIds.join(','),
+				op: 'move'
+			}
+		});
+
+		const { user } = setupTest(
+			<MoveConversation
+				folderId={sourceFolder}
+				selectedIDs={convIds}
+				onClose={jest.fn()}
+				isRestore={false}
+				onMoveComplete={onMoveComplete}
+			/>
+		);
+
+		const inboxFolderListItem = await screen.findByTestId(`folder-accordion-item-${FOLDERS.INBOX}`);
+
+		await user.click(inboxFolderListItem);
+		await user.click(await screen.findByRole('button', { name: /Move/ }));
+
+		await expect(screen.findByText('Conversation successfully moved')).resolves.toBeInTheDocument();
+
+		expect(onMoveComplete).toHaveBeenCalledWith(convIds);
 	});
 });
