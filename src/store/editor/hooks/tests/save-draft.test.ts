a6542dd6913f44645527c91d3ad0ecc76e716d55
Davide Frison <davide.frison@zextras.com>
2025-12-19 16:31:03 +0100

    test: verify debounced save draft calls the save draft after 2 seconds

--- src/store/editor/hooks/tests/save-draft.test.ts
+++ src/store/editor/hooks/tests/save-draft.test.ts
@@ -4,6 +4,7 @@
  * SPDX-License-Identifier: AGPL-3.0-only
  */
 
+import { act } from '@testing-library/react';
 import { HttpResponse } from 'msw';
 
 import { setupHook } from '@test-setup';
@@ -36,21 +37,22 @@ describe('useEditorDraftSave', () => {
 		'call the saveDraft API function after 3 seconds if the saveDraft is invoked twice, with a 1 second delay between the 2 invocations'
 	);
 
-	it('debounced save draft calls the SaveDraft after 3 seconds', async () => {
+	it('debounced save draft calls the SaveDraft after 2 seconds by default', async () => {
+		vi.useFakeTimers();
 		const editor = generateNewMessageEditor();
 		setupEditorStore({ editors: [editor] });
 		const { result: hookResult } = setupHook(useSaveDraftFromEditor, {});
 		const saveDraft = createAPIInterceptor(
 			'post',
-			'/service/SaveDraftRequest',
+			'/service/soap/SaveDraftRequest',
 			HttpResponse.json()
 		);
 
-		hookResult.current.debouncedSaveDraft(editor.id);
+		act(() => hookResult.current.debouncedSaveDraft(editor.id));
 		expect(saveDraft.getCalledTimes()).toBe(0);
-		vi.advanceTimersByTime(1000);
+		await vi.advanceTimersByTimeAsync(1000);
 		expect(saveDraft.getCalledTimes()).toBe(0);
-		vi.advanceTimersByTime(2000);
+		await vi.advanceTimersByTimeAsync(1000);
 		expect(saveDraft.getCalledTimes()).toBe(1);
 	});
 });
