b139a4c895e29ee6a53bcf2cd53f4f753ac34747
Keshav Bhatt <keshavnrj@gmail.com>
2026-01-16 21:13:37 +0530

    feat: replace IconButton with Button in edit view identity selector

--- src/views/app/detail-panel/edit/parts/edit-view-identity-selector.tsx
+++ src/views/app/detail-panel/edit/parts/edit-view-identity-selector.tsx
@@ -8,10 +8,10 @@ import React, { FC, useCallback, useMemo, useState } from 'react';
 import styled from '@emotion/styled';
 import {
 	Avatar,
+	Button,
 	Container,
 	Dropdown,
 	DropdownItem,
-	IconButton,
 	Row,
 	Text,
 	Tooltip
@@ -145,7 +145,12 @@ export const EditViewIdentitySelector: FC<EditViewIdentitySelectorProps> = ({ ed
 						padding={{ all: 'small' }}
 					>
 						{createIdentitySelectorItemElement(selected, false, noName)}
-						<IconButton icon={open ? 'ChevronUpOutline' : 'ChevronDownOutline'} onClick={noop} />
+						<Button
+							icon={open ? 'ChevronUpOutline' : 'ChevronDownOutline'}
+							type={'ghost'}
+							color={'gray0'}
+							onClick={noop}
+						/>
 					</Row>
 				</Dropdown>
 			</Tooltip>
