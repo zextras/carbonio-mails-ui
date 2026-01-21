2c2883ba40ede0882a65666a6a96e59efc0e99b0
FrancescoGottardi <gottardifrancesco92@gmail.com>
2026-01-21 12:51:23 +0100

    fix: dependency

--- src/views/app/detail-panel/edit/parts/rich-text-editor-container.tsx
+++ src/views/app/detail-panel/edit/parts/rich-text-editor-container.tsx
@@ -205,7 +205,6 @@ export const RichTextEditorContainer = ({
 				'[data-testid="edit-view-editor"]'
 			)?.parentElement;
 			handleEditorPaste(editor, editorID, event);
-			onTextChange();
 
 			// Restore scroll position. In firefox scrollbar trips on paste event, see bug [CO-1979]
 			if (editViewWrapper) {
