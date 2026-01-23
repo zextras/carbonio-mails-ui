6c47851822ef3e2e049f6dc5c5e3a993ec20e9a1
Davide Frison <davide.frison@zextras.com>
2026-01-23 12:02:06 +0100

    Revert "wip: editor attachments improvement"
    This reverts commit 6b1ad00b68761ae6801cdd3a76ca645f83e2ce71.

--- src/store/editor/hooks/attachments.ts
+++ src/store/editor/hooks/attachments.ts
@@ -14,7 +14,7 @@ import {
 	UploadCallbacks
 } from 'api/upload-attachments-api';
 import { TIMEOUTS } from 'constants/index';
-import { composeAttachmentDownloadUrlFromUploadID as composeAttachmentDownloadUrlFromAttachmentId } from 'helpers/attachments';
+import { composeAttachmentDownloadUrlFromUploadID } from 'helpers/attachments';
 import { useUiUtilities } from 'hooks/use-ui-utilities';
 import { composeCidUrlFromContentId } from 'store/editor/editor-transformations';
 import { computeAndUpdateEditorStatus } from 'store/editor/hooks/commons';
@@ -165,14 +165,36 @@ export const useEditorAttachments = (editorId: MailsEditorV2['id']): EditorAttac
 	const addAndSaveGenericAttachments = (
 		files: Array<File>,
 		areInline: boolean,
-		callbacks?: UploadAttachmentsOptions
+		callbacks?: UploadAttachmentsOptions & {
+			onSaveComplete?: (savedContentIds: Array<string>) => void;
+		}
 	): Array<UnsavedAttachment> => {
 		const customizedCallbacks = {
 			...callbacks,
 			onUploadsEnd: (completedUploadsId: Array<string>, failedUploadsId: Array<string>): void => {
 				const editor = getEditor({ id: editorId });
 				if (editor) {
-					debouncedSaveDraft(editorId);
+					const uploadedUnsavedAttachments = filterUnsavedAttachmentsByUploadId(
+						editor.unsavedAttachments,
+						completedUploadsId
+					);
+
+					const uploadedContentIds: Array<string> = [];
+					uploadedUnsavedAttachments.forEach((uploadedUnsavedAttachment) => {
+						if (
+							uploadedUnsavedAttachment.isInline === areInline &&
+							uploadedUnsavedAttachment.contentId
+						) {
+							uploadedContentIds.push(uploadedUnsavedAttachment.contentId);
+						}
+					});
+
+					const saveDraftOptions: SaveDraftOptions = {
+						onComplete: (): void => {
+							callbacks?.onSaveComplete && callbacks.onSaveComplete(uploadedContentIds);
+						}
+					};
+					debouncedSaveDraft(editorId, saveDraftOptions);
 				}
 
 				callbacks?.onUploadsEnd && callbacks.onUploadsEnd(completedUploadsId, failedUploadsId);
@@ -243,6 +265,7 @@ export const useEditorAttachments = (editorId: MailsEditorV2['id']): EditorAttac
 		}
 	): Array<UnsavedAttachment> => {
 		const customizedCallbacks = {
+			...omit(callbacks, 'onSaveComplete'),
 			onUploadComplete: (file: File, uploadId: string, attachmentId: string): void => {
 				const editor = getEditor({ id: editorId });
 				if (!editor) {
@@ -253,7 +276,7 @@ export const useEditorAttachments = (editorId: MailsEditorV2['id']): EditorAttac
 				const inlineInfo = {
 					contentId,
 					cidUrl: contentId ? (composeCidUrlFromContentId(contentId) ?? undefined) : undefined,
-					downloadServiceUrl: composeAttachmentDownloadUrlFromAttachmentId(attachmentId)
+					downloadServiceUrl: composeAttachmentDownloadUrlFromUploadID(uploadId)
 				};
 
 				callbacks?.onSaveComplete && callbacks.onSaveComplete(inlineInfo);
