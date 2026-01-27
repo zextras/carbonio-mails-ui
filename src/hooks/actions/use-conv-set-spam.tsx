da4e0a4bada86ca582a26a237acd5a2d4741cd4c
Keshav Bhatt <keshavnrj@gmail.com>
2026-01-27 18:40:26 +0530

    feat: refactor spam handling logic to remove unnecessary timeout and improve snackbar notifications

--- src/hooks/actions/use-conv-set-spam.tsx
+++ src/hooks/actions/use-conv-set-spam.tsx
@@ -35,47 +35,35 @@ export const useConvSetSpamFn = ({
 
 	const { closeConversationPanel, currentConversation } = useConversationDetailPanelControls();
 	const execute = useCallback((): void => {
-		let notCanceled = true;
+		convActionEmailStoreAction({
+			operation: 'spam',
+			ids
+		}).then((res) => {
+			if ('Fault' in res) {
+				createSnackbar({
+					key: `spam-${ids}`,
+					replace: true,
+					severity: 'error',
+					label: t('label.error_try_again', 'Something went wrong, please try again'),
+					autoHideTimeout: 3000,
+					hideButton: true
+				});
+				return;
+			}
 
-		const infoSnackbar = (hideButton = false): void => {
+			onActionComplete && onActionComplete(ids);
+			if (currentConversation && ids.includes(currentConversation.id)) {
+				closeConversationPanel();
+			}
 			createSnackbar({
 				key: `spam-${ids}`,
 				replace: true,
 				severity: 'info',
-				label: t('messages.snackbar.marked_as_spam', 'You’ve marked this e-mail as Spam'),
+				label: t('messages.snackbar.marked_as_spam', "You've marked this e-mail as Spam"),
 				autoHideTimeout: 3000,
-				hideButton,
-				actionLabel: t('label.undo', 'Undo'),
-				onActionClick: (): void => {
-					notCanceled = false;
-				}
+				hideButton: true
 			});
-		};
-		infoSnackbar();
-		setTimeout((): void => {
-			if (notCanceled) {
-				convActionEmailStoreAction({
-					operation: 'spam',
-					ids
-				}).then((res) => {
-					if ('Fault' in res) {
-						createSnackbar({
-							key: `spam-${ids}`,
-							replace: true,
-							severity: 'error',
-							label: t('label.error_try_again', 'Something went wrong, please try again'),
-							autoHideTimeout: 3000
-						});
-						return;
-					}
-
-					onActionComplete && onActionComplete(ids);
-					if (currentConversation && ids.includes(currentConversation.id)) {
-						closeConversationPanel();
-					}
-				});
-			}
-		}, 3000);
+		});
 	}, [closeConversationPanel, createSnackbar, currentConversation, ids, onActionComplete, t]);
 
 	return useMemo(() => ({ canExecute, execute }), [canExecute, execute]);
