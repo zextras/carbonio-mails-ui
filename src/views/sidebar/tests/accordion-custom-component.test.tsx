4f79411fca7e712a0ae26e6a1608fcd9f6bd4a10
Keshav Bhatt <keshavnrj@gmail.com>
2026-02-18 18:51:30 +0530

    chore: cleanup of test
    remove a test that do not align with business logic

--- src/views/sidebar/tests/accordion-custom-component.test.tsx
+++ src/views/sidebar/tests/accordion-custom-component.test.tsx
@@ -14,7 +14,7 @@ import { setupTest } from '@test-setup';
 import { createFakeIdentity } from '@test-utils/accounts/fakeAccounts';
 import { generateFolder, generateFolderLink } from '@test-utils/folders/folders-generator';
 import { getMocksContext } from '@test-utils/utils/mocks-context';
-import AccordionCustomComponent from 'views/sidebar/accordion-custom-component';
+import { AccordionCustomComponent } from 'views/sidebar/accordion-custom-component';
 
 describe('accordion-custom-component', () => {
 	it('should render without crashing', () => {
@@ -108,7 +108,7 @@ describe('accordion-custom-component', () => {
 		expect(folderAccordionItem).not.toBeInTheDocument();
 	});
 
-	it('should render accordion item with identity fullName when folder is ROOT', () => {
+	it('should render accordion item with identity fullName when folder is USER_ROOT', () => {
 		const userRootFolder = generateFolder({
 			id: FOLDERS.USER_ROOT,
 			isLink: false,
@@ -121,7 +121,7 @@ describe('accordion-custom-component', () => {
 		const { fullName } = identities.primary.identity;
 		setupTest(<AccordionCustomComponent item={userRootFolder} />);
 		const userRootItem = screen.getByTestId(`accordion-folder-item-${FOLDERS.USER_ROOT}`);
-		expect(userRootItem).toBeInTheDocument();
+
 		expect(within(userRootItem).getByText(fullName)).toBeInTheDocument();
 	});
 
@@ -137,19 +137,5 @@ describe('accordion-custom-component', () => {
 		setupTest(<AccordionCustomComponent item={sharedDraft} />);
 
 		expect(screen.getByText(87)).toBeInTheDocument();
-	});
-
-	it('should not display message counter on shared account Trash folder', () => {
-		const identity = createFakeIdentity();
-		const sharedDraft = {
-			...generateFolderLink('100', '101', identity),
-			absFolderPath: '/Drafts',
-			id: FOLDERS.TRASH,
-			n: 87
-		};
-
-		setupTest(<AccordionCustomComponent item={sharedDraft} />);
-
-		expect(screen.queryByText(87)).not.toBeInTheDocument();
 	});
 });
