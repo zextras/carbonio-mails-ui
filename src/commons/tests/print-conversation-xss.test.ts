5b35da912568950cc99046bceeb2552046895bab
Dennis <dennis.ceron@zextras.com>
2026-03-24 16:37:17 +0100

    fix: remove redundant copyright comment in print email test file

--- src/commons/tests/print-conversation-xss.test.ts
+++ src/commons/tests/print-conversation-xss.test.ts
@@ -3,11 +3,6 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-only
  */
-/*
- * SPDX-FileCopyrightText: 2026 Zextras <https://www.zextras.com\>
- *
- * SPDX-License-Identifier: AGPL-3.0-only
- */
 
 import * as shell from '@zextras/carbonio-shell-ui';
 
